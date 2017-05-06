68a56e1ab694f6c9d046da9b6d402ece1d8e844e
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-06 03:05:53 +0000

    implement parser up to lexeme

--- src/HFrameNet/Type.hs
+++ src/HFrameNet/Type.hs
@@ -8,6 +8,12 @@ import qualified Data.Text as T
 
 type DateTime = Text
 
+data SemType = SemType { _semtype_ID :: Int
+                       , _semtype_name :: Text }
+             deriving (Show)
+
+makeLenses ''SemType
+
 data FE = FE { _fe_ID :: Int
              , _fe_name :: Text
              , _fe_abbrev :: Text
@@ -16,12 +22,36 @@ data FE = FE { _fe_ID :: Int
              , _fe_fgColor :: Text
              , _fe_bgColor :: Text
              , _fe_definition :: Text
-             -- , _fe_semType :: [Sem]
+             , _fe_semType :: [SemType]
              }
           deriving (Show)
 
 makeLenses ''FE
-  
+
+data Lexeme = Lexeme { _lexeme_name :: Text
+                     , _lexeme_POS :: Text
+                     , _lexeme_breakBefore :: Bool
+                     , _lexeme_headword :: Bool
+                     , _lexeme_order :: Int }
+            deriving (Show)
+
+makeLenses ''Lexeme
+
+data LexUnit = LexUnit { _lexunit_ID :: Int
+                       , _lexunit_name :: Text
+                       , _lexunit_POS :: Text
+                       , _lexunit_status :: Text
+                       , _lexunit_cDate :: DateTime
+                       , _lexunit_lemmaID :: Int
+                       , _lexunit_definition :: Text
+                       -- , _lexunit_annotation :: Annotation
+                       , _lexunit_lexeme :: [Lexeme]
+                       , _lexunit_semType :: [SemType]
+                                            
+                       }
+               deriving (Show)
+
+makeLenses ''LexUnit
 
 
 data Frame = Frame { _frame_ID :: Int
@@ -29,7 +59,7 @@ data Frame = Frame { _frame_ID :: Int
                    , _frame_cDate :: DateTime
                    , _frame_definition :: Text
                    , _frame_fe :: [FE]
-                   -- , _frame_lexUnit :: [LexUnit]
+                   , _frame_lexUnit :: [LexUnit]
                    }
            deriving (Show)
 
--- test/parse.hs
+++ test/parse.hs
@@ -12,6 +12,8 @@ import           Data.Text.Read (decimal)
 import           Text.Taggy.Lens
 --
 import           HFrameNet.Type
+--
+import           Debug.Trace
 
 -- safeHead [] = Nothing
 -- safeHead (x:_) = Just x
@@ -20,25 +22,55 @@ readDecimal :: Text -> Maybe Int
 readDecimal = either (\_ -> Nothing) (Just . fst) . decimal 
 
 
+readBoolean :: Text -> Maybe Bool
+readBoolean "true" = Just True
+readBoolean "false" = Just False
+readBoolean _ = Nothing
+
 p_frame :: Element -> Maybe Frame
 p_frame x = Frame <$> (readDecimal =<< (x ^. attr "ID"))
                   <*> x ^. attr "name"
                   <*> x ^. attr "cDate"
                   <*> listToMaybe (x ^.. elements . named (only "definition") . element . contents)
-                  <*> mapM p_fe (x ^.. elements . named (only "FE"))
---   in Frame id' name cDate def fes
+                  <*> mapM p_FE (x ^.. elements . named (only "FE"))
+                  <*> trace (show (x ^.. elements . named (only "lexUnit"))) 
+                        (mapM p_lexUnit (x ^.. elements . named (only "lexUnit")))
 
 
-p_fe :: Element -> Maybe FE
-p_fe x = FE <$> (readDecimal =<< (x ^. attr "ID"))
+p_FE :: Element -> Maybe FE
+p_FE x = FE <$> (readDecimal =<< (x ^. attr "ID"))
             <*> x ^. attr "name"
             <*> x ^. attr "abbrev"
             <*> x ^. attr "cDate"
             <*> x ^. attr "coreType"
             <*> x ^. attr "fgColor"
             <*> x ^. attr "bgColor"
-            <*> listToMaybe (x ^.. elements . named (only "definition") . element . contents)       
-  -- in FE id' name abbrev cDate coreType fgColor bgColor def
+            <*> listToMaybe (x ^.. elements . named (only "definition") . element . contents)
+            <*> mapM p_semType (x ^.. elements . named (only "semType"))
+
+
+p_semType :: Element -> Maybe SemType
+p_semType x = SemType <$> (readDecimal =<< (x ^. attr "ID"))
+                      <*> x ^. attr "name"
+
+
+p_lexUnit :: Element -> Maybe LexUnit
+p_lexUnit x = LexUnit <$> (readDecimal =<< (x ^. attr "ID"))
+                      <*> x ^. attr "name"
+                      <*> x ^. attr "POS"
+                      <*> x ^. attr "status"
+                      <*> x ^. attr "cDate"
+                      <*> (readDecimal =<< (x ^. attr "lemmaID"))
+                      <*> listToMaybe (x ^.. elements . named (only "definition") . element . contents)
+                      <*> mapM p_lexeme  (x ^.. elements . named (only "lexeme"))
+                      <*> mapM p_semType (x ^.. elements . named (only "semType"))
+
+p_lexeme :: Element -> Maybe Lexeme
+p_lexeme x = Lexeme <$> x ^. attr "name"
+                    <*> x ^. attr "POS"
+                    <*> (readBoolean =<< (x ^. attr "breakBefore"))
+                    <*> (readBoolean =<< (x ^. attr "headword"))
+                    <*> (readDecimal =<< (x ^. attr "order"))
 
 main :: IO ()
 main = do
