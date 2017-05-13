4985e75c675234591d7b09191699d611d85bacd3
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-13 20:27:49 +0000

    complete parser for xml defined by frameset.dtd

--- src/NomBank.hs
+++ src/NomBank.hs
@@ -10,6 +10,60 @@ data Note = Note { _note_content :: Text }
 
 makeLenses ''Note
 
+
+
+data Person = Third | Other                  deriving (Show,Eq,Ord)
+
+data Tense = Present | Past | Future         deriving (Show,Eq,Ord)
+
+data Aspect = Perfect | Progressive | Both   deriving (Show,Eq,Ord)
+
+data Voice = Active | Passive                deriving (Show,Eq,Ord)
+
+data Form = Infinitive | Gerund | Participle | Full
+          deriving (Show,Eq,Ord)
+
+data Inflection = Inflection { _inflection_person :: Maybe Person
+                             , _inflection_tense  :: Maybe Tense
+                             , _inflection_aspect :: Maybe Aspect
+                             , _inflection_voice  :: Maybe Voice
+                             , _inflection_form   :: Maybe Form }
+                deriving Show
+
+makeLenses ''Inflection
+
+data Arg = Arg { _arg_contents :: Text
+               , _arg_n :: Text
+               , _arg_h :: Maybe Text
+               , _arg_f :: Maybe Text
+               }
+         deriving Show
+                  
+makeLenses ''Arg
+
+data Rel = Rel { _rel_contents :: Text
+               , _rel_n :: Maybe Text
+               , _rel_h :: Maybe Text
+               , _rel_f :: Maybe Text
+               }
+         deriving Show
+
+makeLenses ''Rel
+
+data Example = Example { _example_inflection :: Maybe Inflection
+                       , _example_note :: [Note]
+                       , _example_text :: Text
+                       , _example_arg :: [Arg]
+                       , _example_rel :: [Rel]
+                       , _example_name :: Maybe Text
+                       , _example_type :: Maybe Text
+                       , _example_src :: Maybe Text
+                       }
+             deriving Show
+
+makeLenses ''Example
+
+
 data VNTheta = Actor1
              | Actor2
              | Agent
@@ -64,7 +118,7 @@ makeLenses ''Roles
                                   
 data RoleSet = RoleSet { _roleset_note :: [Note]
                        , _roleset_roles :: Roles
-                       -- , _roleset_example :: [Example]
+                       , _roleset_example :: [Example]
                        , _roleset_id :: Text
                        , _roleset_name :: Maybe Text
                        , _roleset_source :: Maybe Text
