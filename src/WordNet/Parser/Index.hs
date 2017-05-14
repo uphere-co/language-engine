8a61eaecf2a06bc1d59b171ea0b7395ce4931a96
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-14 00:48:46 +0000

    move WordNet to WordNet.Parser.Index

---
+++ src/WordNet/Parser/Index.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module WordNet.Parser.Index where
+
+import Control.Lens
+import           Data.Text           (Text)
+import qualified Data.Text    as T
+import           Data.Text.Read
+--
+import WordNet.Type
+
+isComment :: Text -> Bool
+isComment t | T.length t > 2 = T.take 2 t == "  "
+            | otherwise    = True
+
+eitherToMaybe (Left x) = Nothing
+eitherToMaybe (Right x) = Just x
+
+parse :: Text -> Maybe LexItem
+parse = worker . T.words
+  where
+    worker (lem:pos:cnt1:cnt2:rem) = do
+      synset_cnt <- fst <$> eitherToMaybe (decimal cnt1)
+      p_cnt <- fst <$> eitherToMaybe (decimal cnt2)
+      let (ptr_symbol,cnt3:cnt4:rem') = splitAt p_cnt rem
+      sense_cnt <- fst <$> eitherToMaybe (decimal cnt3)
+      tagsense_cnt <- fst <$> eitherToMaybe (decimal cnt4)
+      synset_offset <- mapM (fmap fst . eitherToMaybe . decimal) rem'
+      return (LexItem lem pos synset_cnt p_cnt ptr_symbol sense_cnt tagsense_cnt synset_offset)
+    worker _ = Nothing
+
+
--- test/test.hs
+++ test/test.hs
@@ -2,13 +2,13 @@ import qualified Data.Text    as T
 import qualified Data.Text.IO as TIO
 --
 import WordNet
-import WordNet.Type
+-- import WordNet.Type
 
 
 parseFile :: FilePath -> IO ()
 parseFile fp = do
   txt <- TIO.readFile fp 
-  let lst = take 100 $ filter (not.isComment) (T.lines txt)
+  let lst = filter (not.isComment) (T.lines txt)
   mapM_ (print . (\x -> (x,parse x))) lst
 
 main = parseFile "/scratch/wavewave/wordnet/WordNet-3.0/dict/index.verb"
