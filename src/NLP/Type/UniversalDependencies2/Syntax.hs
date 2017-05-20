2283ad6344e0f7eb873f3e218dde95433d3f4203
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-20 01:56:10 +0000

    implement parseDepRel

--- src/NLP/Type/UniversalDependencies2/Syntax.hs
+++ src/NLP/Type/UniversalDependencies2/Syntax.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module NLP.Type.UniversalDependencies2.Syntax where
+
+import Data.Text (Text)
 
 data DependencyRelation = ACL        -- ^ clausal modifier of noun (adjectival clause)
                         | ADVCL      -- ^ adverbial clause modifier
@@ -38,3 +42,43 @@ data DependencyRelation = ACL        -- ^ clausal modifier of noun (adjectival c
                         | VOCATIVE   -- ^ vocative
                         | XCOMP      -- ^ open clausal complement
                         deriving (Show,Eq,Ord)
+
+parseDepRel :: Text -> Maybe DependencyRelation
+parseDepRel "acl"        = Just ACL
+parseDepRel "advcl"      = Just ADVCL
+parseDepRel "advmod"     = Just ADVMOD
+parseDepRel "amod"       = Just AMOD
+parseDepRel "appos"      = Just APPOS
+parseDepRel "aux"        = Just AUX
+parseDepRel "case"       = Just CASE
+parseDepRel "cc"         = Just CC
+parseDepRel "ccomp"      = Just CCOMP
+parseDepRel "clf"        = Just CLF
+parseDepRel "compound"   = Just COMPOUND
+parseDepRel "conj"       = Just CONJ
+parseDepRel "cop"        = Just COP
+parseDepRel "csubj"      = Just CSUBJ
+parseDepRel "dep"        = Just DEP
+parseDepRel "det"        = Just DET
+parseDepRel "discourse"  = Just DISCOURSE
+parseDepRel "dislocated" = Just DISLOCATED
+parseDepRel "expl"       = Just EXPL
+parseDepRel "fixed"      = Just FIXED
+parseDepRel "flat"       = Just FLAT
+parseDepRel "goeswith"   = Just GOESWITH
+parseDepRel "iobj"       = Just IOBJ
+parseDepRel "list"       = Just LIST
+parseDepRel "mark"       = Just MARK
+parseDepRel "nmod"       = Just NMOD
+parseDepRel "nsubj"      = Just NSUBJ
+parseDepRel "nummod"     = Just NUMMOD
+parseDepRel "obj"        = Just OBJ
+parseDepRel "obl"        = Just OBL
+parseDepRel "orphan"     = Just ORPHAN
+parseDepRel "parataxis"  = Just PARATAXIS
+parseDepRel "punct"      = Just PUNCT
+parseDepRel "reparandum" = Just REPARANDUM
+parseDepRel "root"       = Just ROOT
+parseDepRel "vocative"   = Just VOCATIVE
+parseDepRel "xcomp"      = Just XCOMP
+parseDeprel _            = Nothing
