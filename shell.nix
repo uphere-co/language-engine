d16f06d7cb146ab5a13034dd3305f862c419d598
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-22 19:46:07 +0000

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> {}
+, uphere-nix-overlay ? <uphere-nix-overlay>
+}:
+
+with pkgs;
+
+let
+  hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix")
+               { inherit pkgs; };
+  newHaskellPackages = haskellPackages.override {
+    overrides = hsconfig;
+  };
+
+
+  hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
+            attoparsec
+            haskeline
+            lens
+            monad-loops
+            optparse-applicative
+            split
+            text text-format
+            yayaml
+
+            lens
+            taggy-lens
+          ]);
+
+in
+
+stdenv.mkDerivation {
+  name = "SRL-dev";
+  buildInputs = [ hsenv ];
+  shellHook = ''
+  '';
+}
+
