669c819a2909d2d04abe6bf6aa11700d1a4157c6
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-23 00:00:42 +0000

    add missing cases and comments

--- src/SRL/CoNLL/CoNLL08/Type.hs
+++ src/SRL/CoNLL/CoNLL08/Type.hs
@@ -1,40 +1,75 @@
 module SRL.CoNLL.CoNLL08.Type where
 
-data Deprel = ADV
-            | AMOD
-            | APPO
-            | CONJ
-            | COORD
-            | DEP
+data Deprel = ADV     -- ^ Unclassified adverbial
+            | AMOD    -- ^ Modifier of adjective or adverb 
+            | APPO    -- ^ Apposition
+            | BNF     -- ^ Benefactor (the for phrase for verbs that undergo dative shift)
+            | CONJ    -- ^ Between conjunction and second conjunct in a coordination
+            | COORD   -- ^ Coordination
+            | DEP     -- ^ Unclassified relation
+            | DTV     -- ^ Dative (the to phrase for verbs that undergo dative shift)
+            | EXTR    -- ^ Extraposed element in expletive constructions
+            | GAP     -- ^ Gapping: between conjunction and the parts of a structure with an ellipsed head IM Between infinitive marker and verb
+            | HMOD    -- ^ Modifier in hyphenation, such as two in two-part HYPH Between first part of hyphenation and hyphen NAME Name-internal link
+            | LGS     -- ^ Logical subject
+            | LOC     -- ^ Location
+            | MNR     -- ^ Manner
+            | NMOD    -- ^ Modifier of nominal
+            | OBJ     -- ^ Direct or indirect object or clause complement
+            | OPRD    -- ^ Object complement
+            | P       -- ^ Punctuation
+            | PMOD    -- ^ Between preposition and its child in a PP
+            | POSTHON -- ^ Posthonorifics such as Jr, Inc.
+            | PRD     -- ^ Predicative complement
+            | PRN     -- ^ Parenthetical
+            | PRP     -- ^ Purpose or reason
+            | PRT     -- ^ Particle
+            | PUT     -- ^ Various locative complements of the verb put  * 
+            | ROOT    -- ^ Root
+            | SBJ     -- ^ Subject
+            | SUB     -- ^ Between subordinating conjunction and verb
+            | SUFFIX  -- ^ Possessive ’s
+            | TITLE   -- ^ Titles such as Mr, Dr
+            | TMP     -- ^ Temporal
+            | VC      -- ^ Verb chain
+            | VOC     -- ^ Vocative
+            -- 
+            | ADV_GAP
+            | AMOD_GAP
             | DEP_GAP
-            | DIR
-            | DTV
-            | EXT
-            | EXTR
+            | DIR     -- ^ Direction
+            | DIR_GAP
+            | DTV_GAP
+            | END
+            | EXT     -- ^ Extent              
+            | EXT_GAP
+            | EXTR_GAP
+            | GAP_LGS
+            | GAP_LOC
+            | GAP_LOC_PRD
+            | GAP_MNR
+            | GAP_NMOD
+            | GAP_OBJ
+            | GAP_OPRD
+            | GAP_PMOD
             | GAP_PRD
-            | HMOD
+            | GAP_PRP
+            | GAP_PUT
+            | GAP_SBJ
+            | GAP_SUB
+            | GAP_TMP
+            | GAP_VC
             | HYPH
             | IM
-            | LGS
-            | LOC
+            | LOC_MNR
+            | LOC_OPRD
             | LOC_PRD
-            | MNR
+            | LOC_TMP
+            | MNR_PRD
+            | MNR_TMP
             | NAME
-            | NMOD
-            | OBJ
-            | OPRD
-            | P 
-            | PMOD
-            | PRD
-            | PRN
-            | PRP
-            | PRT                            
-            | ROOT
-            | SBJ
-            | SUB
-            | SUFFIX
-            | TITLE
-            | TMP
-            | VOC
-            | VC
+            | PRD_PRP
+            | PRD_TMP  
+            
+
             deriving (Show,Eq,Ord,Enum)
