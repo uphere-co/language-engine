3f7923aef6dd18254c040d8961ed4c3714272341
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-23 19:00:37 +0000

    NLP.Parser.PennTreebankII moved from autoencode

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{ pkgs ? import <nixpkgs> {}
+, uphere-nix-overlay ? <uphere-nix-overlay>
+}:
+
+with pkgs;
+
+let
+  hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix")
+               { inherit pkgs; };
+  newHaskellPackages = haskellPackages.override {
+    overrides = hsconfig;
+  };
+
+
+  hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
+            aeson
+            attoparsec
+            cabal-install
+          ]);
+
+in
+
+stdenv.mkDerivation {
+  name = "nlp-types-dev";
+  buildInputs = [ hsenv ];
+  shellHook = ''
+  '';
+}
+
--- src/NLP/Type/PennTreebankII.hs
+++ src/NLP/Type/PennTreebankII.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -225,6 +228,12 @@ instance FromJSON AnchorTag where
 instance ToJSON AnchorTag where
   toJSON = genericToJSON defaultOptions
 
+
+data PennTreeGen c p a = PN c [PennTreeGen c p a]
+                       | PL p a
+                   deriving (Show, Functor, Foldable, Traversable)
+                            
+type PennTree = PennTreeGen Text Text Text
 
 
 
