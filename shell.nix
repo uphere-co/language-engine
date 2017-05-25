36108b5da930e933ca7c48bd521952e3816fcef3
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-25 22:12:56 +0000

    add HCoreNLP to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,27 @@
 { pkgs ? import <nixpkgs> {}
 , uphere-nix-overlay ? <uphere-nix-overlay>
+, HCoreNLP ? <HCoreNLP>
+, nlp-types ? <nlp-types>
 }:
 
 with pkgs;
 
 let
+  res_corenlp = import (uphere-nix-overlay + "/nix/linguistic-resources/corenlp.nix") {
+    inherit fetchurl fetchzip srcOnly;
+  };
+  corenlp = res_corenlp.corenlp;
+  corenlp_models = res_corenlp.corenlp_models;
+
   hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix")
                { inherit pkgs; };
+  hsconfig2 =
+    self: super: {
+      "nlp-types" = self.callPackage (import nlp-types) {};
+      "HCoreNLP" = self.callPackage (import HCoreNLP) { inherit jdk corenlp corenlp_models; };
+    };  
   newHaskellPackages = haskellPackages.override {
-    overrides = hsconfig;
+    overrides = self: super: hsconfig self super // hsconfig2 self super;
   };
 
 
@@ -24,6 +37,9 @@ let
 
             lens
             taggy-lens
+
+            p.nlp-types
+            p.HCoreNLP
           ]);
 
 in
