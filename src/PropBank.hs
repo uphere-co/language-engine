f0121e33db005f9bca1d8f53d5c6ec5545c23005
Doh Young Mo <modohyoung@gmail.com>
2017-05-25 00:25:49 +0000

    API.Query to Query.

--- src/PropBank.hs
+++ src/PropBank.hs
@@ -5,12 +5,10 @@
 module PropBank
 (
   -- module PropBank.Parser
-  module PropBank.API.Query
-, module PropBank.Query
+  module PropBank.Query
 , module PropBank.Type
 ) where
 
 -- import PropBank.Parser
-import PropBank.API.Query
 import PropBank.Query
 import PropBank.Type
