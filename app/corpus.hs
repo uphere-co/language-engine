faf133b3876bb488862cc10900546514c9a72d31
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-25 18:55:58 +0000

    refactor out merge

--- app/corpus.hs
+++ app/corpus.hs
@@ -2,7 +2,9 @@
 
 module Main where
 
+import           Control.Applicative
 import           Control.Lens
+import           Control.Monad                     (forM_)
 import qualified Data.Attoparsec.Text         as A
 import           Data.Char
 import           Data.Discrimination               (leftOuter)
@@ -15,12 +17,21 @@ import qualified Data.Text.IO         as TIO
 import           System.FilePath                   ((</>))
 --
 import           NLP.Parser.PennTreebankII
+import           NLP.Type.PennTreebankII
 --
 import           PropBank.Parser.Prop
 import           PropBank.Type.Prop
 
-mainProp :: IO ()
-mainProp =  do
+merge :: (b -> Int) -> [a] -> [b] -> [(Int,(a,[b]))] 
+merge idxf trs props =
+   let itrs = (zip [0..] trs)
+       propgrps = (map (\x->(idxf (head x),x)) . groupBy ((==) `on` idxf)) props
+   in concat $ leftOuter grouping joiner m1 fst fst itrs propgrps
+ where joiner (i,x) (_,y) = (i,(x,y))
+       m1 (i,x) = (i,(x,[]))
+  
+main :: IO ()
+main =  do
   props <- parseProp <$> TIO.readFile "/scratch/wavewave/MASC/Propbank/Propbank-orig/data/written/chZ.prop"
   txt <- TIO.readFile "/scratch/wavewave/MASC/Propbank/Penn_Treebank-orig/data/written/chZ.mrg"
   let xs = T.lines txt
@@ -28,23 +39,36 @@ mainProp =  do
   case mapM (A.parseOnly penntree) txts of
     Left err -> print err
     Right trs -> do
-      let itrs = (zip [0..] trs)
-          propgrps = (map (\x->((head x)^.inst_tree_id,x)) . groupBy ((==) `on` (^.inst_tree_id))) props
-          lst = concat $ leftOuter grouping joiner m1 fst fst itrs propgrps
-            where joiner (i,x) (_,y) = (i,(x,y))
-                  m1 (i,x) = (i,(x,[]))
+      mapM_ showSentenceAnnotation (merge (^.inst_tree_id) trs props) -- lst
 
-      mapM_ showSentenceAnnotation lst
-
+{- 
 main :: IO ()
 main =  do
   -- props <- parseProp <$> TIO.readFile "/scratch/wavewave/MASC/Propbank/Propbank-orig/data/written/chZ.prop"
   let treedir = "/scratch/wavewave/Penn-tbank/MRG"
   nomprops <- parseNomProp <$> TIO.readFile "/scratch/wavewave/nombank.1.0/nombank.1.0"
 
+  let nomprop_grps = groupBy ((==) `on` (^.nominst_tree_file)) nomprops
+  print $ length (nomprop_grps)
+  forM_ (take 1 nomprop_grps) $ \xs@(p:ps) -> do
+    let fp = treedir </> map toUpper (p^.nominst_tree_file)
+    txt <- TIO.readFile fp
+    -- TIO.putStrLn txt
+    case (A.parseOnly (many (A.skipSpace *> penntree)) txt) of
+      Left err -> error err
+      Right trs ->
+        -- mapM_ print trs
+        -- print (groupBy ((==) `on` (^.inst_tree 
+
+-}
+  {- 
   let p = head nomprops
 
   let fp = treedir </> map toUpper (p^.nominst_tree_file)
-
-  print fp
-  print p
+  txt <- TIO.readFile fp
+  TIO.putStrLn txt
+  case (A.parseOnly (many (A.skipSpace *> penntree)) txt) of
+    Left err -> error err
+    Right trs ->
+      zmapM_ print trs
+  -}
