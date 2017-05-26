4d3ad887a7625bdc72d8f45dc624425bdf7dd0c1
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-26 07:36:28 +0000

    move merge to PropBank.Util

---
+++ src/PropBank/Util.hs
@@ -0,0 +1,15 @@
+module PropBank.Util where
+
+import           Data.Discrimination               (leftOuter)
+import           Data.Discrimination.Grouping      (grouping)
+import           Data.Function                     (on)
+import           Data.List                         (groupBy)
+
+merge :: (b -> Int) -> [a] -> [b] -> [(Int,(a,[b]))] 
+merge idxf trs props =
+   let itrs = (zip [0..] trs)
+       propgrps = (map (\x->(idxf (head x),x)) . groupBy ((==) `on` idxf)) props
+   in concat $ leftOuter grouping joiner m1 fst fst itrs propgrps
+ where joiner (i,x) (_,y) = (i,(x,y))
+       m1 (i,x) = (i,(x,[]))
+
