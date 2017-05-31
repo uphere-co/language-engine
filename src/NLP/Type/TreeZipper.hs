7cda03d8d09e7562d7c6a0a2a058a17302a4e6bd
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-31 04:08:25 +0000

    implement mkZipper and mkListZipper

--- src/NLP/Type/TreeZipper.hs
+++ src/NLP/Type/TreeZipper.hs
@@ -2,6 +2,8 @@
 
 module NLP.Type.TreeZipper where
 
+import           Data.List               (unfoldr)
+--
 import           NLP.Type.PennTreebankII
 
 type Tree = PennTreeGen 
@@ -14,6 +16,28 @@ data ZTerm c t = PL' | PN' [Tree c t]
 data TreeZipper c t = TZ (ZTerm c t) [(c,[Tree c t],[Tree c t])]
   deriving (Show)
 
+{- 
 mkRootZipper :: Tree c t -> TreeZipper c t
 mkRootZipper (PL x) = TZ PL' []
 mkRootZipper (PN x xs) = TZ (PN' xs) []
+-}
+ 
+mkZipper :: [(c,[Tree c t],[Tree c t])] -> Tree c t -> Tree (TreeZipper c t) (TreeZipper c t)
+mkZipper zs (PL x) = PL (TZ PL' zs)
+mkZipper zs (PN x xs) = PN (TZ (PN' xs) zs) (map (\(xs1,y,xs2) -> mkZipper ((x,xs1,xs2):zs) y) (mkListZipper xs))
+
+
+mkListZipper [] = error "cannot make a zipper for empty list"
+mkListZipper (k:ks) = unfoldr next ([],k,ks)
+  where next (xs,y,z:zs) = let w = (y:xs,z,zs) in Just (w,w)
+        next (xs,y,[]) = Nothing
+ 
+
+{- 
+mkListZipper xs =
+  where go ys [] = ys
+        go ys (x:xs) = 
+
+  error "cannot treat null"
+mkListZipper (x:xs) = x
+-}
--- test/zipper.hs
+++ test/zipper.hs
@@ -2,6 +2,10 @@
 
 module Main where
 
+-- import           Control.Monad.State
+import           Data.Bifoldable
+import           Data.List           (unfoldr)
+import           Data.Monoid
 import           Data.Text           (Text)
 import qualified Data.Text.IO as TIO
 --
@@ -19,4 +23,17 @@ tree = PN "VP" [PN "NP" [PL ("VB","a"), PL ("NN","b"), PL ("DD","c")], PL ("CC",
 main = do
   -- print tzipper
   TIO.putStrLn $ prettyPrint 0 tree
-  print (mkRootZipper tree)
+  -- print (mkRootZipper tree)
+
+  print (bifoldMap id (\(x,y) -> x<>y) tree)
+
+  print (mkZipper [] tree)
+
+  putStrLn "-----------"
+  bimapM_ print print (mkZipper [] tree)
+
+next (xs,y,z:zs) = let w = (y:xs,z,zs) in Just (w,w)
+next (xs,y,[]) = Nothing
+ 
+main2 = do
+  print $ unfoldr next ([],1,[2,3,4,5])
