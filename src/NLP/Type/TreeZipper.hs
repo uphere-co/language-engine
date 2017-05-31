5ea352628f1e8c7e7fbb125e8637cefeec4b75b1
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-31 04:40:14 +0000

    even further simplification (no need for ZTerm)

--- src/NLP/Type/TreeZipper.hs
+++ src/NLP/Type/TreeZipper.hs
@@ -5,15 +5,17 @@ module NLP.Type.TreeZipper where
 import           Data.List               (unfoldr)
 --
 import           NLP.Type.PennTreebankII
+--
+import Debug.Trace
 
 type Tree = PennTreeGen 
 
 -- | Zipper terminal
-data ZTerm c t = PL' | PN' [Tree c t]
-  deriving (Show)
+-- data ZTerm c t = PL' | PN' [Tree c t]
+--   deriving (Show)
 
 -- | Tree Zipper                 
-data TreeZipper c t = TZ (ZTerm c t) [(c,[Tree c t],[Tree c t])]
+data TreeZipper c t = TZ (Tree c t) [(c,[Tree c t],[Tree c t])]
   deriving (Show)
 
 {- 
@@ -22,22 +24,30 @@ mkRootZipper (PL x) = TZ PL' []
 mkRootZipper (PN x xs) = TZ (PN' xs) []
 -}
  
-mkZipper :: [(c,[Tree c t],[Tree c t])] -> Tree c t -> Tree (TreeZipper c t) (TreeZipper c t)
-mkZipper zs (PL x) = PL (TZ PL' zs)
-mkZipper zs (PN x xs) = PN (TZ (PN' xs) zs) (map (\(xs1,y,xs2) -> mkZipper ((x,xs1,xs2):zs) y) (mkListZipper xs))
+mkTreeZipper :: [(c,[Tree c t],[Tree c t])] -> Tree c t -> Tree (TreeZipper c t) (TreeZipper c t)
+mkTreeZipper zs p@(PL x) = PL (TZ p zs)
+mkTreeZipper zs p@(PN x xs) = PN (TZ p zs) lst
+  where  lst0 = mkListZipper xs
+         lst = map (\(xs1,y,xs2) -> mkTreeZipper ((x,xs1,xs2):zs) y) lst0  -- (mkListZipper xs))
 
 
+mkListZipper :: [a] -> [([a],a,[a])]
 mkListZipper [] = error "cannot make a zipper for empty list"
-mkListZipper (k:ks) = unfoldr next ([],k,ks)
-  where next (xs,y,z:zs) = let w = (y:xs,z,zs) in Just (w,w)
-        next (xs,y,[]) = Nothing
- 
+mkListZipper (k:ks) = ([],k,ks) : unfoldr next ([],k,ks)
+  where next (xs,y,[]) = Nothing
+        next (xs,y,z:zs) = let w = (y:xs,z,zs) in Just (w,w)
 
 {- 
-mkListZipper xs =
-  where go ys [] = ys
-        go ys (x:xs) = 
+current :: TreeZipper c t -> Tree c t
+current 
 
-  error "cannot treat null"
-mkListZipper (x:xs) = x
+
+prevSibling :: TreeZipper c t -> Maybe (TreeZipper c t)
+
+
+
+nextSibling :: TreeZipper c t -> Maybe (TreeZipper c t)
+
+parent :: TreeZipper c t -> Maybe (TreeZipper c t)
+
 -}
--- test/zipper.hs
+++ test/zipper.hs
@@ -13,27 +13,21 @@ import           NLP.Printer.PennTreebankII
 import           NLP.Type.PennTreebankII
 import           NLP.Type.TreeZipper
 
+{- 
 tzipper :: TreeZipper Text Text
 tzipper = TZ (PL') [("abc",[PL "test"],[PL "blah"])] 
+-}
 
 tree :: Tree Text (Text,Text)
 tree = PN "VP" [PN "NP" [PL ("VB","a"), PL ("NN","b"), PL ("DD","c")], PL ("CC","d")]
 
 
 main = do
-  -- print tzipper
   TIO.putStrLn $ prettyPrint 0 tree
-  -- print (mkRootZipper tree)
-
   print (bifoldMap id (\(x,y) -> x<>y) tree)
-
-  print (mkZipper [] tree)
-
   putStrLn "-----------"
-  bimapM_ print print (mkZipper [] tree)
+  bimapM_ print print (mkTreeZipper [] tree)
 
-next (xs,y,z:zs) = let w = (y:xs,z,zs) in Just (w,w)
-next (xs,y,[]) = Nothing
  
 main2 = do
-  print $ unfoldr next ([],1,[2,3,4,5])
+  print $ mkListZipper [1,2,3]
