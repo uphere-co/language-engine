4ea42c3db6e9b48545018470c62cdc0b3c5ce2bc
Doh Young Mo <modohyoung@gmail.com>
2017-06-01 22:54:49 +0000

    c_get_vector is added.

--- fasttext/testFastText.hs
+++ fasttext/testFastText.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 module Main where
 
+import Data.Vector.Storable
+import Foreign.ForeignPtr
 import Foreign.C.String
+import Foreign.Ptr
+import Foreign.C.Types
 --
 -- import FastText.Binding.CppString
 -- import FastText.Binding.FastText
 import FastText.Binding
+
+foreign import ccall "get_vector" c_get_fasttextvector :: FastTextVector -> IO (Ptr CFloat)
 
 main = do
   let binfile = "/scratch/wavewave/wordvector/wiki.en.bin"
@@ -14,8 +22,13 @@ main = do
       v <- newFastTextVector 300
       str_bin <- newCppString cstr_bin
       str_word <- newCppString cstr_word
-      
       fastTextloadModel t str_bin
       -- fastTextprintWordVectors t
       fastTextgetVector t v str_word
       print =<< fastTextVectornorm v
+      cfloat <- c_get_fasttextvector v
+      ptr <- newForeignPtr_ cfloat
+      let mv = MVector 30000 ptr
+      let v = create (return mv)
+      
+      print v
