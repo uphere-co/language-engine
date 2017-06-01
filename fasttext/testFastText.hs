5c1d727d42cf2a6a512a3900e105bae9a342f8ec
Doh Young Mo <modohyoung@gmail.com>
2017-06-01 22:14:20 +0000

    vector print test in testFastText.

--- fasttext/testFastText.hs
+++ fasttext/testFastText.hs
@@ -2,13 +2,20 @@ module Main where
 
 import Foreign.C.String
 --
-import FastText.Binding.CppString
-import FastText.Binding.FastText
+-- import FastText.Binding.CppString
+-- import FastText.Binding.FastText
+import FastText.Binding
 
 main = do
   let binfile = "/scratch/wavewave/wordvector/wiki.en.bin"
   withCString binfile $ \cstr_bin -> do
-    t <- newFastText
-    str_bin <- newCppString cstr_bin
-    fastTextloadModel t str_bin
-    fastTextprintWordVectors t
+    withCString "vector" $ \cstr_word -> do
+      t <- newFastText
+      v <- newFastTextVector 300
+      str_bin <- newCppString cstr_bin
+      str_word <- newCppString cstr_word
+      
+      fastTextloadModel t str_bin
+      -- fastTextprintWordVectors t
+      fastTextgetVector t v str_word
+      print =<< fastTextVectornorm v
