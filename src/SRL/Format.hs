604f11b8d592ef7be8de8a7a99135d4d4a7d3f25
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-14 03:33:17 +0000

    add DepRelPath to SRLFeature

--- src/SRL/Format.hs
+++ src/SRL/Format.hs
@@ -38,9 +38,10 @@ formatDRP (LZ tostart root totarget) = foldMap f lst
 
 
 formatArgNodeFeature :: Int -> ArgNodeFeature -> String
-formatArgNodeFeature predidx (AFeat label (SRLFeat rng ptp mhead)) =
-    printf "%10s %10s %6s %30s %5s %s" (T.unpack (pbLabelText label)) (show rng) (show (position predidx rng))
-                                       (formatPTP ptp) (w^._1) (w^._2)
+formatArgNodeFeature predidx (AFeat label (SRLFeat rng ptp drpath mhead)) =
+    printf "%10s %10s %6s %30s %30s %5s %s" (T.unpack (pbLabelText label)) (show rng) (show (position predidx rng))
+                                            (formatPTP ptp) (formatDRP drpath)
+                                            (w^._1) (w^._2)
   where
     w = hstr mhead
     hstr Nothing = ("","")
