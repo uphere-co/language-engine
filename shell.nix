99ac0c2eb68e5da9d28d6de048ac9055383a25a7
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-17 04:28:34 +0000

    copied code from search-idiom

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 { pkgs ? import <nixpkgs> {}
 , uphere-nix-overlay ? <uphere-nix-overlay>
+, textview ? <textview>
 }:
 
 with pkgs;
@@ -7,12 +8,21 @@ with pkgs;
 let
   hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix")
                { inherit pkgs; };
+  config2 =
+    self: super: {
+      "textview" = self.callPackage (import textview) {};
+    };
   newHaskellPackages = haskellPackages.override {
-    overrides = hsconfig;
+    overrides = self: super: hsconfig self super // config2 self super;
   };
 
 
   hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
+            attoparsec
+            containers
+            haskeline
+            monad-loops
+            p.textview
             cabal-install
           ]);
 
