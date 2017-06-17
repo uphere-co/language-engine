062a7a299f5c2986f14c8001446559f34ee6771b
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-17 04:42:07 +0000

    copy loadIdiom from textview lib

--- src/MWE.hs
+++ src/MWE.hs
@@ -1,14 +1,28 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module MWE where
 
-import           Control.Applicative          (many)
+import           Control.Applicative               (many)
 --
-import           Control.Monad.Trans.Either (EitherT(..))
+import           Control.Monad.Trans.Either        (EitherT(..))
 import           Control.Monad.Trans.State
--- import           Control.Monad.State.Lazy
+import qualified Data.Text                  as T
+import qualified Data.Text.IO               as TIO
+import           Data.Tree                         (Forest)
 --
-import           Generic.SearchTree
+import           Generic.SearchTree                (addTreeItem)
 -- import           SearchTree
 import           ParserCustom
+
+loadIdiom :: FilePath -> IO (Forest (Maybe String))
+loadIdiom fp = do
+  txt <- TIO.readFile fp 
+  let (lst :: [[String]]) = map (read . T.unpack) $ drop 1 $ T.lines $ txt
+      idiomsent = map head lst
+      nentities = map words idiomsent
+      forest = foldr addTreeItem [] nentities
+  return forest
+
 
 main :: IO ()
 main = do
