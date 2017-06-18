84e08b77b7558bcdef04beaede1fa593017aba8a
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-18 05:13:55 +0000

    made query interface for lexunit. query by name.

--- src/FrameNet/Query/LexUnit.hs
+++ src/FrameNet/Query/LexUnit.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
@@ -6,10 +7,18 @@ module FrameNet.Query.LexUnit where
 import           Control.Concurrent.Async     (async,wait)
 import           Control.Lens
 import           Control.Monad                (when)
+import           Control.Monad.IO.Class       (liftIO)
+import           Control.Monad.Loops          (whileJust_)
+import           Data.HashMap.Strict          (HashMap)
+import qualified Data.HashMap.Strict  as HM
 import           Data.IntMap                  (IntMap)
 import qualified Data.IntMap          as IM
 import           Data.List                    (foldl',sort)
+import           Data.Text                    (Text)
+import qualified Data.Text            as T
 import qualified Data.Text.Lazy.IO    as TLIO
+import           Data.Text.Read
+import           System.Console.Haskeline
 import           System.Directory             (getDirectoryContents)
 import           System.FilePath              ((</>),takeExtensions)
 import           Text.Taggy.Lens
@@ -19,14 +28,16 @@ import           FrameNet.Type.Common
 import           FrameNet.Type.LexUnit
 
 
-newtype LexUnitDB = LexUnitDB { _lexunitDB :: IntMap LexUnit } 
+data LexUnitDB = LexUnitDB { _lexunitDB :: IntMap LexUnit
+                           , _nameIDmap :: HashMap Text Int
+                           } 
 
 
 makeLenses ''LexUnitDB
 
 
 emptyDB :: LexUnitDB
-emptyDB = LexUnitDB IM.empty
+emptyDB = LexUnitDB IM.empty HM.empty
 
 
 parseLUFile :: FilePath -> IO LexUnit
@@ -36,6 +47,13 @@ parseLUFile fp = do
   case p_lexUnit lu of
     Nothing -> error fp
     Just x -> return x
+
+
+insertLU :: LexUnitDB -> LexUnit -> LexUnitDB
+insertLU !db lu =
+  let i = lu^.lexunit_basicLUAttributes.bluattr_ID
+      n = lu^.lexunit_basicLUAttributes.bluattr_name
+  in  (lexunitDB %~ IM.insert i lu) . (nameIDmap %~ HM.insert n i) $ db 
 
 
 loadLUData :: FilePath -> IO LexUnitDB
@@ -48,10 +66,25 @@ loadLUData dir = do
     async (parseLUFile fp)
   xs <- mapM wait as
     
-  let lumap = foldl' (\m x -> (lexunitDB %~ IM.insert (x^.lexunit_basicLUAttributes.bluattr_ID) x) m) emptyDB xs
+  let lumap = foldl' insertLU emptyDB xs
+
   return lumap 
 
 
 queryLU :: LexUnitDB -> IO ()
 queryLU db = do
-  print $ IM.lookup 16412 (db^.lexunitDB)
+  -- print $ IM.lookup 16412 (db^.lexunitDB)
+  runInputT defaultSettings $ whileJust_ (getInputLine "% ") $ \input' -> liftIO $ do
+    let input = T.pack input'
+        mlu = HM.lookup input (db^.nameIDmap) >>= \i -> IM.lookup i (db^.lexunitDB)
+    case mlu of
+      Nothing -> putStrLn "no such lexical unit"
+      Just lu -> print lu
+        
+    {- 
+     let einput = decimal (T.pack input')
+     case einput of
+       Left err -> print err
+       Right (input,_) -> do
+         let mlu = IM.lookup input (db^.lexunitDB)
+    -}
