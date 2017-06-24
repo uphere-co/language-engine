a0404d20f71587f90a1fb6ca5cb6bb33368289e3
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-24 21:36:34 +0000

    remove warnings and redundant functions

--- src/WordNet/API/Query.hs
+++ src/WordNet/API/Query.hs
@@ -17,24 +17,26 @@ import           NLP.Type.WordNet
 
 loadDB :: FilePath -> IO WordNetDB
 loadDB fp = do
-  is@(inoun,iverb,iadj,iadv) <-
-    (,,,) <$> (catMaybes <$> parseFile parseIndex (fp </> "index.noun"))
-          <*> (catMaybes <$> parseFile parseIndex (fp </> "index.verb"))
-          <*> (catMaybes <$> parseFile parseIndex (fp </> "index.adj"))
-          <*> (catMaybes <$> parseFile parseIndex (fp </> "index.adv"))
-  ds@(dnoun,dverb,dadj,dadv) <-
-    (,,,) <$> (catMaybes <$> parseFile (parseData False) (fp </> "data.noun"))
-          <*> (catMaybes <$> parseFile (parseData True ) (fp </> "data.verb"))
-          <*> (catMaybes <$> parseFile (parseData False) (fp </> "data.adj"))
-          <*> (catMaybes <$> parseFile (parseData False) (fp </> "data.adv"))
+  is <- (,,,) <$> (catMaybes <$> parseFile parseIndex (fp </> "index.noun"))
+              <*> (catMaybes <$> parseFile parseIndex (fp </> "index.verb"))
+              <*> (catMaybes <$> parseFile parseIndex (fp </> "index.adj"))
+              <*> (catMaybes <$> parseFile parseIndex (fp </> "index.adv"))
+  ds <- (,,,) <$> (catMaybes <$> parseFile (parseData False) (fp </> "data.noun"))
+              <*> (catMaybes <$> parseFile (parseData True ) (fp </> "data.verb"))
+              <*> (catMaybes <$> parseFile (parseData False) (fp </> "data.adj"))
+              <*> (catMaybes <$> parseFile (parseData False) (fp </> "data.adv"))
   ss <- (catMaybes <$> parseFile parseSense (fp </> "index.sense"))
   return (createWordNetDB is ds ss)
 
+
+runSingleQuery :: String -> POS -> WordNetDB -> IO ()
 runSingleQuery input typ db = do
   case decimal (T.pack input) of
-    Left str    -> queryLemma (T.pack input) typ db
+    Left _str    -> queryLemma (T.pack input) typ db
     Right (n,_) -> queryConcept n typ db
 
+
+queryLemma :: Text -> POS -> WordNetDB -> IO ()
 queryLemma input typ db = do
   case typ of
     POS_N -> putStrLn "-- Noun --" >> (mapM_ (TIO.putStrLn . format) $ lookupLemma db POS_N input)
@@ -42,6 +44,8 @@ queryLemma input typ db = do
     POS_A -> putStrLn "-- Adjective --" >> (mapM_ (TIO.putStrLn . format) $ lookupLemma db POS_A input)
     POS_R -> putStrLn "-- Adverb --" >> (mapM_ (TIO.putStrLn . format) $ lookupLemma db POS_R input)
 
+
+queryConcept :: Int -> POS -> WordNetDB -> IO ()
 queryConcept n typ db = do
   case typ of
     POS_N -> putStrLn "-- Noun --" >> (mapM_ (TIO.putStrLn . format) $ lookupConcept db POS_N n)
@@ -49,6 +53,8 @@ queryConcept n typ db = do
     POS_A -> putStrLn "-- Adjective --" >> (mapM_ (TIO.putStrLn . format) $ lookupConcept db POS_A n)
     POS_R -> putStrLn "-- Adverb --" >> (mapM_ (TIO.putStrLn . format) $ lookupConcept db POS_R n)
 
+
+getQueryLemma :: Text -> POS -> WordNetDB -> [Text]
 getQueryLemma input typ db = do
   case typ of
     POS_N -> fmap format $ lookupLemma db POS_N input
@@ -56,6 +62,8 @@ getQueryLemma input typ db = do
     POS_A -> fmap format $ lookupLemma db POS_A input
     POS_R -> fmap format $ lookupLemma db POS_R input
 
+
+getQueryConcept :: Int -> POS -> WordNetDB -> Maybe ([LexItem],Text)
 getQueryConcept n typ db = do
   case typ of
     POS_N -> lookupConcept db POS_N n
@@ -63,7 +71,8 @@ getQueryConcept n typ db = do
     POS_A -> lookupConcept db POS_A n
     POS_R -> lookupConcept db POS_R n
 
-getQuerySense t i db = lookupSense db t i
+
+-- getQuerySense t i db = lookupSense db t i
 
 format :: ([LexItem],Text) -> Text
 format (xs,txt) = T.intercalate "," (map formatLI xs) <> " | " <> txt
