360015ad1547d8a78de82c38b8ccbb842de63ab3
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-24 23:37:35 +0000

    add explicit Enum and Bounded

--- src/NLP/Type/WordNet.hs
+++ src/NLP/Type/WordNet.hs
@@ -3,4 +3,21 @@ module NLP.Type.WordNet where
 data POS = POS_N | POS_V | POS_A | POS_R deriving (Show,Eq,Ord)
 
 data SSType = Noun | Verb | Adjective | Adverb | AdjectiveSatellite
-            deriving (Show,Eq,Ord,Enum)
+            deriving (Show,Eq,Ord)
+
+instance Enum SSType where
+  toEnum 1 = Noun
+  toEnum 2 = Verb
+  toEnum 3 = Adjective
+  toEnum 4 = Adverb
+  toEnum 5 = AdjectiveSatellite
+
+  fromEnum Noun               = 1
+  fromEnum Verb               = 2
+  fromEnum Adjective          = 3
+  fromEnum Adverb             = 4
+  fromEnum AdjectiveSatellite = 5
+
+instance Bounded SSType where
+  minBound = Noun
+  maxBound = AdjectiveSatellite
