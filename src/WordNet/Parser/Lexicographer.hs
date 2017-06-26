70c4e6e661fd215cd53cada63c1b73ee31c869cf
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-26 18:05:01 +0000

    bug fix in p_synset_verb. remove warnings by adding type signatures

--- src/WordNet/Parser/Lexicographer.hs
+++ src/WordNet/Parser/Lexicographer.hs
@@ -6,11 +6,10 @@
 
 module WordNet.Parser.Lexicographer where
 
-import           Control.Applicative             (many,optional,pure
-                                                 ,(<$>),(<*>),(<*),(*>),(<|>))
+import           Control.Applicative
 import           Control.Monad
 import           Data.Attoparsec.Text
-import           Data.Char                       (digitToInt,isAlpha,isDigit)
+import           Data.Char                       (isAlpha,isDigit)
 import           Data.List                       (foldl1')
 import           Data.Maybe                      (fromMaybe)
 import           Data.Monoid
@@ -19,10 +18,8 @@ import qualified Data.Text                  as T
 -- import qualified Data.Text.Read             as TR
 --
 import           NLP.Type.WordNet
-import           WordNet.Type
 import           WordNet.Type.Lexicographer
---
-import qualified Data.Text.IO as TIO
+
 
 pointerSymbol_table :: [ (PointerSymbol, Text) ]
 pointerSymbol_table 
@@ -54,20 +51,36 @@ pointerSymbol_table
     , (MemberOfThisDomain_USAGE , "-u")
     ]
 
-p_comment = char '(' >> manyTill anyChar endOfLine
 
+p_comment :: Parser ()
+p_comment = char '(' >> void (manyTill anyChar endOfLine)
+
+
+p_empty :: Parser ()
 p_empty = skipWhile (\c -> c == ' ' || c == '\t') >> endOfLine
 
+
+p_skipEmptyLine :: Parser ()
 p_skipEmptyLine = skipWhile (\c -> c == ' ' || c == '\n')
 
+
+p_escapednumber :: Parser Text
 p_escapednumber = takeWhile1 isDigit <* char '"'
 
+
+isLetter :: Char -> Bool
 isLetter c = inClass "a-zA-Z" c || c `elem` ['-','.','\'','/']
 
+
+p_letters :: Parser Text
 p_letters = takeWhile1 isLetter
 
+
+p_nonlasttoken :: Parser Text
 p_nonlasttoken = T.concat <$> many (p_letters <|> p_escapednumber <|> takeWhile1 isDigit)
 
+
+p_token :: Parser Text
 p_token = do
   xs <- many $ do txt <- p_escapednumber <|>
                          (do num <- Data.Attoparsec.Text.takeWhile1 isDigit
@@ -77,9 +90,12 @@ p_token = do
                   return txt
   return $ T.concat xs
 
+
+p_lexfile :: Parser (Either Text LexicographerFile)
 p_lexfile = foldl1' (<|>) (map (\(x,y) -> string x *> pure y) lexicographerFileTable)
 
 
+p_word_lexid :: Parser ([Text],Maybe Int)
 p_word_lexid = do
   ws <- do ws' <- many (p_nonlasttoken <* char '_')
            w <- p_token
@@ -87,6 +103,8 @@ p_word_lexid = do
   md :: Maybe Int <- optional (read <$> many1 digit)
   return (ws,md)
 
+
+p_word_marker_lexid :: Parser ([Text],Maybe Marker,Maybe Int)
 p_word_marker_lexid = do
   ws <- do ws' <- many (p_nonlasttoken <* char '_')
            w <- p_token
@@ -98,12 +116,15 @@ p_word_marker_lexid = do
   md :: Maybe Int <- optional (read <$> many1 digit)
   return (ws,mk,md)
 
+
+p_word :: Parser SSWord
 p_word = do
   (ws,mk,md) <- p_word_marker_lexid
   char ','
   c <- peekChar'
   guard (c == ' ' || isAlpha c ) -- this is due to verb.motion:body-surf
   return (SSWord ws mk md)
+
 
 p_pointer :: Parser SSPointer
 p_pointer = do
@@ -115,6 +136,8 @@ p_pointer = do
   s <-p_pointer_symbol
   return (SSPointer lexfile ws md msatellite s)
 
+
+p_frames :: Parser [Int]
 p_frames = do
   string "frames:"
   skipSpace 
@@ -122,6 +145,8 @@ p_frames = do
   -- TR.decimal
   return xs
 
+
+p_wordpointer :: Bool -> Parser (SSWord,[SSPointer],[Int])
 p_wordpointer isVerb = do
   char '['
   skipSpace
@@ -140,6 +165,7 @@ p_pointer_symbol :: Parser PointerSymbol
 p_pointer_symbol = foldl1' (<|>) (map (\(x,y)-> string y *> return x) pointerSymbol_table)
 
 
+p_synset_noun_adv :: Parser Synset
 p_synset_noun_adv = do
   char '{'
   wps <- many1 (skipSpace *> (fmap Left p_word <|> fmap Right (p_wordpointer False)))
@@ -151,6 +177,8 @@ p_synset_noun_adv = do
   manyTill anyChar endOfLine
   return (Synset wps ps [] (T.pack gloss'))
 
+
+p_synset_verb :: Parser Synset
 p_synset_verb = do
   char '{'
   wps <- many1 (skipSpace *> (fmap Left p_word <|> fmap Right (p_wordpointer True)))
@@ -168,10 +196,10 @@ p_synset_verb = do
   char '('
   gloss' <- manyTill anyChar (char ')' >> skipSpace >> char '}')
   manyTill anyChar endOfLine
-  return (Synset wps ps fs (T.pack gloss'))
+  return (Synset wps (ps++ps') (fs++fromMaybe [] fs') (T.pack gloss'))
 
   
-
+p_synset_adjective :: Parser Synset
 p_synset_adjective = do
   char '{'
   wps <- many1 (skipSpace *> (fmap Left p_word <|> fmap Right (p_wordpointer False)))
@@ -191,4 +219,4 @@ p_synset t = (Just <$> p) <|> (p_comment *> return Nothing) <|> (p_empty *> retu
               Verb      -> p_synset_verb
               Adjective -> p_synset_adjective
               Adverb    -> p_synset_noun_adv
-  
+              x         -> error ("p_synset: I do not know how to deal with " ++ show x)
