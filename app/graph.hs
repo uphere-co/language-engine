9223410cc66d48e15f7f53b1199721d519183988
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-27 19:58:00 +0000

    process noun, verb, adverb

--- app/graph.hs
+++ app/graph.hs
@@ -19,28 +19,14 @@ import           WordNet.Type.Lexicographer
 
 data SynsetDBFull
   = SynsetDBFull { _synsetdbfull_noun      :: HashMap Text (HashMap Int [SSWord],HashMap SSWord Int)
-                                              
-
-{-
-                 , _synsetdbfull_verb      :: [(Text,[Synset])]
-                 , _synsetdbfull_adverb    :: [(Text,[Synset])]
-                 , _synsetdbfull_adjective :: [(Text,[Either Synset SynsetCluster])] -}
+                 , _synsetdbfull_verb      :: HashMap Text (HashMap Int [SSWord],HashMap SSWord Int)
+                 , _synsetdbfull_adverb    :: HashMap Text (HashMap Int [SSWord],HashMap SSWord Int)
+                 {- , _synsetdbfull_adjective :: [(Text,[Either Synset SynsetCluster])] -}
                  }
 
 
 makeLenses ''SynsetDBFull
 
--- mkidx =  (\x -> (map formatWord . getSSWords) x) 
-
-{-
-newtype SenseKey = SenseKey (LexicographerFile,SSWord)
-                 deriving (Show)
-
-
-filekey t = L.lookup t (map swap lexicographerFileTable)
-
-mkSenseKey
--}
 
 mkSSWord :: Text -> Maybe SSWord
 mkSSWord txt = 
@@ -49,19 +35,25 @@ mkSSWord txt =
     Right w  -> Just w
 
 
-mkIndexDB :: SynsetDB -> Maybe SynsetDBFull
-mkIndexDB db =
-  let ks = map fst (db^.synsetdb_noun)
-      sidx2word = map (\(fname,xs) -> (fname,zip [1..] (map getSSWords xs))) (db^.synsetdb_noun)
+mkIndexDB :: [(Text,[Synset])] -> Maybe (HashMap Text (HashMap Int [SSWord],HashMap SSWord Int))
+mkIndexDB lst =
+  let ks = map fst lst
+      sidx2word = map (\(fname,xs) -> (fname,zip [1..] (map getSSWords xs))) lst
       word2sidx = map (\(fname,xs) -> (fname,f xs)) sidx2word
         where f :: [(Int,[SSWord])] -> [(SSWord,Int)]
               f xs = [(w,i) | (i,ws) <-xs, w <- ws ]
       nounmap_s2w = fmap HM.fromList (HM.fromList sidx2word)
       nounmap_w2s = fmap HM.fromList (HM.fromList word2sidx)
-  in fmap (SynsetDBFull . HM.fromList) . flip mapM ks $ \k -> do
+  in fmap HM.fromList . flip mapM ks $ \k -> do
        s2w <- HM.lookup k nounmap_s2w
        w2s <- HM.lookup k nounmap_w2s
        return (k,(s2w,w2s))
+
+
+mkSynsetDBFull :: SynsetDB -> Maybe SynsetDBFull
+mkSynsetDBFull db = SynsetDBFull <$> mkIndexDB (db^.synsetdb_noun)
+                                 <*> mkIndexDB (db^.synsetdb_verb)
+                                 <*> mkIndexDB (db^.synsetdb_adverb)
 
 
 findSynonym db txt  = do
@@ -72,30 +64,12 @@ findSynonym db txt  = do
   return (map formatWord ws)
 
 
-
 main :: IO ()
 main = do
   let fp = "wordnet31hs.bin"
   lbstr <- BL.readFile fp
   let db = decode lbstr :: SynsetDB
-      Just dbfull = mkIndexDB db
+      Just dbfull = mkSynsetDBFull db
   mapM_ print (findSynonym dbfull "ruggelach")
   mapM_ print (findSynonym dbfull "soul_food")
   mapM_ print (findSynonym dbfull "coffee")
-{-                   
-  case HM.lookup "noun.food" nounmap_w2s of
-    Nothing -> error "Nothing"
-    Just xs -> do
-  -}                     
-                      
---      mapM_ print (mkSSWord "soul_food" >>= \w -> HM.lookup w xs)
-        
-{-
-
-  case HM.lookup "noun.food" nounmap_s2w of
-    Nothing -> error "Nothing"
-    Just xs -> mapM_ print (L.take 10 (reverse xs))
-
-      mapM_ print (take 10 (reverse xs))
-  -}
-
