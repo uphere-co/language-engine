1a25050de05f57ac52fe29d44be796d9eb2b1946
Ian-Woo Kim <ianwookim@gmail.com>
2017-06-27 19:39:07 +0000

    find up to synonym

--- app/graph.hs
+++ app/graph.hs
@@ -10,6 +10,7 @@ import           Data.HashMap.Strict              (HashMap)
 import qualified Data.HashMap.Strict        as HM
 import qualified Data.List                  as L
 import           Data.Monoid
+import           Data.Text                        (Text)
 --
 import           WordNet.Parser.Lexicographer
 import           WordNet.Query.SynsetDB
@@ -28,32 +29,53 @@ filekey t = L.lookup t (map swap lexicographerFileTable)
 mkSenseKey
 -}
 
+mkSSWord :: Text -> Maybe SSWord
+mkSSWord txt = 
+  case parseOnly (fmap (\(w,d,k)->SSWord w d k) p_word_lexid_marker) txt of
+    Left err -> Nothing
+    Right w  -> Just w
+
+mkIndexDB db =
+  let sidx2word = map (\(fname,xs) -> (fname,zip [1..] (map getSSWords xs))) (db^.synsetdb_noun)
+      word2sidx = map (\(fname,xs) -> (fname,f xs)) sidx2word
+        where f :: [(Int,[SSWord])] -> [(SSWord,Int)]
+              f xs = [(w,i) | (i,ws) <-xs, w <- ws ]
+      nounmap_s2w = fmap HM.fromList (HM.fromList sidx2word)
+      nounmap_w2s = fmap HM.fromList (HM.fromList word2sidx)
+  in (nounmap_s2w,nounmap_w2s)
+
+findSynonym (nounmap_s2w,nounmap_w2s) txt  = do
+  foodmap_w2s <- HM.lookup "noun.food" nounmap_w2s
+  foodmap_s2w <- HM.lookup "noun.food" nounmap_s2w
+  w <- mkSSWord txt
+  i <- HM.lookup w foodmap_w2s
+  ws <- HM.lookup i foodmap_s2w
+  return (map formatWord ws)
+
+
 
 main :: IO ()
 main = do
   let fp = "wordnet31hs.bin"
   lbstr <- BL.readFile fp
   let db = decode lbstr :: SynsetDB
-      sidx2word = map (\(fname,xs) -> (fname,zip [1..] (map getSSWords xs))) (db^.synsetdb_noun)
-      word2sidx = map (\(fname,xs) -> (fname,HM.fromList (f xs))) sidx2word
-        where f :: [(Int,[SSWord])] -> [(SSWord,Int)]
-              f xs = [(w,i) | (i,ws) <-xs, w <- ws ]
-      nounmap_s2w = HM.fromList sidx2word
-      nounmap_w2s = HM.fromList word2sidx
-  case HM.lookup "noun.food" nounmap_s2w of
-    Nothing -> error "Nothing"
-    Just xs -> mapM_ print (L.take 10 (reverse xs))
+      ms@(nounmap_s2w,nounmap_w2s) = mkIndexDB db
+  mapM_ print (findSynonym ms "ruggelach")
+  mapM_ print (findSynonym ms "soul_food")
+  mapM_ print (findSynonym ms "coffee")
+{-                   
   case HM.lookup "noun.food" nounmap_w2s of
     Nothing -> error "Nothing"
     Just xs -> do
-      case parseOnly (fmap (\(w,d,k)->SSWord w d k) p_word_lexid_marker) "ruggelach" of
-        Left err -> error err
-        Right w -> mapM_ print (HM.lookup w xs)
-      case parseOnly (fmap (\(w,d,k)->SSWord w d k) p_word_lexid_marker) "soul_food" of
-        Left err -> error err
-        Right w -> mapM_ print (HM.lookup w xs)
+  -}                     
+                      
+--      mapM_ print (mkSSWord "soul_food" >>= \w -> HM.lookup w xs)
         
 {-
+
+  case HM.lookup "noun.food" nounmap_s2w of
+    Nothing -> error "Nothing"
+    Just xs -> mapM_ print (L.take 10 (reverse xs))
 
       mapM_ print (take 10 (reverse xs))
   -}
