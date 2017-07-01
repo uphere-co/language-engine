edfe44ce676e00ebec06701ad9053b51e1d4f663
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-01 05:17:43 +0000

    import Text.Taggy.Lens.Util

--- src/OntoNotes/Parser/SenseInventory.hs
+++ src/OntoNotes/Parser/SenseInventory.hs
@@ -13,36 +13,8 @@ import           Data.Traversable          (traverse)
 import           Text.Taggy.Lens
 import           System.Directory
 import           System.FilePath
-
-
-class FromAttr a where
-  fromAttr :: Element -> Text -> Either String a
-
-instance FromAttr Text where
-  fromAttr x n = case x ^. attr n of
-                   Nothing -> Left (T.unpack n)
-                   Just r  -> Right r
-
-{- 
-instance FromAttr a => FromAttr (Maybe a) where
-  fromAttr x n = either (\_ -> Right Nothing) (Right . Just) (fromAttr x n :: Either String a)
--}
-
-(.:) :: FromAttr a => Element -> Text -> Either String a
-(.:) = fromAttr 
-
-
-getOnly :: Element -> Text -> [Element] 
-getOnly x k = x ^.. elements . named (only k)
-
-
-getOnly1 :: Element -> Text -> Parser Element
-getOnly1 x k = case getOnly x k of
-                 [] -> Left (T.unpack k)
-                 (x':_) -> Right x'
-
-
-type Parser = Either String
+--
+import           Text.Taggy.Lens.Util
 
 data WordMeta = WordMeta { _word_meta_authors :: Text
                          , _word_meta_sample_score :: Maybe Text
