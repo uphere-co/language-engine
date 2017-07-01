01ef13f3f4404226fc5c350f89396fa34dd83b8c
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-01 05:28:08 +0000

    Change according to D1277

--- src/VerbNet.hs
+++ src/VerbNet.hs
@@ -15,23 +15,8 @@ import           Data.Traversable          (traverse)
 import           Text.Taggy.Lens
 import           System.Directory
 import           System.FilePath
-
-
-class FromAttr a where
-  fromAttr :: Element -> Text -> Either String a
-
-instance FromAttr Text where
-  fromAttr x n = case x ^. attr n of
-                   Nothing -> Left (T.unpack n)
-                   Just r  -> Right r
-
-{- 
-instance FromAttr a => FromAttr (Maybe a) where
-  fromAttr x n = either (\_ -> Right Nothing) (Right . Just) (fromAttr x n :: Either String a)
--}
-
-(.:) :: FromAttr a => Element -> Text -> Either String a
-(.:) = fromAttr 
+--
+import           Text.Taggy.Lens.Util
 
 
 data Description = Description { _desc_primary           :: Text
@@ -158,19 +143,8 @@ data VNClass = VNClass { _vnclass_members    :: [Member]
 makeLenses ''VNClass                        
 
 
-getOnly :: Element -> Text -> [Element] 
-getOnly x k = x ^.. elements . named (only k)
-
-getOnly1 :: Element -> Text -> Parser Element
-getOnly1 x k = case getOnly x k of
-                 [] -> Left (T.unpack k)
-                 (x':_) -> Right x'
 
 
-type Parser = Either String
-
-
-p_list p each group x = getOnly1 x group >>= \y -> traverse p (getOnly y each) 
 
 p_member :: Element -> Parser Member
 p_member x = Member <$> x .: "name"
