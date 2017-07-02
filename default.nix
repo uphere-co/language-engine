a5c103806ae23420f67fe2d05f92b7281f80e083
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-02 13:25:46 +0000

    FromJSON, ToJSON and Binary instance for Bitree

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, aeson, base, bifunctors, discrimination, lens, stdenv, taggy-lens, text }:
+{ mkDerivation, aeson, base, bifunctors, binary, discrimination, lens, stdenv, taggy-lens, text }:
 mkDerivation {
   pname = "nlp-types";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ aeson base bifunctors discrimination lens taggy-lens text ];
+  libraryHaskellDepends = [ aeson base bifunctors binary discrimination lens taggy-lens text ];
   license = "unknown";
 }
--- src/Data/Bitree.hs
+++ src/Data/Bitree.hs
@@ -1,23 +1,28 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveTraversable #-}
+
 
 module Data.Bitree where
 
 import Control.Applicative
+import Data.Aeson
 import Data.Bifoldable
 import Data.Bifunctor
+import Data.Binary
 import Data.Bitraversable
 import Data.Foldable
 import Data.Monoid
 import Data.Traversable
+import GHC.Generics
 
 -- | chunk = chunktag, token = token in node. 
 --   typically token will be (pos = postag, a = content)
 
 data Bitree n l = PN n [Bitree n l]
                 | PL l
-                deriving (Show, Eq, Functor, Foldable, Traversable)
+                deriving (Show, Eq, Functor, Foldable, Traversable, Generic)
 
 instance Bifunctor Bitree where
   bimap f g (PN x xs) = PN (f x) (map (bimap f g) xs)
@@ -31,6 +36,13 @@ instance Bitraversable Bitree where
   bitraverse f g (PN x xs) = PN <$> f x <*> traverse (bitraverse f g) xs
   bitraverse f g (PL y)    = PL <$> g y
 
+instance (FromJSON n, FromJSON l) => FromJSON (Bitree n l) where
+  parseJSON = genericParseJSON defaultOptions
+
+instance (ToJSON n, ToJSON l) => ToJSON (Bitree n l) where
+  toJSON = genericToJSON defaultOptions
+
+instance (Binary n, Binary l) => Binary (Bitree n l)
 
 -- | duplicate of comonad is dual to join of monad, i.e. duplicate :: w a -> w (w a)
 --   In Bitree case, we can make a tree where each node is the subtree at the node point using duplicate 
