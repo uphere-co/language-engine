ea9af2b7266ebcd33ac4f836d8f23a01c1f6ae56
Doh Young Mo <modohyoung@gmail.com>
2017-07-05 02:43:01 +0000

    Get a name from EntityMention.

--- src/WikiEL/Convert.hs
+++ src/WikiEL/Convert.hs
@@ -1,3 +1,11 @@
 module WikiEL.Convert where
 
+import           Data.Vector                           (Vector)
+--
+import WikiEL.EntityLinking
 
+getNameFromEntityMention :: (Show w) => (EntityMention w) -> IO (Vector w)
+getNameFromEntityMention x = do
+  case x of
+    Cite uid ref info@(ir,v,ne) -> return v
+    Self uid info@(ir,v,ne)   -> return v
