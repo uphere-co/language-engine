8e7f711e8e2200f51730ac343024547acb1ccc9b
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-07 07:07:03 +0000

    remove dead code

--- src/SRL/Feature/Clause.hs
+++ src/SRL/Feature/Clause.hs
@@ -15,17 +15,6 @@ import qualified NLP.Type.PennTreebankII.Separated as N
 import           SRL.Format
 
 
-
-{- 
-clauseForest :: Bitree N.CombinedTag (POSTag,Text) -> [Bitree N.ClauseTag (Either POSTag N.PhraseTag)]
-clauseForest (PN N.RT xs) = concatMap clauseForest xs
-clauseForest (PN (N.CL c) xs) = [PN c (concatMap clauseForest xs)]
-clauseForest (PN (N.PH p) xs) = [PL (Right p)]
-clauseForest (PL (pos,_))   = [PL (Left pos)]
-
-
--}
-
 currentlevel (PN (_,l) _) = l
 currentlevel (PL _ )      = 0
 
@@ -43,15 +32,12 @@ clauseLevel (PL pt)     = PL (T.pack (show pt))
 
 
 
-
+showClauseLeve :: PennTree -> IO ()
 showClauseLevel ptree  = do
-  -- T.IO.putStrLn $ T.intercalate " " (map snd (toList testpt2))
   let tr = clauseLevel (bimap N.convert id (getADTPennTree ptree))
       tr' = bimap f id tr
         where f (N.CL c,l) = T.pack (show c) <> ":" <> T.pack (show l)
               f (N.PH p,l) = T.pack (show p) <> ":" <> T.pack (show l)
               f (N.RT  ,l) = "ROOT" <> ":" <> T.pack (show l)
-              -- g       = -- T.pack (show p)
-      
   T.IO.putStrLn (formatBitree id tr')
-  -- print (clauseForest tr)
+
