4a34a96fc30ecddfd178b8d43cb95955d863c067
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-10 05:59:03 +0000

    print all NML.

--- app/parse.hs
+++ app/parse.hs
@@ -5,15 +5,18 @@ module Main where
 
 import           Control.Exception
 import           Control.Lens                  hiding ((<.>))
+import           Control.Monad
 import           Data.Aeson
 import qualified Data.Attoparsec.Text         as A
+import           Data.Bifoldable
 import qualified Data.ByteString.Char8        as B
 import qualified Data.ByteString.Lazy.Char8   as BL
 import           Data.Default
 import           Data.Foldable                        (toList,traverse_)
 import qualified Data.IntMap                  as IM
-import           Data.List                            (sort)
+import           Data.List                            (intercalate,sort)
 import           Data.Maybe
+import           Data.Monoid
 import qualified Data.Sequence                as Seq
 import           Data.Text                            (Text)
 import qualified Data.Text                    as T
@@ -24,6 +27,7 @@ import           System.Directory.Tree
 import           System.Environment
 import           System.FilePath
 import           System.IO
+import           Text.Printf
 --
 import qualified CoreNLP.Proto.CoreNLPProtos.Document  as D
 import qualified CoreNLP.Proto.CoreNLPProtos.Sentence  as S
@@ -87,30 +91,52 @@ errorHandler h_err msg action = do
     _ -> return ()
 
 
-process pp = do
-  let basedir = "/scratch/wavewave/LDC/ontonotes/b/data/files/data/english/annotations/nw/wsj"
+convertTop (PN _ xs) = PN "ROOT" xs
+
+filterNML x@(PN NML xs) = [x]
+filterNML (PN c xs) = concatMap filterNML xs
+filterNML (PL _) = []
+
+
+
+
+process basedir pp = do
   dtr <- build basedir
   let fps = sort (toList (dirTree dtr))
       parsefiles = filter (\x -> takeExtensions x == ".parse") fps
   withFile "error.log" WriteMode $ \h_err -> do
     flip traverse_ parsefiles $ \f -> do
+      {- 
       putStrLn "\n\n\n=============================================================================================="
       print f
-      putStrLn "=============================================================================================="
+      putStrLn "==============================================================================================" -}
       etr <- parseOntoNotesPennTree f
       case etr of
         Left err -> hPutStrLn h_err f
         Right trs -> do
+          mapM_ ((\xs -> when ((not.null) xs) (mapM_ formatPrint xs)) . filterNML . getADTPennTree . convertTop) trs
+            where formatPrint :: PennTreeGen ChunkTag (POSTag,Text) -> IO ()
+                  formatPrint x =putStrLn $
+                    printf "%30s : %s "
+                      ((T.intercalate " " . map (^._2) . toList) x)
+                      (show x)
+           {-  where f :: ChunkTag -> [ChunkTag]
+                  f x = [x]
+                  g :: (POSTag,Text) -> [ChunkTag]
+                  g x = [] -}
+          {- 
           let bname = takeBaseName f
           withFile (bname <.> "corenlp_lemma") WriteMode $ \h_lemma -> do
             errorHandler h_err f (serializeLemma pp trs h_lemma)
           withFile (bname <.> "corenlp_udep") WriteMode $ \h_ud -> 
             withFile (bname <.> "corenlp_ptree") WriteMode $ \h_tr -> 
               errorHandler h_err f (serializePennTreeDep pp trs (h_ud,h_tr))
-
+          -}
 
 main :: IO ()
 main = do
+  let basedir = "/scratch/wavewave/LDC/ontonotes/b/data/files/data/english/annotations/nw/wsj"
+  
   clspath <- getEnv "CLASSPATH"
   J.withJVM [ B.pack ("-Djava.class.path=" ++ clspath) ] $ do
     let pcfg = def & ( tokenizer .~ True )
@@ -122,4 +148,4 @@ main = do
                      . ( constituency .~ False ) -- . ( constituency .~ True )
                      . ( ner .~ False )
     pp <- prepare pcfg
-    process pp 
+    process basedir pp 
