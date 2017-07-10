bc9976a635dca92165528e93bf60fff954fe7ede
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-10 02:22:31 +0000

    factor out serializeLemma and serializePennTreeDep

--- app/parse.hs
+++ app/parse.hs
@@ -38,6 +38,73 @@ getTerms :: PennTree -> [Text]
 getTerms = map snd . filter (\(t,_) -> t /= "-NONE-") . toList 
 
 
+serializeLemma pp h_err f = do
+  let bname = takeBaseName f
+  withFile (bname <.> "corenlp_lemma") WriteMode $ \h_lemma -> do
+      putStrLn "\n\n\n=============================================================================================="
+      print f
+      putStrLn "=============================================================================================="
+      etr <- fmap (A.parseOnly (A.many1 (A.skipSpace *> pnode))) (T.IO.readFile f)
+
+      case etr of
+        Left err -> print err
+        Right trs -> do
+          let tss = map getTerms trs
+              ts = map (T.intercalate " ") tss
+              -- ntxt = T.intercalate "\n\n" ts
+              docs = map (flip Document (fromGregorian 1990 1 1)) ts -- ntxt
+          r <- try $ do 
+            anns <- traverse (annotate pp) docs
+            rdocs' <- traverse protobufDoc anns
+            let rdocs = sequenceA rdocs'
+            case rdocs of
+              Left err -> print err
+              Right ds -> do
+                let sents = map (flip Seq.index 0 . (^. D.sentence)) ds
+                    lmap= map (map (_2 %~ unLemma) . IM.toList . mkLemmaMap) sents
+                BL.hPutStrLn h_lemma (encode lmap)
+          case r of
+            Left (e :: SomeException) -> hPutStrLn h_err f
+            _ -> return ()
+
+
+serializePennTreeDep pp h_err f = do
+  let bname = takeBaseName f
+  withFile (bname <.> "corenlp_udep") WriteMode $ \h_ud -> do
+    withFile (bname <.> "corenlp_ptree") WriteMode $ \h_tr -> do
+      putStrLn "\n\n\n=============================================================================================="
+      print f
+      putStrLn "=============================================================================================="
+      etr <- fmap (A.parseOnly (A.many1 (A.skipSpace *> pnode))) (T.IO.readFile f)
+      case etr of
+        Left err -> print err
+        Right trs -> do
+          let tss = map getTerms trs
+              ts = map (T.intercalate " ") tss
+              -- ntxt = T.intercalate "\n\n" ts
+              docs = map (flip Document (fromGregorian 1990 1 1)) ts -- ntxt
+          r <- try $ do 
+            anns <- traverse (annotate pp) docs
+            rdocs' <- traverse protobufDoc anns
+            let rdocs = sequenceA rdocs'
+            case rdocs of
+              Left err -> print err
+              Right ds -> do
+                let sents = map (flip Seq.index 0 . (^. D.sentence)) ds
+                    ntrs = map decodeToPennTree (mapMaybe (^.S.parseTree) sents)
+                    edeps = mapM sentToDep sents
+                case edeps of
+                  Left err -> print err
+                  Right deps -> do
+                    BL.hPutStrLn h_ud (encode deps)
+                    BL.hPutStrLn h_tr (encode ntrs)
+                -- T.IO.putStrLn ntxt
+          case r of
+            Left (e :: SomeException) -> hPutStrLn h_err f
+            _ -> return ()
+
+
+
 
 process pp = do
   let basedir = "/scratch/wavewave/LDC/ontonotes/b/data/files/data/english/annotations/nw/wsj"
@@ -48,71 +115,9 @@ process pp = do
   withFile "error.log" WriteMode $ \h_err -> do
   
     flip traverse_ parsefiles $ \f -> do
-      let bname = takeBaseName f
+      serializeLemma pp h_err f
+      serializePennTreeDep pp h_err f 
 
-
-      withFile (bname <.> "corenlp_lemma") WriteMode $ \h_lemma -> do
-          putStrLn "\n\n\n=============================================================================================="
-          print f
-          putStrLn "=============================================================================================="
-          etr <- fmap (A.parseOnly (A.many1 (A.skipSpace *> pnode))) (T.IO.readFile f)
-          
-          case etr of
-            Left err -> print err
-            Right trs -> do
-              let tss = map getTerms trs
-                  ts = map (T.intercalate " ") tss
-                  -- ntxt = T.intercalate "\n\n" ts
-                  docs = map (flip Document (fromGregorian 1990 1 1)) ts -- ntxt
-              r <- try $ do 
-                anns <- traverse (annotate pp) docs
-                rdocs' <- traverse protobufDoc anns
-                let rdocs = sequenceA rdocs'
-                case rdocs of
-                  Left err -> print err
-                  Right ds -> do
-                    let sents = map (flip Seq.index 0 . (^. D.sentence)) ds
-                        lmap= map (map (_2 %~ unLemma) . IM.toList . mkLemmaMap) sents
-                    BL.hPutStrLn h_lemma (encode lmap)
-              case r of
-                Left (e :: SomeException) -> hPutStrLn h_err f
-                _ -> return ()
-      
-
-      {-
-      withFile (bname <.> "corenlp_udep") WriteMode $ \h_ud -> do
-        withFile (bname <.> "corenlp_ptree") WriteMode $ \h_tr -> do
-          putStrLn "\n\n\n=============================================================================================="
-          print f
-          putStrLn "=============================================================================================="
-          etr <- fmap (A.parseOnly (A.many1 (A.skipSpace *> pnode))) (T.IO.readFile f)
-          case etr of
-            Left err -> print err
-            Right trs -> do
-              let tss = map getTerms trs
-                  ts = map (T.intercalate " ") tss
-                  -- ntxt = T.intercalate "\n\n" ts
-                  docs = map (flip Document (fromGregorian 1990 1 1)) ts -- ntxt
-              r <- try $ do 
-                anns <- traverse (annotate pp) docs
-                rdocs' <- traverse protobufDoc anns
-                let rdocs = sequenceA rdocs'
-                case rdocs of
-                  Left err -> print err
-                  Right ds -> do
-                    let sents = map (flip Seq.index 0 . (^. D.sentence)) ds
-                        ntrs = map decodeToPennTree (mapMaybe (^.S.parseTree) sents)
-                        edeps = mapM sentToDep sents
-                    case edeps of
-                      Left err -> print err
-                      Right deps -> do
-                        BL.hPutStrLn h_ud (encode deps)
-                        BL.hPutStrLn h_tr (encode ntrs)
-                    -- T.IO.putStrLn ntxt
-              case r of
-                Left (e :: SomeException) -> hPutStrLn h_err f
-                _ -> return ()
-      -}
 main :: IO ()
 main = do
   clspath <- getEnv "CLASSPATH"
