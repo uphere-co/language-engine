28c4633c0fa855a43b6c2717dfa96db037876fda
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-13 07:09:18 +0000

    using contiguousMatch, test how propbank annotations are matched with verb arguments.

--- app/argument.hs
+++ app/argument.hs
@@ -19,7 +19,7 @@ import           Data.HashMap.Strict                (HashMap)
 import qualified Data.HashMap.Strict        as HM
 import qualified Data.IntMap                as IM
 import           Data.List
-import           Data.Maybe                         (fromMaybe,maybeToList)
+import           Data.Maybe                         (fromJust,fromMaybe,maybeToList)
 import           Data.Monoid
 import           Data.Text                          (Text)
 import qualified Data.Text                  as T
@@ -98,8 +98,12 @@ contiguousMatch (ContiguousSegments segs) (i,j) = foldMap match segs
           in case rs of
                []    -> []
                (r:_) -> es++[r]
-  
 
+
+toMatchResult :: [Range] -> MatchResult
+toMatchResult []     = Unmatched
+toMatchResult (x:[]) = ExactMatch x
+toMatchResult xs     = MergeMatch xs
 
   
 prepare {- framedir -} basedir = do
@@ -141,6 +145,7 @@ loadMatchArticle ptreedir {- framedir -} basedir article = do
         pairs = zip cores proptrs
     return (merge (^.inst_tree_id) pairs insts)
 
+
 propbankCorpus ptreedir basedir article = do
   void . runEitherT $ do
     lst <- concat <$> loadMatchArticle ptreedir {- framedir -} basedir article
@@ -152,10 +157,15 @@ propbankCorpus ptreedir basedir article = do
       T.IO.putStrLn (T.intercalate " " tokens)
 
       putStrLn "\n\n======"
+      let minsts = matchInstances (coretr,proptr) insts
+          lmap = IM.fromList (map (_2 %~ Lemma) corelma)
+          verbprops = verbPropertyFromPennTree lmap coretr
+          tr = clauseStructure verbprops (bimap (\(rng,c) -> (rng,N.convert c)) id (mkPennTreeIdx coretr))
+      
+      {- 
       putStrLn "-----"
       putStrLn "propbank"
       putStrLn "-----"
-      let minsts = matchInstances (coretr,proptr) insts
       flip mapM_ minsts $ \minst-> do
         let inst = minst^.mi_instance
             args = minst^.mi_arguments
@@ -165,23 +175,19 @@ propbankCorpus ptreedir basedir article = do
       putStrLn "-----"
       putStrLn "verb property"
       putStrLn "-----"
-      let lmap = IM.fromList (map (_2 %~ Lemma) corelma)
-          verbprops = verbPropertyFromPennTree lmap coretr
-      -- print $ map (\vp->(vp^.vp_index,vp^.vp_lemma.to unLemma)) verbprops
-      print verbprops
-      let tr = clauseStructure verbprops (bimap (\(rng,c) -> (rng,N.convert c)) id (mkPennTreeIdx coretr))
+      print $ map (\vp->(vp^.vp_index,vp^.vp_lemma.to unLemma)) verbprops
+      -- print verbprops
+      -- print $ map (\vp->getVerbArgs tr vp) verbprops
+      -}
+      showClauseStructure lmap coretr
       
-      print $ map (\vp->getVerbArgs tr vp) verbprops
-
       putStrLn "-----"
       putStrLn "propbank match test"
       putStrLn "-----"
       
       flip mapM_ minsts $ \minst-> do
         let inst = minst^.mi_instance
-            -- marg0 = find (\a->a^.ma_argument.arg_label == NumberedArgument 0) (minst^.mi_arguments)
             args = filter (\a->a^.ma_argument.arg_label /= Relation) (minst^.mi_arguments)
-            -- args = maybeToList marg0 ++ oargs
         putStrLn "*************"
         T.IO.putStrLn (formatRoleSetID (inst^.inst_lemma_roleset_id))
         let relidx = findRelNode (minst^.mi_arguments)
@@ -192,31 +198,18 @@ propbankCorpus ptreedir basedir article = do
           Nothing -> putStrLn "unmatched!"
           Just (vp,va) -> do
             print "relation matched"
-            {- 
-            case marg0 of
-              Nothing -> putStrLn "ARG0 unmatched!"
-              Just arg0 -> do
-                T.IO.putStrLn (arg0^.ma_argument.arg_label.to pbLabelText)
-                let ns = arg0^..ma_nodes.traverse.mn_node._1
-                let m = case va^.va_arg0 of
-                          Nothing -> Nothing
-                          Just varg0 -> (find (== getRange varg0))  ns
-                -- print ns
-                print m -}
             let vargs = maybeToList (va^.va_arg0) ++ va^.va_args
             
             flip mapM_ args $ \arg -> do
-              T.IO.putStrLn (arg^.ma_argument.arg_label.to pbLabelText)
               let ns = arg^..ma_nodes.traverse.mn_node._1
                   getRng = either (^._1) (\x->(x^._1,x^._1)) 
-              
-              print ns
-              print (map getRng vargs)
-              -- print va
-            
+              let nosegs = fromJust (mkNoOverlapSegments (map getRng vargs))
+              putStrLn $
+                printf "%15s : %s"
+                  (arg^.ma_argument.arg_label.to pbLabelText)              
+                  (show (zip ns (map (toMatchResult . contiguousMatch (mkContiguousSegments nosegs)) ns)))
         -- mapM_ (print . (\a->(a^.ma_argument.arg_label.to pbLabelText,a^..ma_nodes.traverse.mn_node._1))) args
-{-       
-      showClauseStructure lmap coretr
+
       putStrLn "-----"
       putStrLn "dependency"
       putStrLn "-----"
@@ -225,7 +218,7 @@ propbankCorpus ptreedir basedir article = do
           deptree = fmap (\(i,r) -> let t = fromMaybe "" (IM.lookup (i-1) tkmap) in (i-1,t,r))
                       (dependencyLabeledTree coredep)
       T.IO.putStrLn (linePrint (T.pack.show) deptree)
--}
+
 
 main = do
   let article = "wsj_2445"
