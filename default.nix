b8fd7cf5bcae67f3cf964e10fa1707afcbcd5ddc
Doh Young Mo <modohyoung@gmail.com>
2017-07-13 05:57:18 +0000

    Added missing lines in default.nix .

--- default.nix
+++ default.nix
@@ -5,7 +5,11 @@
 mkDerivation {
   pname = "wiki-ner";
   version = "0.1.0.0";
-  src = ./.;
+  src = builtins.filterSource
+
+  (path: type: type != "directory" || baseNameOf path != "dist" || baseNameOf path != ".cabal-sandbox")
+  ./.;
+  
   libraryHaskellDepends = [
     attoparsec base containers nlp-types primitive tasty-hunit text
     vector vector-algorithms xxhash
@@ -15,5 +19,6 @@ mkDerivation {
     text vector vector-algorithms xxhash
   ];
   license = stdenv.lib.licenses.unfree;
+  doHaddock = false;
   doCheck = false;
 }
