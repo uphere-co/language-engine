1a04c36420d8b7feda6ac2152186cc37929d7745
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-13 00:42:36 +0000

    remove test code in app/parse.hs

--- app/parse.hs
+++ app/parse.hs
@@ -54,23 +54,6 @@ parseOntoNotesPennTree :: FilePath -> IO (Either String [PennTree])
 parseOntoNotesPennTree f = fmap (A.parseOnly (A.many1 (A.skipSpace *> pnode))) (T.IO.readFile f)
 
 
---   print trclst
---   print $ bifoldMap (\x -> [x]) (const []) tr
---   print $ chkidlst
-{-   let b = getAll (foldMap (All . (== 1) . length) (group chkidlst))
-  when (not b) $ do
-    print tr
-    T.IO.putStrLn $ prettyPrint 0 tr
-    (error "not unique")
--}
-
-
-{-   let trcidlst = sort  . mapMaybe (\x->x^._2) $ trclst
-      chk = getAll $ foldMap (\x -> All (x `elem` chkidlst)) trcidlst
-  when (not chk) (error "not matched")
--}
-
-
 errorHandler h_err msg action = do
   r <- try action
   case r of
@@ -78,13 +61,9 @@ errorHandler h_err msg action = do
     _ -> return ()
 
 
-
-
 filterNML x@(PN NML xs) = [x]
 filterNML (PN c xs) = concatMap filterNML xs
 filterNML (PL _) = []
-
-
 
 
 serializeLemma pp txts h_lemma = do
@@ -121,7 +100,7 @@ serializePennTreeDep pp txts (h_ud,h_tr)= do
 process basedir pp = do
   dtr <- build basedir
   let fps = sort (toList (dirTree dtr))
-      parsefiles = take 20 $ filter (\x -> takeExtensions x == ".parse") fps
+      parsefiles = filter (\x -> takeExtensions x == ".parse") fps
   withFile "error.log" WriteMode $ \h_err -> do
     flip traverse_ parsefiles $ \f -> do
       putStrLn "\n\n\n=============================================================================================="
@@ -131,27 +110,10 @@ process basedir pp = do
       case etr of
         Left err -> hPutStrLn h_err f
         Right trs -> do
-          -- mapM_ print trs
           flip mapM_ trs $ \tr -> do
-            putStrLn "-------------"
-            print (createTraceMap tr)
-            {-
-            let trclst = (map identifyTrace . getNONETerms) tr
-            print trclst
-            print $ bifoldMap (\x -> [x]) (const []) tr
-            let chkidlst = sort (bifoldMap (\x -> linkIDChunk x) (const []) tr)
-            print $ chkidlst
-            let b = getAll (foldMap (All . (== 1) . length) (group chkidlst))
-            when (not b) $ do
-              print tr
-              T.IO.putStrLn $ prettyPrint 0 tr
-              (error "not unique")
-            let trcidlst = sort  . mapMaybe (\x->x^._2) $ trclst
-                chk = getAll $ foldMap (\x -> All (x `elem` chkidlst)) trcidlst
-            when (not chk) (error "not matched")
-            -}
-{-          let txts = flip map trs $ \tr ->
-                in T.intercalate " " (map (T.concat . map snd) merged)
+          let txts = flip map trs $ \tr ->
+                let merged = getMerged tr
+                in T.intercalate " " (map (T.concat . map (snd.snd)) merged)
 
           let bname = takeBaseName f
           withFile (bname <.> "corenlp_lemma") WriteMode $ \h_lemma -> do
@@ -159,7 +121,7 @@ process basedir pp = do
           withFile (bname <.> "corenlp_udep") WriteMode $ \h_ud ->
             withFile (bname <.> "corenlp_ptree") WriteMode $ \h_tr ->
               errorHandler h_err f (serializePennTreeDep pp txts (h_ud,h_tr))
--}
+
 
 main :: IO ()
 main = do
@@ -172,8 +134,8 @@ main = do
                      . ( postagger .~ True )
                      . ( lemma .~ True )
                      . ( sutime .~ False )
-                     . ( depparse .~ False ) -- . ( depparse .~ True )
-                     . ( depparse .~ False ) -- . ( constituency .~ True )
+                     . ( depparse .~ True )
+                     . ( constituency .~ True )
                      . ( ner .~ False )
     pp <- prepare pcfg
     process basedir pp
