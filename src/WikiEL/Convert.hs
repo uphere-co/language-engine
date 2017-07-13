e78a6e66d9ccbb05efd4f881d9f61888f69f1886
Doh Young Mo <modohyoung@gmail.com>
2017-07-13 02:43:07 +0000

    NE, UID, Range functions are added.

--- src/WikiEL/Convert.hs
+++ src/WikiEL/Convert.hs
@@ -3,9 +3,25 @@ module WikiEL.Convert where
 import           Data.Vector                           (Vector)
 --
 import WikiEL.EntityLinking
+import WikiEL.Misc                                     (IRange(..))
 
-getNameFromEntityMention :: (Show w) => (EntityMention w) -> IO (Vector w)
-getNameFromEntityMention x = do
-  case x of
-    Cite uid ref info@(ir,v,ne) -> return v
-    Self uid info@(ir,v,ne)   -> return v
+getNameFromEntityMention :: (Show w) => (EntityMention w) -> Vector w
+getNameFromEntityMention x = case x of
+                               Cite uid ref info@(ir,v,ne) -> v
+                               Self uid     info@(ir,v,ne) -> v
+
+getRangeFromEntityMention :: (Show w) => (EntityMention w) -> (Int,Int)
+getRangeFromEntityMention x = case x of
+                                Cite uid ref info@(ir,v,ne) -> (beg ir,end ir)
+                                Self uid     info@(ir,v,ne) -> (beg ir, end ir)
+    
+getUIDFromEntityMention :: (Show w) => (EntityMention w) -> Int
+getUIDFromEntityMention x = case x of
+                                Cite uid ref info@(ir,v,ne) -> (_emuid uid)
+                                Self uid     info@(ir,v,ne) -> (_emuid uid)
+
+getNEFromEntityMention :: (Show w) => (EntityMention w) -> String
+getNEFromEntityMention x = case x of
+                                Cite uid ref info@(ir,v,ne) -> (show ne)
+                                Self uid     info@(ir,v,ne) -> (show ne)
+
