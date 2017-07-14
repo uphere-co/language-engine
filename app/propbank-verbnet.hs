f459d5dc7b0cd603f8399008ef56c00db42188bd
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-14 01:44:42 +0000

    make hashmap from verbnet

--- app/propbank-verbnet.hs
+++ app/propbank-verbnet.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
@@ -6,7 +7,7 @@ import           Control.Lens
 import           Data.Foldable
 import qualified Data.HashMap.Strict as HM
 import qualified Data.HashSet        as HS
-import           Data.List                     (sort)
+import           Data.List                     (isSuffixOf,sort)
 import           Data.Maybe
 import qualified Data.Text           as T
 import qualified Data.Text.IO        as T.IO
@@ -21,23 +22,51 @@ import           PropBank.Type.Frame
 import           VerbNet.Parser
 import           VerbNet.Type
 
+data VNClassContent = VNC { _vnc_members :: [Member]
+                          , _vnc_them_roles :: [ThemRole]
+                          , _vnc_frames :: [Frame] }
+                    deriving (Show)
 
-main1 :: IO ()
-main1 = do
+
+
+addaction dir f acc = do
+  let fp = dir  </> f
+  -- print fp
+  txt <- T.L.IO.readFile fp
+  case txt ^? html . allNamed (only "VNCLASS") of
+    Nothing -> error "nothing"
+    Just f -> case p_vnclass f of
+                Left err -> error err
+                Right c  -> do
+                  putStrLn "----------------"
+                  let allclasses = let i0 = c^.vnclass_id
+                                       vnc0 = VNC (c^.vnclass_members)
+                                                  (c^.vnclass_themroles)
+                                                  (c^.vnclass_frames)
+                                   in ((i0,vnc0) : go (c^.vnclass_subclasses))
+                      go [] = []
+                      go lst = do x <- lst
+                                  let i = x^.vnsubclass_id
+                                      vnc = VNC (x^.vnsubclass_members)
+                                                (x^.vnsubclass_themroles)
+                                                (x^.vnsubclass_frames)
+                                  ((i,vnc) : go (x^.vnsubclass_subclasses))
+
+                  return (foldl' (\(!acc') (k,v) -> HM.insert k v acc') acc allclasses)
+                  -- let cmap = HM.fromList allclasses -- (c^.vnclass_id)
+                  -- print (HM.lookup "bump-18.4" cmap)
+                  -- print (c^..vnclass_subclasses.traverse.vnsubclass_id)
+
+
+main :: IO ()
+main = do
   let dir= "/scratch/wavewave/VerbNet/verbnet"
   cnts <- getDirectoryContents dir
-  let fs = sort (filter (\x -> takeExtensions x == ".xml") cnts) 
+  let fs = sort (filter (\x -> ".xml" `isSuffixOf` takeExtensions x) cnts) 
   -- let fs = [ "admit-65.xml" ] -- [ "get-13.5.1.xml" ]
-  flip mapM_ fs $ \f -> do
-    let fp = dir  </> f
-    print fp
-    txt <- T.L.IO.readFile fp
-    case txt ^? html . allNamed (only "VNCLASS") of
-      Nothing -> error "nothing"
-      Just f -> case p_vnclass f of
-                  Left err -> error err
-                  Right c  -> print c
+  clmap <- foldrM (addaction dir) HM.empty fs
 
+  print (HM.lookup "bump-18.4" clmap)
 
 getVNCls rdb k = do 
   r <- maybeToList (HM.lookup k (rdb^.rolesetDB))
@@ -63,7 +92,7 @@ main2 = do
     putStrLn (printf "%20s : %6s :  %s" l f (T.intercalate ", " cls_dedupe))
   
 
-
+{-
 main = do
   main2 
-
+-}
