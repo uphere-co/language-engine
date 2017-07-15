d21071af6a67c3d0aa1cab4dda43bfa03c1798e1
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-15 20:32:06 +0000

    introduce CoreType and add instances of Eq,Ord,Generic to most of types.

--- src/FrameNet/Type/Common.hs
+++ src/FrameNet/Type/Common.hs
@@ -1,13 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module FrameNet.Type.Common where
 
 import           Control.Lens
 import           Data.Text    (Text)
+import           GHC.Generics
 
 data SemType = SemType { _semtype_ID :: Int
                        , _semtype_name :: Text }
-             deriving (Show)
+             deriving (Show,Eq,Ord,Generic)
 
 makeLenses ''SemType
 
@@ -16,7 +18,7 @@ data Lexeme = Lexeme { _lexeme_name :: Text
                      , _lexeme_breakBefore :: Maybe Bool
                      , _lexeme_headword :: Maybe Bool
                      , _lexeme_order :: Maybe Int }
-            deriving (Show)
+            deriving (Show,Eq,Ord,Generic)
 
 makeLenses ''Lexeme
 
@@ -34,7 +36,7 @@ data POS = A
          | PRON
          | SCON
          | V
-         deriving (Show)
+         deriving (Show,Eq,Ord,Enum,Bounded,Generic)
 
 data BasicLUAttributes = BasicLUAttributes { _bluattr_ID :: Int
                                            , _bluattr_name :: Text
@@ -42,13 +44,18 @@ data BasicLUAttributes = BasicLUAttributes { _bluattr_ID :: Int
                                            , _bluattr_incorporatedFE :: Maybe Text
                                            , _bluattr_status :: Maybe Text
                                            }
-                       deriving (Show)
+                       deriving (Show,Eq,Ord,Generic)
 
 makeLenses ''BasicLUAttributes
 
 data FrameReference = FrameReference { _fr_frameID :: Maybe Int
                                      , _fr_frame :: Maybe Text
                                      }
-                    deriving (Show)
+                    deriving (Show,Eq,Ord,Generic)
 
 makeLenses ''FrameReference                             
+
+data CoreType = Core | Peripheral | ExtraThematic | CoreUnexpressed
+              deriving (Show,Eq,Ord,Enum,Bounded,Generic)
+
+                       
