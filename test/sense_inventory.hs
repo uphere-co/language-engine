1bfa3581b0864eb882930d326c238c1442fd8248
Ian-Woo Kim <ianwookim@gmail.com>
2017-07-16 21:42:42 +0000

    show propbank and verbnet

--- test/sense_inventory.hs
+++ test/sense_inventory.hs
@@ -26,7 +26,7 @@ import           OntoNotes.Parser.SenseInventory
 
 load dir = do
   cnts <- getDirectoryContents dir
-  let fs = sort (filter (\x -> takeExtensions x == ".xml") cnts) 
+  let fs = sort (filter (\x -> takeExtensions x == ".xml") cnts)
   flip traverse fs $ \f -> do
     let fp = dir  </> f
     txt <- T.L.IO.readFile fp
@@ -37,18 +37,34 @@ load dir = do
                   Right c  -> return c
 
 
+verbnet txt = let (_,cls') = T.breakOn "-" txt
+                  cls = if T.null cls' then cls' else T.tail cls'
+              in cls
+
+
 getSenses lma simap = do
   si <- maybeToList (HM.lookup lma simap)
   s <- si^.inventory_senses
   let txt1 = text (printf "%2s.%-6s %-40s   " (s^.sense_group) (s^.sense_n) (T.take 40 (s^.sense_name)))
-      mappings = s^.sense_mappings 
-  
+      mappings = s^.sense_mappings
+      txt_pb = vcat top $ let lst = T.splitOn "," (mappings^.mappings_pb)
+                          in if null lst
+                             then [text (printf "%-20s" ("" :: String))]
+                             else map (text.printf "%-20s") lst
       txt_fn = vcat top $ let lst = maybe [] (T.splitOn ",") (mappings^.mappings_fn)
-                          in if null lst then [text (printf "%-30s" ("" :: String))] else map (text.printf "%-30s") lst
+                          in if null lst
+                             then [text (printf "%-30s" ("" :: String))]
+                             else map (text.printf "%-30s") lst
       txt_wn = vcat top $ let lst = map (text.printf "%-30s") (catMaybes (mappings^..mappings_wn.traverse.wn_lemma))
                           in if null lst then [text (printf "%-30s" ("" :: String))] else lst
-                                              
-  return (txt1 <+> txt_fn <+> txt_wn)
+      txt_vn = vcat top $ let lst = maybe [] (map verbnet . T.splitOn ",") (mappings^.mappings_vn)
+                          in if null lst
+                             then [text (printf "%-20s" ("" :: String))]
+                             else map (text.printf "%-20s") lst
+
+
+
+  return (txt1 <+> txt_pb <+> txt_fn <+> txt_wn <+> txt_vn)
 
 
 
@@ -66,15 +82,14 @@ main = do
                   in case mapM (decimal.snd) lst of
                        Left _     -> (lma,0)
                        Right lst' -> (lma,sum (map fst lst'))
-                     
+
       merged = sortBy (flip compare `on` snd) . map merge . groupBy ((==) `on` fst)
              . sortBy (flip compare `on` fst)
              . map (\(l,f)-> let (lma,_) = T.break (== '.') l in (lma,f))
-             $ ws 
-   
+             $ ws
+
   forM_ merged $ \(lma,f) -> do
     let lmav = lma <> "-v"
-        doc = text (printf "%20s : %6d " lma f) <+> vcat top (getSenses lmav simap)
+        doc = text (printf "%20s:%6d " lma f) <+> vcat top (getSenses lmav simap)
     putStrLn "-------------------------------------------"
     putStrLn (render doc)
- 
