d064317821552a29ce46e8e7a3a6c39f20d1a749
Doh Young Mo <modohyoung@gmail.com>
2017-07-25 01:19:40 +0000

    Added default.nix produced by cabal2nix.

--- default.nix
+++ default.nix
@@ -2,20 +2,20 @@
 , bytestring, containers, data-default, discrimination, either
 , fastText, filepath, foreign-store, HCoreNLP, HCoreNLP-Proto, jni
 , jvm, lens, nlp-types, optparse-applicative, PropBank, split
-, stdenv, text, time, transformers, vector
-, textview
+, stdenv, tasty, tasty-hunit, text, textview, time, transformers
+, vector
 }:
 mkDerivation {
   pname = "semantic-role-labeler";
   version = "0.1.0.0";
-  src = builtins.filterSource (path: type: type != "directory" || baseNameOf path != "dist" || baseNameOf path != ".cabal-sandbox") ./.;
+  src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     attoparsec base bifunctors bindings-svm bytestring containers
     data-default discrimination either fastText filepath foreign-store
     HCoreNLP HCoreNLP-Proto jni jvm lens nlp-types PropBank split text
-    time vector textview
+    textview time vector
   ];
   executableHaskellDepends = [
     attoparsec base bytestring containers data-default either fastText
@@ -23,7 +23,8 @@ mkDerivation {
     nlp-types optparse-applicative PropBank text time transformers
     vector
   ];
+  testHaskellDepends = [
+    base containers HCoreNLP lens nlp-types tasty tasty-hunit text
+  ];
   license = "unknown";
-  doHaddock = false;
-  doCheck = false;
 }
