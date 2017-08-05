cf39c28e489fd799f5dcfbac31bf6d153e917eb0
Ian-Woo Kim <ianwookim@gmail.com>
2017-08-05 13:52:49 +0000

    makeKeyAttrib: extract key attribute from a list of a singleton.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, base, bifunctors, binary, discrimination, lens, stdenv, taggy-lens, text }:
 mkDerivation {
   pname = "nlp-types";
-  version = "0.1.0.0";
+  version = "0.1.1.0";
   src = ./.;
   libraryHaskellDepends = [ aeson base bifunctors binary discrimination lens taggy-lens text ];
   license = "unknown";
