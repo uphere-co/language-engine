a742a51c8b162a868a59cf50bfb934349c7c9dab
Jihun Kim <jihun@uphere.co>
2017-08-10 02:31:34 +0000

    Tests for finding paths in undirected graph.

--- test/Test/GraphOps.hs
+++ test/Test/GraphOps.hs
@@ -139,6 +139,11 @@ testNeighborNodes = testCaseSteps "Get neighbor nodes in directed/undirected gra
   eassertEqual (nodesBackward directed 1 1) (UV.fromList [(4,1),(8,1),(9,1),(10,1),(1,0)])
   eassertEqual (nodesBackward directed 1 3) (UV.fromList [(11,3),(2,2),(3,2),(4,1),(8,1),(9,1),(10,1),(1,0)])
 
+  eassertEqual (nodesBackward undirected 8 1) (UV.fromList [(1,1),(10,1),(11,1),(8,0)])
+  eassertEqual (nodesBackward undirected 8 2) (nodesForward undirected 8 2)
+  eassertEqual (nodesBackward undirected 8 2) (UV.fromList [(2,2),(3,2),(4,2),(5,2),(6,2),(9,2),(1,1),(10,1),(11,1),(8,0)])
+  eassertEqual (nodesBackward undirected 1 1) (UV.fromList [(2,1),(3,1),(4,1),(5,1),(6,1),(8,1),(9,1),(10,1),(1,0)])
+
 
 testAllPaths :: TestTree
 testAllPaths = testCaseSteps "Get all paths within distance cutoff between a pair of nodes" $ \step -> do
