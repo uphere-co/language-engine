f8e1dcf8647bc4eee6f755a8c19a6b43c947b354
Ian-Woo Kim <ianwookim@gmail.com>
2017-08-22 22:32:37 +0000

    toTree : convert Bitree to Tree

--- src/Data/Bitree.hs
+++ src/Data/Bitree.hs
@@ -6,32 +6,33 @@
 
 module Data.Bitree where
 
-import Control.Applicative
-import Data.Aeson
-import Data.Bifoldable
-import Data.Bifunctor
-import Data.Binary
-import Data.Bitraversable
-import Data.Foldable
-import Data.Monoid
-import Data.Traversable
-import GHC.Generics
+import           Control.Applicative
+import           Data.Aeson
+import           Data.Bifoldable
+import           Data.Bifunctor
+import           Data.Binary
+import           Data.Bitraversable
+import           Data.Foldable
+import           Data.Monoid
+import           Data.Traversable
+import qualified Data.Tree            as Tr
+import           GHC.Generics
 
--- | chunk = chunktag, token = token in node. 
---   typically token will be (pos = postag, a = content)
 
+-- | n = node, l = leaf
+--
 data Bitree n l = PN n [Bitree n l]
                 | PL l
                 deriving (Show, Eq, Functor, Foldable, Traversable, Generic)
 
 instance Bifunctor Bitree where
   bimap f g (PN x xs) = PN (f x) (map (bimap f g) xs)
-  bimap f g (PL y)    = PL (g y) 
+  bimap f g (PL y)    = PL (g y)
 
 instance Bifoldable Bitree where
   bifoldMap f g (PN x xs) = f x <> foldMap (bifoldMap f g) xs
   bifoldMap f g (PL y)    = g y
-  
+
 instance Bitraversable Bitree where
   bitraverse f g (PN x xs) = PN <$> f x <*> traverse (bitraverse f g) xs
   bitraverse f g (PL y)    = PL <$> g y
@@ -60,12 +61,14 @@ getNodes (PN x xs) = x:(concatMap getNodes xs)
 getNodes (PL _) = []
 
 
+toTree :: Bitree a a -> Tr.Tree a
+toTree (PN x xs) = Tr.Node x (map toTree xs)
+toTree (PL x)    = Tr.Node x []
 
 
 
 -- | duplicate of comonad is dual to join of monad, i.e. duplicate :: w a -> w (w a)
---   In Bitree case, we can make a tree where each node is the subtree at the node point using duplicate 
+--   In Bitree case, we can make a tree where each node is the subtree at the node point using duplicate
 duplicate :: Bitree c a -> Bitree (Bitree c a) (Bitree c a)
 duplicate (PN x xs) = PN (PN x xs) (map duplicate xs)
 duplicate (PL x) = PL (PL x)
-
