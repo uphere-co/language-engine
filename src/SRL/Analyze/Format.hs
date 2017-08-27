21dfef8ebb37dcae5dde8bc0cbb38eb7e977407d
Ian-Woo Kim <ianwookim@gmail.com>
2017-08-27 09:30:31 +0000

    Migrate OntoNotes/analyze to semantic-role-labeler/analyze

---
+++ src/SRL/Analyze/Format.hs
@@ -0,0 +1,349 @@
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE FlexibleContexts   #-}
+{-# LANGUAGE LambdaCase         #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE TupleSections      #-}
+
+module SRL.Analyze.Format where
+
+import           Control.Applicative
+import           Control.Lens                            ((^..),(^.),(^?),(%~),_1,_2,_3,_4,_5,_6,_7,_Just,_Right,to
+                                                         ,lengthOf,folded)
+import           Control.Monad                           ((>=>),guard)
+import           Data.Foldable
+import           Data.Function                           ((&),on)
+import           Data.List                               (find,groupBy,intercalate,intersperse,maximumBy,sortBy)
+import           Data.Maybe                              (catMaybes,fromMaybe,listToMaybe,mapMaybe,maybeToList)
+import           Data.Monoid                             ((<>))
+import           Data.Text                               (Text)
+import qualified Data.Text                       as T
+import qualified Data.Text.IO                    as T.IO
+import           Data.Traversable                        (traverse)
+import           Text.PrettyPrint.Boxes                  (Box,left,hsep,text,top,vcat,render)
+import           Text.Printf                             (printf)
+import           Text.ProtocolBuffers.Basic              (Utf8)
+--
+import           CoreNLP.Simple.Convert                  (sentToTokens,sentToTokens')
+import           CoreNLP.Simple.Type.Simplified          (Token,token_lemma,token_pos)
+import           Data.Bitree
+import           Data.BitreeZipper
+import           Data.BitreeZipper.Util                  (root)
+import           Data.Range
+import           Lexicon.Format                          (formatArgPatt,formatArgPattStat,formatRoleMap)
+import           Lexicon.Merge                           (constructTopPatterns,mergePatterns,patternGraph,patternRelation
+                                                         ,listOfSupersetSubset,topPatterns)
+import           Lexicon.Query                           (cutHistogram)
+import           Lexicon.Type                            (ArgPattern(..),type RoleInstance
+                                                         ,type RolePattInstance,POSVorN(..),GRel(..),GArg(..)
+                                                         ,patt_arg0,patt_arg1,patt_arg2,patt_arg3,patt_arg4
+                                                         ,findGArg
+                                                         )
+import           NLP.Syntax.Clause
+import           NLP.Syntax.Format
+import           NLP.Printer.PennTreebankII              (formatIndexTokensFromTree)
+import           NLP.Syntax.Type
+import           NLP.Type.PennTreebankII
+import           NLP.Type.SyntaxProperty                 (Voice)
+import qualified WikiEL                        as WEL
+import           WikiEL.EntityLinking                    (UIDCite(..),EMInfo,EntityMentionUID,_emuid)
+import qualified WikiEL.EntityLinking          as EL
+import           WikiEL.WikiNamedEntityTagger            (resolveNEs,getStanfordNEs,parseStanfordNE
+                                                         ,namedEntityAnnotator,resolvedUID)
+
+--
+import           OntoNotes.App.Util                      (CharIdx,TokIdx,TagPos(..),SentItem
+                                                         ,addTag,convertTagPosFromTokenToChar
+                                                         ,underlineText)
+import           OntoNotes.App.WikiEL                    (linkedMentionToTagPos)
+--
+import           SRL.Analyze.Type              (ExceptionalFrame(..),ONSenseFrameNetInstance(..)
+                                                         ,DocStructure(..),SentStructure(..),VerbStructure(..)
+                                                         ,chooseFrame
+                                                         ,onfn_senseID,onfn_definition,onfn_frame
+                                                         ,tf_frameID,tf_feCore,tf_fePeri
+                                                         ,vs_lma,vs_vp,vs_mrmmtoppatts
+                                                         ,ss_verbStructures, ss_mcpstr, ss_clausetr
+                                                         ,ds_sentStructures
+                                                         )
+--
+import Debug.Trace
+
+
+getTopPatternsFromONFNInst :: [RoleInstance]
+                           -> [RolePattInstance Voice]
+                           -> ONSenseFrameNetInstance
+                           -> Maybe (RoleInstance, Maybe [(ArgPattern () GRel,Int)])
+getTopPatternsFromONFNInst rolemap subcats inst = do
+  let sid = inst^.onfn_senseID
+  rm <- find (\rm -> rm^._1 == sid) rolemap
+  let msubcat = find ((== sid) . (^._1)) subcats
+      mtoppatts_cut = cutHistogram 0.9 . constructTopPatterns . (^._2) <$> msubcat
+  return (rm,mtoppatts_cut)
+
+
+
+formatExFrame FrameCopula = "** COPULA **"
+formatExFrame FrameIdiom  = "** IDIOM **"
+formatExFrame FrameLightVerb = "** LIGHT VERB **"
+formatExFrame _              = "** UNIDENTIFIED **"
+
+formatLemmaPOS :: Token -> String
+formatLemmaPOS t = printf "%10s %5s" (t^.token_lemma) (show (t^.token_pos))
+
+
+formatTimex :: (SentItem CharIdx, [TagPos CharIdx (Maybe Text)]) -> [Text]
+formatTimex (s,a) = (underlineText (const "") (s^._2) (s^._3) a) ++ ["----------"] ++ [T.pack (show a)]
+
+
+showTimex :: (SentItem CharIdx, [TagPos CharIdx (Maybe Text)]) -> IO ()
+showTimex (s,a) = T.IO.putStrLn (T.intercalate "\n" (formatTimex (s,a)))
+
+
+
+getFormatTimex' :: (SentItem CharIdx,[TagPos CharIdx (Maybe Text)]) -> [Text]
+getFormatTimex' (s,a) = (underlineText (const "") (s^._2) (s^._3) a) ++ ["----------"] ++ [T.pack (show a)]
+
+showFormatTimex' :: (SentItem CharIdx,[TagPos CharIdx (Maybe Text)]) -> IO ()
+showFormatTimex' (s,a) = T.IO.putStrLn (T.intercalate "\n" (getFormatTimex' (s,a)))
+
+
+
+
+formatSense :: (ONSenseFrameNetInstance,Int) -> String
+formatSense (onfninst,num) =
+  printf "%-8s (%4d cases) | %-40s | %-20s | %-40s      ------       %-30s "
+         (onfninst^.onfn_senseID._3)
+         num
+         (onfninst^.onfn_definition)
+         (onfninst^.onfn_frame.to (either formatExFrame (^.tf_frameID)))
+         (maybe "" (T.intercalate ", ") ((onfninst^?onfn_frame._Right.tf_feCore)))
+         (maybe "" (T.intercalate ", ") ((onfninst^?onfn_frame._Right.tf_fePeri)))
+
+
+
+formatFrame t =
+  printf " %-20s | %-40s      ------      %-30s\n"
+    (fromMaybe "" (t ^? _Just . _1 . onfn_frame . to (either formatExFrame (^.tf_frameID))))
+    (maybe "" (T.intercalate ", ") (t^?_Just._1.onfn_frame._Right.tf_feCore))
+    (maybe "" (T.intercalate ", ") (t^?_Just._1.onfn_frame._Right.tf_fePeri))
+
+
+formatSenses :: Bool  -- ^ doesShowOtherSense
+             -> [(ONSenseFrameNetInstance,Int)]
+             -> Maybe (RoleInstance, Maybe [(ArgPattern () GRel,Int)])
+             -> String
+formatSenses doesShowOtherSense onfnlst mrmmtoppatts
+  = let t = chooseFrame onfnlst
+    in "Top frame: "
+       ++ formatFrame t
+       ++ "--------------------------------------------------------------------------------------------------\n"
+       ++ flip (maybe "") mrmmtoppatts
+            (\(rm,mtoppatts) ->
+               let margpattstr = fmap formatArgPattStat mtoppatts
+               in (formatRoleMap (rm^._2) ++ maybe "" ("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"<>) margpattstr))
+       ++ "\n--------------------------------------------------------------------------------------------------\n"
+       ++ if doesShowOtherSense
+          then "\n\n\n*********************************************\n" ++ intercalate "\n" (map formatSense onfnlst)
+          else ""
+
+
+
+formatLinkedMention Cite {..} = printf "%3d: (-> %3d) %s " (EL._emuid _uid) (EL._emuid _ref) (formatEMInfo _info)
+formatLinkedMention Self {..} = printf "%3d:          %s " (EL._emuid _uid)                  (formatEMInfo _info)
+
+
+formatIndexedTimex (c,mtxt)   = printf "%3s:          %s " (T.singleton c) (fromMaybe "" mtxt)
+
+formatTaggedSentences :: (a -> Text)
+                      ->  [(SentItem CharIdx,[TagPos CharIdx a])]
+                      -> Box
+formatTaggedSentences f sents_tagged =
+  let txts = concatMap (\(s,a) -> underlineText f (s^._2) (s^._3) a) sents_tagged
+  in vcat top $ map (text . T.unpack) txts
+
+
+formatPreNE tag = case resolvedUID tag of
+                    Left e -> "unresolved"
+                    Right i -> show i
+
+
+formatEMInfo :: EL.EMInfo Text -> String
+formatEMInfo em@(_,ws,tag) = printf "%-25s %-20s" (WEL.entityName em) (formatPreNE tag)
+
+
+formatTagged :: [[Maybe Token]]
+             -> [SentItem CharIdx]
+             -> [TagPos TokIdx (Either (EL.EntityMention Text) (Char,(Maybe Text)))]
+             -> Box
+formatTagged mtokenss sentitems tlst =
+  let toks = concatMap (map snd . sentToTokens') mtokenss
+      clst = mapMaybe (convertTagPosFromTokenToChar toks) tlst
+      sents_tagged = map (addTag clst) sentitems
+      doc1 = formatTaggedSentences (either (T.pack . show . _emuid . EL._uid) (T.singleton . (^._1))) sents_tagged
+      doc2 = vcat top . intersperse (text "") . map (text. either formatLinkedMention formatIndexedTimex) $ map (\(TagPos (_,_,x)) -> x) clst
+  in hsep 10 left [doc1,doc2]
+
+
+
+
+formatDocStructure :: Bool -> DocStructure -> [Text]
+formatDocStructure showdetail (DocStructure mtokenss sentitems mergedtags sstrs) =
+  let line1 = [ "=================================================================================================="
+              , "-- Time and NER tagged text ----------------------------------------------------------------------"
+              , T.pack (render (formatTagged mtokenss sentitems mergedtags))
+              , "--------------------------------------------------------------------------------------------------"
+              , "-- Sentence analysis -----------------------------------------------------------------------------"
+              , "--------------------------------------------------------------------------------------------------" ]
+      line3 = concatMap (maybe [""] (formatSentStructure showdetail)) sstrs
+
+  in line1  ++ line3
+
+
+formatSentStructure :: Bool -> SentStructure -> [Text]
+formatSentStructure showdetail (SentStructure i ptr vps clausetr mcpstr vstrs) =
+   let subline1 = [ T.pack (printf "-- Sentence %3d ----------------------------------------------------------------------------------" i)
+                  , formatIndexTokensFromTree 0 ptr
+                  ]
+       subline1_1 = [ "--------------------------------------------------------------------------------------------------"
+                    , formatClauseStructure vps clausetr
+                    , "================================================================================================="
+                    ]
+       subline2 = map (formatVerbStructure clausetr mcpstr) vstrs
+   in subline1 ++ (if showdetail then subline1_1 else []) ++ concat subline2
+
+
+formatVerbStructure :: ClauseTree -> Maybe [Bitree (Range,CP) (Range,CP)] -> VerbStructure -> [Text]
+formatVerbStructure clausetr mcpstr (VerbStructure vp lma senses mrmmtoppatts) =
+  [ formatVPwithPAWS clausetr mcpstr vp
+  , T.pack (printf "Verb: %-20s" lma)
+  , T.pack $ (formatSenses False senses mrmmtoppatts)
+  ]
+
+
+
+matchFrame mcpstr vstr paws = do
+  let cp = paws^.pa_CP
+      verbp = cp^.cp_TP.tp_VP
+      mrmmtoppatts = vstr^.vs_mrmmtoppatts
+      mdp_resolved = resolveDP mcpstr cp
+  (rm,mtoppatts) <- mrmmtoppatts
+  let rolemap = rm^._2
+  frame <- lookup "frame" rolemap
+  let selected = do
+        toppattstats <- mtoppatts
+        dp_resolved <- mdp_resolved
+        let matched = map (matchSO rolemap (dp_resolved,verbp,paws)) toppattstats
+            cmpmatch = flip compare `on` lengthOf (_2.folded)
+            cmpstat  = flip compare `on` (^._1._2)
+            eq       = (==) `on` lengthOf (_2.folded)
+        (listToMaybe . sortBy cmpstat . head . groupBy eq . sortBy cmpmatch) matched
+  return (frame,selected)
+
+
+
+mkPAWSTriples dstr = do
+  msstr <- dstr^.ds_sentStructures
+  sstr <- maybeToList msstr
+  let clausetr = sstr^.ss_clausetr
+      mcpstr = sstr^.ss_mcpstr
+  vstr <- sstr ^.ss_verbStructures
+  let vp = vstr^.vs_vp
+  paws <- maybeToList (findPAWS clausetr vp)
+  return (mcpstr,vstr,paws)
+
+
+
+showMatchedFrame (mcpstr,vstr,paws) = do
+  let cp = paws^.pa_CP
+      vp =vstr^.vs_vp
+      gettokens = T.intercalate " " . map (tokenWord.snd) . toList . current
+  T.IO.putStrLn "---------------------------"
+  putStrLn ("predicate: " <> maybe "unidentified CP" show (cpRange cp))
+  T.IO.putStrLn ("Verb: " <> (vstr^.vs_lma))
+  flip traverse_ (matchFrame mcpstr vstr paws) $ \(frame,mselected) -> do
+    T.IO.putStrLn ("Frame: " <> frame)
+    flip traverse_ mselected $ \((patt,num),felst) -> do
+      mapM_ putStrLn . map (\(fe,z) -> printf "%-15s: %-7s %s" fe (show (getRange (current z))) (gettokens z)) $ felst
+
+
+
+matchSO rolemap (dp,verbp,paws) (patt,num) =
+  case verbp^.vp_verbProperty.vp_voice of
+    Active -> ((patt,num), maybeToList (matchSubject rolemap dp patt) ++ matchObjects rolemap verbp patt ++ matchPrepArgs rolemap paws patt )
+    Passive -> ((patt,num),catMaybes [matchAgentForPassive rolemap paws patt,matchThemeForPassive rolemap dp patt] ++ matchPrepArgs rolemap paws patt)
+
+
+matchPrepArgs rolemap paws patt = do
+  (p,prep) <- pbArgForPP patt
+  -- prep <- maybeToList mprep
+  z <- maybeToList (matchPP paws prep)
+  (,z) <$> maybeToList (lookup p rolemap)
+
+
+matchPP paws prep = do
+    Left (rng,_) <- find ppcheck (paws^.pa_candidate_args)
+    tr <- current . root <$> paws^.pa_CP.cp_maximal_projection
+    find (\z -> case getRoot (current z) of Left (rng',_) -> rng' == rng; _ -> False) $ getNodes (mkBitreeZipper [] tr)
+  where
+    ppcheck (Left (_,S_PP prep')) = prep == prep'
+    ppcheck _                     = False
+
+
+
+matchSubject rolemap dp patt = do
+  (p,GR_NP (Just GASBJ)) <- pbArgForGArg GASBJ patt
+  (,dp) <$> lookup p rolemap
+
+
+matchAgentForPassive rolemap paws patt = do
+    (p,GR_NP (Just GASBJ)) <- pbArgForGArg GASBJ patt
+    z <- matchPP paws "by"
+    (,z) <$> lookup p rolemap
+
+
+
+
+matchThemeForPassive rolemap dp patt = do
+  (p,GR_NP (Just GA1)) <- pbArgForGArg GA1 patt
+  (,dp) <$> lookup p rolemap
+
+
+matchObjects rolemap verbp patt = do
+  (garg,obj) <- zip [GA1,GA2] (verbp^.vp_complements)
+  ctag <- case getRoot (current obj) of
+            Left (_,node) -> [chunkTag node]
+            _             -> []
+  (p,a) <- maybeToList (pbArgForGArg garg patt)
+  case ctag of
+    NP   -> guard (a == GR_NP   (Just garg))
+    S    -> guard (a == GR_SBAR (Just garg))
+    SBAR -> guard (a == GR_SBAR (Just garg))
+    _    -> []
+  fe <- maybeToList (lookup p rolemap)
+  return (fe,obj)
+
+-- matchPP patt =
+  
+
+pbArgForGArg grel patt = check patt_arg0 "arg0" <|>
+                         check patt_arg1 "arg1" <|>
+                         check patt_arg2 "arg2" <|>
+                         check patt_arg3 "arg3" <|>
+                         check patt_arg4 "arg4"
+  where check l label = do a <- patt^.l
+                           grel' <- findGArg a
+                           if grel==grel' then Just (label,a) else Nothing
+
+
+pbArgForPP patt = catMaybes [ check patt_arg0 "arg0"
+                            , check patt_arg1 "arg1"
+                            , check patt_arg2 "arg2"
+                            , check patt_arg3 "arg3"
+                            , check patt_arg4 "arg4"
+                            ]
+  where check l label = do a <- patt^.l
+                           -- grel <- findGArg a
+                           case a of
+                             GR_PP mprep -> (label,) <$> mprep
+                             _           -> Nothing
---
+++ src/SRL/Analyze/SentenceStructure.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
+
+module SRL.Analyze.SentenceStructure where
+
+import           Control.Lens                              ((^.),(^..),_1,to)
+import           Data.Bifunctor                            (bimap)
+import           Data.Foldable                             (forM_)
+import           Data.Function                             (on)
+import           Data.HashMap.Strict                       (HashMap)
+import qualified Data.HashMap.Strict               as HM
+import           Data.List                                 (sortBy,zip5)
+import           Data.Maybe                                (fromMaybe,maybeToList)
+import           Data.Monoid                               ((<>))
+import qualified Data.Text                         as T
+import           Data.Text                                 (Text)
+import qualified Data.Text.IO                      as T.IO
+import           Data.Traversable                          (forM)
+import qualified Language.Java                     as J
+import           Text.PrettyPrint.Boxes                    (render)
+import           Text.Printf                               (printf)
+--
+import           CoreNLP.Simple.Convert                    (mkLemmaMap,mkLemmaMap',sentToNER')
+import           CoreNLP.Simple.Type.Simplified            (NERSentence(..),Token,Dependency,Sentence,SentenceIndex
+                                                           ,sentenceNER,sentenceWord,sentenceToken,sentenceLemma)
+import           FrameNet.Query.Frame                      (FrameDB,frameDB)
+import           FrameNet.Type.Common                      (CoreType(..))
+import           FrameNet.Type.Frame                       (fe_coreType,fe_name,frame_FE)
+import           Lexicon.Type                              (ArgPattern(..),POSVorN(..),RoleInstance,RolePattInstance)
+import           NLP.Printer.PennTreebankII                (formatIndexTokensFromTree)
+import           NLP.Type.PennTreebankII                   (PennTree)
+import           NLP.Syntax.Clause                         (clauseStructure,constructCP
+                                                           ,identifyCPHierarchy
+                                                           )
+import           NLP.Syntax.Verb                           (verbPropertyFromPennTree)
+import           NLP.Syntax.Type                           (BitreeZipperICP,VerbProperty(..),Voice,vp_lemma)
+import qualified NLP.Type.NamedEntity              as N
+import           NLP.Type.PennTreebankII                   (Lemma(..),mkPennTreeIdx)
+import qualified NLP.Type.PennTreebankII.Separated as PS
+import           WikiEL.EntityLinking                      (EntityMentionUID,EntityMention(..),UIDCite(..)
+                                                           ,entityLinking,entityLinkings,buildEntityMentions,entityUID)
+import           WikiEL.WikiNamedEntityTagger              (PreNE(..))
+--
+import           OntoNotes.App.Util                        (CharIdx,SentItem,TagPos(..),TokIdx)
+import           OntoNotes.App.WikiEL                      (getWikiResolvedMentions
+                                                           ,linkedMentionToTagPos
+                                                           )
+import           OntoNotes.Type.SenseInventory
+--
+import qualified SRL.Analyze.Config   as Analyze (Config, showDetail)
+import           SRL.Analyze.CoreNLP             (runParser)
+import           SRL.Analyze.Format              (formatSenses,formatTimex
+                                                           ,formatTagged
+                                                           ,showTimex,showFormatTimex'
+                                                           ,getTopPatternsFromONFNInst
+                                                           )
+import           SRL.Analyze.Type
+
+
+mergeTagPos :: (Ord i) => [TagPos i a] -> [TagPos i b] -> [TagPos i (Either a b)]
+mergeTagPos xs ys =
+  let zs = map (fmap Left) xs ++ map (fmap Right) ys
+      idx (TagPos (i,_,_)) = i
+  in sortBy (compare `on` idx) zs
+
+
+
+
+getSenses :: Text
+          -> HashMap Text Inventory
+          -> HashMap (Text,Text) Int
+          -> FrameDB
+          -> HashMap Text [(Text,Text)]
+          -> [(ONSenseFrameNetInstance,Int)]
+getSenses lma sensemap sensestat framedb ontomap = do
+  let lmav = lma <> "-v"
+  si <- maybeToList (HM.lookup lmav sensemap)
+  s <- si^.inventory_senses
+  let sid = (lma,Verb, s^.sense_group <> "." <> s^.sense_n)
+  let num = fromMaybe 0 (HM.lookup (lma,s^.sense_n) sensestat)
+      txt_def = T.take 40 (s^.sense_name)
+      tframe = fromMaybe (Left FrameNone) $ do
+        lst <- HM.lookup lma ontomap
+        frtxt <- lookup (s^.sense_group <> "." <> s^.sense_n) lst
+        case frtxt of
+          "copula"    -> return (Left FrameCopula)
+          "idioms"    -> return (Left FrameIdiom)
+          "lightverb" -> return (Left FrameLightVerb)
+          _ -> do
+            frame <- HM.lookup frtxt (framedb^.frameDB)
+            let fes = frame^..frame_FE.traverse
+                corefes = map (^.fe_name)
+                        . filter (\fe -> fe^.fe_coreType == Core || fe^.fe_coreType == CoreUnexpressed)
+                        $ fes
+                perifes = map (^.fe_name)
+                        . filter (\fe -> fe^.fe_coreType == Peripheral)
+                        $ fes
+            return (Right (TF frtxt corefes perifes))
+  return ((ONFNInstance sid txt_def tframe),num)
+
+
+getTopPatternsFromSensesAndVP rolemap subcats senses vp =
+  let mcp = constructCP vp
+      mrmmtoppatts = getTopPatternsFromONFNInst rolemap subcats =<< fmap (^._1) (chooseFrame senses)
+  in (senses,mrmmtoppatts)
+
+
+-- | Finding the structure of the sentence and formatting it.
+--
+docStructure :: AnalyzePredata
+             -> ([(Text, N.NamedEntityClass)] -> [EntityMention Text])
+             -> DocAnalysisInput
+             -> DocStructure
+docStructure apredata emTagger docinput@(DocAnalysisInput sents sentidxs sentitems _tokss mptrs deps mtmxs) =
+  let lmass = sents ^.. traverse . sentenceLemma . to (map Lemma)
+      mtokenss = sents ^.. traverse . sentenceToken
+      linked_mentions_resolved = getWikiResolvedMentions emTagger
+                                                         (docinput^.dainput_sents)
+                                                         (concat (docinput^.dainput_tokss))
+      lnk_mntns_tagpos = map linkedMentionToTagPos linked_mentions_resolved
+      mkidx = zipWith (\i x -> fmap (i,) x) (cycle ['a'..'z'])
+      mergedtags = maybe (map (fmap Left) lnk_mntns_tagpos) (mergeTagPos lnk_mntns_tagpos . mkidx) mtmxs
+      sentStructures = map (sentStructure apredata) (zip3 ([0..]::[Int]) lmass mptrs)
+  in DocStructure mtokenss sentitems mergedtags sentStructures
+
+
+
+sentStructure :: AnalyzePredata -> (Int,[Lemma],Maybe PennTree) -> Maybe SentStructure
+sentStructure apredata (i,lmas,mptr) =
+  flip fmap mptr $ \ptr ->
+    let lemmamap = (mkLemmaMap' . map unLemma) lmas
+        vps = verbPropertyFromPennTree lemmamap ptr
+        clausetr = clauseStructure vps (bimap (\(rng,c) -> (rng,PS.convert c)) id (mkPennTreeIdx ptr))
+        mcpstr = identifyCPHierarchy vps
+        verbStructures = map (verbStructure apredata) vps
+    in SentStructure i ptr vps clausetr mcpstr verbStructures
+
+
+verbStructure :: AnalyzePredata -> VerbProperty (BitreeZipperICP '[Lemma]) -> VerbStructure
+verbStructure apredata vp =
+  let lma = vp^.vp_lemma.to unLemma
+      sensemap = apredata^.analyze_sensemap
+      sensestat = apredata^.analyze_sensestat
+      framedb = apredata^.analyze_framedb
+      ontomap = apredata^.analyze_ontomap
+      rolemap = apredata^.analyze_rolemap
+      subcats = apredata^.analyze_subcats
+
+      senses = getSenses lma sensemap sensestat framedb ontomap
+      mrmmtoppatts = getTopPatternsFromONFNInst rolemap subcats =<< fmap (^._1) (chooseFrame senses)
+  in VerbStructure vp lma senses mrmmtoppatts
