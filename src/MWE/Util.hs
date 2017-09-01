d1d2f94870a4af49f0a1dfbac4a2fbd928196b74
Doh Young Mo <modohyoung@gmail.com>
2017-09-01 02:07:22 +0000

    Purified the function.

--- src/MWE/Util.hs
+++ src/MWE/Util.hs
@@ -9,11 +9,12 @@ import qualified Data.Text           as T
 --
 import           NLP.Type.CoreNLP
 
-mkTextFromToken mtoks = do
+
+mkTextFromToken :: [Maybe Token] -> T.Text
+mkTextFromToken mtoks =
   let toks = catMaybes mtoks
-  result <- flip evalStateT (0 :: Int) $ do
-    forM toks $ \t -> do
-      s <- get
-      modify' $ (\s -> t ^. token_char_idx_range ^. _2)
-      return (T.append (T.replicate ((t ^. token_char_idx_range ^. _1) - s) " ") (t ^. token_text))
-  return $ T.intercalate "" result
+      txts = join $ flip evalStateT 0 $ forM toks $ \t -> do
+          s <- get
+          modify' $ (\s -> t ^. token_char_idx_range ^. _2)
+          return (T.append (T.replicate ((t ^. token_char_idx_range ^. _1) - s) " ") (t ^. token_text))
+  in T.intercalate "" txts
