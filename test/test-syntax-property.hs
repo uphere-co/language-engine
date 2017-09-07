ed9569fe473a0288222bac64b7d552595ce4513f
Ian-Woo Kim <ianwookim@gmail.com>
2017-09-07 16:07:10 +0000

    verb complement tests are integrated into syntax property test. problematic cases are now commented out.

--- test/test-syntax-property.hs
+++ test/test-syntax-property.hs
@@ -1,13 +1,19 @@
 module Main where
 
-import           NLP.Syntax.Format
 --
-import qualified Test.Clause       as T.C
-import qualified Test.VerbProperty as T.V
+import qualified Test.Clause         as T.C
+import qualified Test.VerbComplement as T.V.C 
+import qualified Test.VerbProperty   as T.V.P
 --
 import           Test.Tasty
 
+
+tests = testGroup "Syntax property unit tests" [ T.V.P.unitTests
+                                               , T.V.C.unitTests
+                                               ]
+
+
 main :: IO ()
-main = defaultMain T.V.unitTests
+main = defaultMain tests
 
 
