07f740af79e808433b2ad8bc7774b8c98ac328ed
Ian-Woo Kim <ianwookim@gmail.com>
2017-09-24 17:20:20 +0000

    add type signature.

--- src/WikiEL/Run.hs
+++ src/WikiEL/Run.hs
@@ -22,37 +22,8 @@ import qualified WikiEL.WikiEntityClass        as WC
 import qualified WikiEL.WikiNamedEntityTagger  as WNET
 import qualified WikiEL                        as WEL
 
-{-
-prepareNETokens :: [Sentence] -> [NERToken]
-prepareNETokens sents =
-  let mws = sents ^.. traverse . sentenceWord
-      mns = sents ^.. traverse . sentenceNER
-      unNER (NERSentence tokens) = tokens
-      neTokens = concat $ map (\(x,y) -> (unNER $ sentToNER' x y)) (zip mws mns)
-  in neTokens
--}
 
-
-{-
-getWikiResolvedMentions :: ([NERToken] -> [EntityMention w]) -> [Sentence] -> [EntityMention w]
-getWikiResolvedMentions emTagger sents =
-  let tokenss = catMaybes <$> sents ^.. traverse . sentenceToken
-      linked_mentions_all = getWikiAllMentions emTagger sents
-      input_pos = V.fromList (map (^. token_pos) $ concat tokenss)
-      linked_mentions_all_unfiltered = (EMP.filterEMbyPOS input_pos linked_mentions_all)
-  in filter (\x -> let (_,_,pne) = _info x in case pne of Resolved _ -> True ; _ -> False) linked_mentions_all_unfiltered
--}
-
-{-
-getWikiAllMentions :: ([NERToken] -> [EntityMention w]) -> [Sentence] -> [EntityMention w]
-getWikiAllMentions emTagger sents =
-  let neTokens = prepareNETokens sents
-      linked_mentions_all = emTagger neTokens
-  in linked_mentions_all
-
--}
-
--- prepareWNP :: [Sentence] -> Double
+prepareWNP :: [Sentence] -> [(Text,NamedEntityClass,POSTag)]
 prepareWNP sents =
   let ws = catMaybes $ sents ^.. traverse . sentenceWord . traverse
       ns' = catMaybes $ sents ^.. traverse . sentenceNER . traverse
@@ -65,7 +36,10 @@ prepareWNP sents =
 mkConstraintFromWikiEL :: [EntityMention Text] -> [(Int,Int)]
 mkConstraintFromWikiEL wikiel = map (\x -> let irange = entityIRange x in (beg irange, end irange)) $ wikiel
 
--- runEL :: [Sentence] -> Double -> Double -> IO ()
+runEL :: [Sentence]
+      -> ([(Text,NamedEntityClass,POSTag)] -> [EntityMention Text])
+      -> ([EntityMention Text] -> [EntityMention Text])
+      -> IO ()
 runEL sents tagger entityResolve = do
   let wnps = prepareWNP sents
       linked_mentions = tagger wnps
@@ -81,7 +55,11 @@ loadWikiData = do
   let entityResolve = WEL.disambiguateMentions edges uidTag titles
   return (tagger,entityResolve)
 
+
+reprFileTinyG :: EntityReprFile
 reprFileTinyG       = EntityReprFile (globalData </> "wiki-ner/data/wikidata.test.entities")
+
+orgItemFileG,personItemFileG,brandItemFileG,locationItemFileG,occupationItemFileG,humanRuleItemFileG,buildingItemFileG :: ItemIDFile
 orgItemFileG        = ItemIDFile (globalData </> "wiki-ner/data/ne.org")
 personItemFileG     = ItemIDFile (globalData </> "wiki-ner/data/ne.person")
 brandItemFileG      = ItemIDFile (globalData </> "wiki-ner/data/ne.brand")
@@ -90,6 +68,7 @@ occupationItemFileG = ItemIDFile (globalData </> "wiki-ner/data/ne.occupation")
 humanRuleItemFileG  = ItemIDFile (globalData </> "wiki-ner/data/ne.human_rule")
 buildingItemFileG   = ItemIDFile (globalData </> "wiki-ner/data/ne.building")
 
+classFilesG :: [(WC.ItemClass,ItemIDFile)]
 classFilesG = [ (WC.personClass, personItemFileG)
               , (WC.orgClass,    orgItemFileG)
               , (WC.brandClass,  brandItemFileG)
@@ -99,14 +78,29 @@ classFilesG = [ (WC.personClass, personItemFileG)
               , (WC.buildingClass,   buildingItemFileG)
               ]
 
+reprFileG :: EntityReprFile
 reprFileG     = EntityReprFile (globalData </> "wiki-ner/data/names")
 
 -- Full data
-wikiTitleMappingFileG = WikiTitleMappingFile (globalData </> "wiki-ner/wiki_id.page_title.txt")
-wordnetMappingFileG   = WordNetMappingFile (globalData </> "wiki-ner/page_id.wiki_id.wordnet.tsv")
-propertyNameFileG     = PropertyNameFile (globalData </> "wiki-ner/properties.tsv")
-listedCompanyFileG    = globalData </> "enwiki/companies"
 
+wikiTitleMappingFileG :: WikiTitleMappingFile
+wikiTitleMappingFileG = WikiTitleMappingFile (globalData </> "wiki-ner/wiki_id.page_title.txt")
+
+
+wordnetMappingFileG :: WordNetMappingFile
+wordnetMappingFileG = WordNetMappingFile (globalData </> "wiki-ner/page_id.wiki_id.wordnet.tsv")
+
+
+propertyNameFileG :: PropertyNameFile
+propertyNameFileG = PropertyNameFile (globalData </> "wiki-ner/properties.tsv")
+
+listedCompanyFileG :: FilePath
+listedCompanyFileG = globalData </> "enwiki/companies"
+
+
+graphFilesG :: FilePath
 graphFilesG = globalData </> "wiki-ner/interlinks.filtered"
 
+
+globalData :: FilePath
 globalData = "/data/groups/uphere/data/Wiki"
