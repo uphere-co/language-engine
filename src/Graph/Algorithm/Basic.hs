00dd88ad27472255fa71aa676ddab0d4d071feb4
Doh Young Mo <modohyoung@gmail.com>
2017-10-10 05:13:07 +0000

    Basic operation are moved from SRL module.

---
+++ src/Graph/Algorithm/Basic.hs
@@ -0,0 +1,11 @@
+module Graph.Algorithm.Basic where
+
+import           Data.Graph
+import           Data.Tree
+
+
+maxConnectedNodes :: Graph -> Int
+maxConnectedNodes grph = maximum $ map (length . flatten) $ dff grph
+
+numberOfIsland :: Graph -> Int
+numberOfIsland grph = length $ components $ grph
