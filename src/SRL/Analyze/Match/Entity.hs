f5a3bb597ec700f91a124b2a36700d27e3788852
Ian-Woo Kim <ianwookim@gmail.com>
2017-11-08 02:53:24 +0000

    first implementation of pronounResolution

--- src/SRL/Analyze/Match/Entity.hs
+++ src/SRL/Analyze/Match/Entity.hs
@@ -4,30 +4,48 @@
 module SRL.Analyze.Match.Entity where
 
 import           Control.Lens
-import           Control.Monad  (mzero)
-import           Data.Foldable  (foldMap)
-import           Data.Monoid    ((<>),First(..))
-import           Data.List      (find)
-import           Data.Text      (Text)
-import qualified Data.Text as T
+import           Control.Lens.Extras      (is)
+import           Control.Monad            (guard,mzero)
+import           Data.Foldable            (foldMap)
+import           Data.Monoid              ((<>),First(..))
+import           Data.List                (find)
+import           Data.Text                (Text)
+import qualified Data.Text           as T
 --
 import           Data.BitreeZipper
 import           Data.Range
+import           NLP.Syntax.Clause        (currentCPDPPP)
 import           NLP.Syntax.Type.XBar
 import           NLP.Type.PennTreebankII
 --
 import Debug.Trace
 
 
-pronounResolution :: [X'Tree '[Lemma]] -> TaggedLemma t -> DetP t -> Maybe (Range,Text,Maybe (Range,Text))
+pronounResolution :: [X'Tree '[Lemma]]
+                  -> TaggedLemma '[Lemma]
+                  -> DetP '[Lemma]
+                  -> Maybe (Range,Text,Maybe (Range,Text))
 pronounResolution x'tr tagged dp = do
   let rng_dp = dp^.maximalProjection
-  w <- getFirst (foldMap (First . extractZipperById rng_dp) x'tr)
-  trace "pronounResolution: here" $ 
-    mzero
+  trace ("pronounResolution1: "++ show (headText tagged dp)) $ do
+   prnclass <- dp^?headX.hd_class._Pronoun
+   trace ("pronounResolution2: "++ show (headText tagged dp)) $ do
+    w <- getFirst (foldMap (First . extractZipperById rng_dp) x'tr)
+    trace ("pronounResolution3: "++ show (headText tagged dp)) $ do
+     w' <- parent w
+     trace ("pronounResolution4: "++ show (headText tagged dp)) $ do
+      guard (is _Just (currentCPDPPP w' ^? _CPCase))
+      trace ("pronounResolution5: "++ show (headText tagged dp)) $ do
+       w'' <- parent w'
+       trace ("pronounResolution6: "++ show (headText tagged dp)) $ do
+        cp <- currentCPDPPP w'' ^? _CPCase
+        trace ("pronounResolution7: " ++ show (headText tagged dp) ) $ do
+         dp' <- cp^?complement.specifier.trResolved._Just._Right
+         trace ("pronounResolution8: " ++ show (headText tagged dp) ) $ do
+          return (entityFromDP x'tr tagged dp')
 
 
-entityFromDP :: [X'Tree '[Lemma]] -> TaggedLemma t -> DetP t -> (Range,Text,Maybe (Range,Text))
+entityFromDP :: [X'Tree '[Lemma]] -> TaggedLemma '[Lemma] -> DetP '[Lemma] -> (Range,Text,Maybe (Range,Text))
 entityFromDP x'tr tagged dp =
   case pronounResolution x'tr tagged dp of
     Just result -> result
