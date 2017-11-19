35c2a63f8cdf0c6d2c573f1f2ff5eabd4d2f11b6
Ian-Woo Kim <ianwookim@gmail.com>
2017-11-19 16:23:04 +0000

    add more examples

--- src/SRL/Analyze/Config.hs
+++ src/SRL/Analyze/Config.hs
@@ -9,7 +9,6 @@ data Config = Config { _showDetail     :: Bool
                      , _showFullDetail :: Bool
                      , _bypassNER      :: Bool
                      , _configFile     :: FilePath
-                     -- , _useGlobalConfig :: Bool
                      }
             deriving (Show)
 
