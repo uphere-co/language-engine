178aec3cdd54b4b1d736eec78b3453e3f1c97961
Doh Young Mo <modohyoung@gmail.com>
2017-11-22 05:03:47 +0000

    Data type for CSV parsing.

--- src/NER.hs
+++ src/NER.hs
@@ -6,12 +6,13 @@ module NER where
 import qualified Data.ByteString.Lazy.Char8 as BL8
 import qualified Data.Csv             as C
 import qualified Data.HashMap.Strict  as HM
-import           Data.List                  (find)
+import           Data.List                  (find,foldl')
 import           Data.Text                  (Text)
 import qualified Data.Text            as T
 import qualified Data.Text.IO         as T.IO
 import qualified Data.Vector          as V
 --
+import           NER.Type
 import qualified WikiEL.ETL.LoadData     as LD
 import           WikiEL.Run                    (reprFileG)
 import qualified WikiEL.Type.FileFormat  as FF
@@ -21,20 +22,33 @@ loadNameTable = do
   reprs <- LD.loadEntityReprs reprFileG
   return $ map (\x -> ((WD._itemID . FF._uid) x,(WD._repr . FF._repr) x)) reprs
 
+loadNameHM :: IO (HM.HashMap Text Int)
+loadNameHM = do
+  reprs <- LD.loadEntityReprs reprFileG
+  let nameTable = map (\x -> ((WD._itemID . FF._uid) x, (WD._repr . FF._repr) x)) reprs
+  return $ foldl' (\acc (i,n) -> HM.insert n i acc) HM.empty nameTable
+
+-- loadUIDInfo :: IO (HM.HashMap Int Text)
+-- loadUIDInfo = do
+  
+  
 parseCompany nt = do
+  nhm <- loadNameHM
   let fp = "/home/modori/temp/companylist.csv"
   txt' <- T.IO.readFile fp
   let tlines = T.lines txt'
       txt = T.intercalate "\n" $ map (T.reverse . (T.drop 2) . T.reverse) tlines
       bstr = BL8.pack $ T.unpack txt 
-  let (ecompany :: Either String (V.Vector (Text,Text,Text,Text,Text,Text,Text,Text))) = C.decode C.HasHeader bstr
+  let (ecompany :: Either String (V.Vector Company)) = C.decode C.HasHeader bstr
   case ecompany of
     Left err -> error err
     Right  v -> do
       let ctable = V.toList v
-      flip mapM_ ctable $ \(_,name,_,_,_,_,_,_) -> do
+      flip mapM_ ctable $ \c -> do
+        print $ HM.lookup (_name c) nhm 
+      {- flip mapM_ ctable $ \(_,name,_,_,_,_,_,_) -> do
         print $ (name, aliasFinder nt name)
-
+      -}
 uidFinder nt txt = fmap fst $ find (\(_,x) -> x == txt) nt
 
 aliasFinder nt txt =
