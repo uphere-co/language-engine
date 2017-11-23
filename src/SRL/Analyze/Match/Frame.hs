ffc335cdb251518abc61dedc827cf730c96ab049
Ian-Woo Kim <ianwookim@gmail.com>
2017-11-23 17:24:42 +0000

    missing matchPP in AdjunctVP

--- src/SRL/Analyze/Match/Frame.hs
+++ src/SRL/Analyze/Match/Frame.hs
@@ -124,14 +124,16 @@ matchPP :: CP '[Lemma]
         -> Maybe (PP '[Lemma])
 matchPP cp (mprep,mpclass,mising) = do
     let compvps = cp^..complement.complement.complement.traverse.trResolved._Just
-        candidates = do compvp <- compvps
-                        case compvp of
-                          CompVP_PP pp ->
-                            let pp's = pp^..complement._CompPP_DP.adjunct.traverse._AdjunctDP_PP
-                            in (pp:pp's)
-                          CompVP_DP dp -> dp^..adjunct.traverse._AdjunctDP_PP
-                          _            -> []
-    find ppcheck candidates
+        pps_adj = cp^..complement.complement.adjunct.traverse._AdjunctVP_PP
+        pps_comp = do
+          compvp <- compvps
+          case compvp of
+            CompVP_PP pp ->
+              let pp's = pp^..complement._CompPP_DP.adjunct.traverse._AdjunctDP_PP
+              in (pp:pp's)
+            CompVP_DP dp -> dp^..adjunct.traverse._AdjunctDP_PP
+            _            -> []
+    find ppcheck (pps_comp ++ pps_adj)
   where
     ppcheck pp = let HeadPP prep' pclass' = pp^.headX
                      ising' = is _Just (pp^?complement._CompPP_Gerund)
