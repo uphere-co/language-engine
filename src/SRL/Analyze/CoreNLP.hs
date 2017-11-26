9e3868b89673e17363b486b71047261d354fb35a
Ian-Woo Kim <ianwookim@gmail.com>
2017-11-26 15:13:07 +0000

    separate out runUKB

--- src/SRL/Analyze/CoreNLP.hs
+++ src/SRL/Analyze/CoreNLP.hs
@@ -46,6 +46,26 @@ emptyDocAnalysisInput = DocAnalysisInput
   , _dainput_mtmxs     = Nothing
   }
 
+
+posTagToPOS p = (guard (isVerb p)        >> return POS_V) <|>
+                (guard (isNoun p == Yes) >> return POS_N) <|>
+                Nothing
+
+
+runUKB sents parsetrees = do
+  let lmass = sents ^.. traverse . sentenceLemma . to (map Lemma)
+  flip mapM_ (zip lmass parsetrees) $ \(lmas,mpt) ->
+    flip traverse_ mpt $ \pt -> do
+      let lmap = (mkLemmaMap . map unLemma) lmas
+          lemmapt = mkBitreeICP lmap pt
+          mkContextWord (i,x) = CtxtWord (unLemma (ahead (getAnnot x))) <$> posTagToPOS (posTag x) <*> pure i <*> pure 1
+          ctxt = Context "0" (mapMaybe mkContextWord (toList lemmapt))
+      print ctxt
+      result <- ppr ctxt
+      putStrLn ("\n\nTHIS RESULT: \n" ++ show result)
+  
+
+
 runParser :: J.J ('J.Class "edu.stanford.nlp.pipeline.AnnotationPipeline")
           -> Text
           -> IO DocAnalysisInput
@@ -54,10 +74,6 @@ runParser pp txt = do
   ann <- annotate pp doc
   pdoc <- getProtoDoc ann
   lbstr_sutime <- BL.fromStrict <$> serializeTimex ann
-
-
-
-
 
   let psents = pdoc ^.. D.sentence . traverse
       sentitems = map (addText txt) (getSentenceOffsets psents)
@@ -68,36 +84,8 @@ runParser pp txt = do
       tktokss = map (getTKTokens) psents
       tokss = map (mapMaybe convertToken) tktokss
 
-  {-
-  -- testing HUKB
-  let cwords = zipWith f [1..] [ ("Britain",POS_N), ("cut",POS_V), ("stamp",POS_N), ("duty",POS_N), ("property",POS_N), ("tax",POS_N)
-                               , ("buyer",POS_N), ("bid",POS_N), ("help",POS_V), ("people",POS_N), ("struggle",POS_V), ("get",POS_V)
-                               , ("property",POS_N), ("ladder",POS_N), ("finance",POS_N), ("minister",POS_N), ("Philip",POS_N)
-                               , ("Hammond",POS_N), ("say",POS_V), ("Wednesday",POS_N) ]
-        where f i (w,p) = CtxtWord w p i 1
-  let ctxt = Context "1" cwords
-  result <- ppr ctxt
-  print result
-  --- end testing
-  -}
-  let lmass = sents ^.. traverse . sentenceLemma . to (map Lemma)
 
-
-  flip mapM_ (zip lmass parsetrees) $ \(lmas,mpt) ->
-    flip traverse_ mpt $ \pt -> do
-      let lmap = (mkLemmaMap . map unLemma) lmas
-          lemmapt = mkBitreeICP lmap pt
-          posTagToPOS p = (guard (isVerb p)        >> return POS_V) <|>
-                          (guard (isNoun p == Yes) >> return POS_N) <|>
-                          Nothing
-          mkContextWord (i,x) = CtxtWord (unLemma (ahead (getAnnot x))) <$> posTagToPOS (posTag x) <*> pure i <*> pure 1
-          ctxt = Context "0" (mapMaybe mkContextWord (toList lemmapt))
-      print ctxt
-      result <- ppr ctxt
-
-      putStrLn ("\n\nTHIS RESULT: \n" ++ show result)
-      -- TIO.put
-      -- traverse_ (TIO.putStrLn . prettyPrint 0) mpt
+  
 
   mtmx <- case fmap fst (messageGet lbstr_sutime) :: Either String T.ListTimex of
     Left _ -> return Nothing
