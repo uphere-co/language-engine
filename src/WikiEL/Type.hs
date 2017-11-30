35104f9eccf6177e08eb8821a43522a579a9c250
Doh Young Mo <modohyoung@gmail.com>
2017-11-30 06:51:41 +0000

    OnlyTextMathced type is added.

--- src/WikiEL/Type.hs
+++ src/WikiEL/Type.hs
@@ -69,9 +69,12 @@ data PreNE = UnresolvedUID NamedEntityClass           -- Tagged by CoreNLP NER, 
            | AmbiguousUID ([ItemID],NamedEntityClass) -- Tagged by CoreNLP NER, and matched Wikidata UIDs of the NamedEntityClass
            | Resolved (ItemID, ItemClass)             -- A wikidata UID of a CoreNLP NER Class type.                                
            | UnresolvedClass [ItemID]                 -- Not tagged by CoreNLP NER, but matched Wikidata UID(s)                     
+           | OnlyTextMatched TextMatchedEntityType
            deriving(Show, Eq, Generic)
 
 makePrisms ''PreNE
+
+data TextMatchedEntityType = PublicCompany | PrivateCompany deriving (Show, Eq)
 
 instance ToJSON PreNE where
   toJSON = genericToJSON defaultOptions
