e5f2e0377735883a211ebb2d5d64922297ef6c8e
Ian-Woo Kim <ianwookim@gmail.com>
2018-01-09 16:08:56 +0000

    Frame.Type.Definition. parser for <def-root> in <definition> in <frame>

--- test/frame.hs
+++ test/frame.hs
@@ -7,21 +7,26 @@ import           Data.Function
 import qualified Data.HashMap.Strict as HM
 import           Data.List
 import           Data.List.Split
+import           Data.Maybe                  (fromMaybe,listToMaybe,maybeToList)
 import qualified Data.Text           as T
 import qualified Data.Text.IO        as T.IO
+import qualified Data.Text.Lazy      as T.L
 import           System.FilePath
 import           Text.Printf
 --
 import           FrameNet.Query.Frame
 import           FrameNet.Type.Common
+import           FrameNet.Type.Definition
 import           FrameNet.Type.Frame
 
 format xs = intercalate "\t" (map (printf "%-30s") xs)
 
 frameType f = printf "%-30s: %s" (f^.frame_name) (T.intercalate ", " (f^..frame_semType.traverse.semtype_name))
 
-main = do
-  let fndir = "/scratch/wavewave/FrameNet/1.7/fndata/fndata-1.7"
+
+fndir = "/scratch/wavewave/FrameNet/1.7/fndata/fndata-1.7"
+
+countFrame = do
   FrameDB db <- loadFrameData (fndir </> "frame")
 
   let lst = sortBy (compare `on` (^.frame_name)) . map snd . HM.toList $ db
@@ -33,3 +38,13 @@ main = do
   {-     lsts = chunksOf 5 lst
   mapM (putStrLn . format) lsts
   -}
+
+
+showDefinition = do
+  FrameDB db <- loadFrameData (fndir </> "frame")
+  let txt = fromMaybe "" ((^.frame_definition) <$> HM.lookup "Purpose" db)
+      defn = p_defRoot (T.L.fromStrict txt)
+  T.IO.putStrLn txt
+  print defn
+
+main = showDefinition
