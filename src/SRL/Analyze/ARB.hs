bf89a49bcaa4f5de9f6b220725487ef1aa9d8b74
Ian-Woo Kim <ianwookim@gmail.com>
2018-02-15 18:52:03 +0000

    Revive ARB
    Summary:
    We migrated our representation to MeaningTree, but for a transient period,
    we had better keep A-R-B representation. This diff revives the deleted module.
    
    Test Plan: none
    
    Reviewers: wavewave
    
    Reviewed By: wavewave
    
    Subscribers: wavewave
    
    Maniphest Tasks: T628
    
    Differential Revision: https://uphere.phacility.com/D2204

---
+++ src/SRL/Analyze/ARB.hs
@@ -0,0 +1,208 @@
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TupleSections       #-}
+
+module SRL.Analyze.ARB where
+
+import           Control.Applicative       ((<|>))
+import           Control.Lens
+import           Control.Lens.Extras
+import           Control.Monad             (guard)
+import           Data.List
+import qualified Data.Text             as T
+import           Control.Monad.Loops       (unfoldM)
+import           Control.Monad.Trans.Class (lift)
+import           Control.Monad.Trans.Maybe (MaybeT(..))
+import           Control.Monad.Trans.State (State,evalState,get,put,modify')
+import           Data.Aeson                (FromJSON(..),ToJSON(..))
+import           Data.Array                ((!))
+import           Data.Graph                (Graph,Vertex,topSort)
+import           Data.List                 (delete,find,elem)
+import           Data.Maybe                (catMaybes,fromMaybe
+                                           ,mapMaybe,maybeToList)
+import           Data.Text                 (Text)
+import           GHC.Generics              (Generic)
+--
+import           Lexicon.Mapping.Causation (causeDualMap,cm_causativeFrame,cm_externalAgent)
+import           Lexicon.Type              (FNFrame(..),FNFrameElement(..),RoleInstance,SenseID)
+import           NLP.Syntax.Clause         (hoistMaybe) -- this should be moved somewhere
+import           NLP.Syntax.Type.Verb      (vp_lemma,vp_negation)
+import           NLP.Syntax.Type.XBar      (X'Tree)
+import           NLP.Type.PennTreebankII   (Lemma(..))
+-- import           NLP.Shared.Type
+import           SRL.Analyze.Type
+import           SRL.Statistics
+--
+
+
+data PrepOr a = PrepOr { _po_prep :: Maybe Text
+                       , _po_main :: a
+                       }
+                              deriving (Eq,Show,Generic)
+
+makeLenses ''PrepOr
+
+instance ToJSON a => ToJSON (PrepOr a)
+instance FromJSON a => FromJSON (PrepOr a)
+
+noprep = PrepOr Nothing
+
+type Frame = Text
+type FrameElement = Text
+type TWord = Text
+
+-- | ARB is the representation of meaning graph with NER tagging. This is necessary in web interface because original meaning graph is too lengthy to be appeared
+--   in webpage. It consists of subject, predicate, object and hashtags. Each item is appeared as a pair of word and its semantic role like Frame or FrameElement.
+--   Hashtag will be used to show additional information, but it is not used now. Object can be an (noun) entity or another subsequent ARB, though only an entity
+--   can be subject.
+data ARB = ARB
+  { _frameARB :: Frame                -- ^ Unlemma form of verb when predicate. Frame when nominal predicate.
+  , _subjectA :: (FrameElement,TWord) -- ^ FrameElement of subject in a sentence and word itself.
+  , _predicateR :: (Frame,TWord)      -- ^ FrameElement of predicate and word itself. Frame is a duplication to _frameARB.
+  , _isNegated :: Bool                -- ^ Negation information of verb.
+  , _objectB :: [(FrameElement,Either (PrepOr ARB) (PrepOr TWord))] -- ^ A tuple of FrameElement and word, or a tuple of FrameElement and subsequent ARB.
+  , _hashTags :: [TWord] -- ^ A list of words to be presented as hashtags.
+  } deriving (Eq,Show, Generic)
+
+makeLenses ''ARB
+
+instance ToJSON ARB
+instance FromJSON ARB
+
+type VertexARB = (Vertex, Vertex, [Vertex])
+
+
+{-
+squashRelFrame :: MeaningGraph -> MeaningGraph
+squashRelFrame mg0 = last (mg0 : unfoldr f mg0)
+  where f mg = do
+         let vs = mg^.mg_vertices
+             es = mg^.mg_edges
+         v <- find (is _Nothing . (^.mv_range)) vs
+         let i = v^.mv_id
+             vs' = filter ((/= i) . (^.mv_id)) vs
+
+
+         j0 <- (^.me_end) <$> find (\e -> (e^.me_start) == i && (e^.me_ismodifier)) es
+         j1 <- (^.me_end) <$> find (\e -> (e^.me_start) == i && not (e^.me_ismodifier)) es
+         (_,_,frm,info) <- v ^? _MGPredicate
+         prep <- info^?_PredPrep
+         let e' = MGEdge (FNFrameElement (unFNFrame frm)) False (Just prep) j0 j1
+             es' = filter ((/= i) . (^.me_start)) es ++ [e']
+             mg' = mg & (mg_vertices .~ vs') . (mg_edges .~ es')
+
+         return (mg',mg')
+-}
+
+isFrame :: MGVertex -> Bool
+isFrame (MGEntity {..})           = False
+isFrame (MGPredicate {..})        = True
+
+
+isEntity :: MGVertex -> Bool
+isEntity = (not . isFrame)
+
+
+subjectList :: [FNFrameElement]
+subjectList = [ "Agent","Speaker","Owner","Cognizer","Actor","Author","Cognizer_agent"
+              , "Protagonist", "Cause", "Instance", "Figure"
+              ]
+
+
+isSubject :: [RoleInstance] -> FNFrame -> Maybe (SenseID,Bool) -> FNFrameElement -> Bool
+isSubject _       _     Nothing rel = any (==rel) subjectList
+isSubject rolemap frame (Just (sense,cause)) rel =
+  fromMaybe False $ do
+    roles <- lookup sense rolemap
+    if cause
+      then do
+        m <- find (\m -> m^.cm_causativeFrame == frame) causeDualMap
+        return (m^.cm_externalAgent == rel)
+      else
+        (((==unFNFrameElement rel) <$> lookup "arg0" roles)
+         <|>
+        ((==unFNFrameElement rel) <$> lookup "arg1" roles))
+
+
+findRel :: [MGEdge] -> Int -> Int -> Maybe MGEdge
+findRel mes i j = find (\me -> (me ^. me_start) == i && (me ^. me_end) == j) mes
+
+
+findVertex :: [MGVertex] -> Int -> Maybe MGVertex
+findVertex mvs i = find (\mv -> (mv ^. mv_id) == i) mvs
+
+
+findLabel :: [MGVertex] -> Int -> Maybe Text
+findLabel mvs i = do
+  v <- findVertex mvs i
+  case v of
+    MGEntity {..}           -> Just _mv_text
+    MGPredicate {..}        -> case _mv_pred_info of
+                                 PredVerb idiom _ _ vrb -> Just (T.intercalate " " idiom) -- (vrb ^. vp_lemma . to unLemma)
+                                 PredPrep p     -> Just p
+                                 PredNominalized n _ _ -> Just (unLemma n)
+                                 PredAppos      -> Just (unFNFrame _mv_frame)
+
+
+findSubjectObjects :: ([RoleInstance],MeaningGraph,Graph)
+                   -> Vertex
+                   -> MaybeT (State [Vertex]) ARB
+findSubjectObjects (rolemap,mg,grph) frmid = do
+  let chldrn = grph ! frmid
+  v <- hoistMaybe $ findVertex (mg^.mg_vertices) frmid
+  (frmtxt,msense,mneg) <- case v of
+                            MGEntity           {..} -> hoistMaybe Nothing
+                            MGPredicate        {..} ->
+                              case _mv_pred_info of
+                                PredVerb _ sns _ vrb -> return (unFNFrame _mv_frame,sns,vrb^.vp_negation)
+                                PredPrep _       -> return (unFNFrame _mv_frame,Nothing,Nothing)
+                                PredNominalized _ _ _ -> return (unFNFrame _mv_frame,Nothing,Nothing)
+                                PredAppos        -> return (unFNFrame _mv_frame,Nothing,Nothing)
+  verbtxt <- hoistMaybe $ findLabel (mg^.mg_vertices) frmid
+  let rels = mapMaybe (findRel (mg^.mg_edges) frmid) chldrn
+      (sidx,subject,b) = fromMaybe (-999,("",""),False) $ do
+        e <- find (\e -> isSubject rolemap (FNFrame frmtxt) msense (e^.me_relation)) rels
+        let sidx = e^.me_end
+        (sidx,,e^.me_ismodifier) . (unFNFrameElement (e^.me_relation),) <$> findLabel (mg^.mg_vertices) sidx
+  guard (not b)
+  (objs :: [(FrameElement,Either (PrepOr ARB) (PrepOr Text))]) <- lift $ do
+    fmap catMaybes . flip mapM (filter (\e -> e ^.me_end /= sidx) rels) $ \o -> do
+      let oidx = o^.me_end
+          oprep = o^.me_prep
+          orole = unFNFrameElement (o^.me_relation)
+      runMaybeT $ do
+        guard (not (o^.me_ismodifier))
+        v' <- hoistMaybe (findVertex (mg^.mg_vertices) oidx)
+        if isFrame v'
+          then do
+            lift (modify' (delete oidx))
+            arb <- findSubjectObjects (rolemap,mg,grph) oidx
+            return (orole,Left (PrepOr oprep arb))
+          else do
+            olabel <- hoistMaybe (findLabel (mg^.mg_vertices) oidx)
+            return (orole,Right (PrepOr oprep olabel))
+  return (ARB frmtxt subject (frmtxt,verbtxt) (maybe False (const True) mneg) objs [])
+
+
+mkARB1 :: ([RoleInstance],MeaningGraph,Graph) -> State [Vertex] (Maybe (Maybe ARB))
+mkARB1 (rolemap,mg,graph) = do
+  xs <- get
+  case xs of
+    [] -> return Nothing
+    (y:ys) -> do
+      put ys
+      r <- runMaybeT $ findSubjectObjects (rolemap,mg,graph) y
+      return (Just r)
+
+
+mkARB :: [RoleInstance] -> MeaningGraph -> [ARB]
+mkARB rolemap mg = filter (^.subjectA._1.to (not.T.null)) . catMaybes $ do
+  let -- mg = squashRelFrame mg0
+      mgraph = getGraphFromMG mg
+  graph <- maybeToList mgraph
+  let framelst = map (^.mv_id) $ filter isFrame $ mg^. mg_vertices
+      vs = filter (`elem` framelst) $ topSort graph
+  evalState (unfoldM (mkARB1 (rolemap,mg,graph))) vs
