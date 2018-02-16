a766135cf61473b81b8ba11d5f12c3abeb48ab67
Ian-Woo Kim <ianwookim@gmail.com>
2018-02-16 04:28:01 +0000

    migrate ARB from rSRL to here
    Summary: this is the right place.
    
    Test Plan: none
    
    Reviewers: wavewave
    
    Reviewed By: wavewave
    
    Subscribers: wavewave
    
    Maniphest Tasks: T631
    
    Differential Revision: https://uphere.phacility.com/D2205

--- src/NLP/Semantics/Type.hs
+++ src/NLP/Semantics/Type.hs
@@ -69,3 +69,21 @@ instance FromJSON MeaningTree
 makePrisms ''MeaningRoleContent
 makeLenses ''MeaningRole
 makeLenses ''MeaningTree
+
+-- | ARB is the representation of meaning graph with NER tagging. This is necessary in web interface because original meaning graph is too lengthy to be appeared
+--   in webpage. It consists of subject, predicate, object and hashtags. Each item is appeared as a pair of word and its semantic role like Frame or FrameElement.
+--   Hashtag will be used to show additional information, but it is not used now. Object can be an (noun) entity or another subsequent ARB, though only an entity
+--   can be subject.
+data ARB = ARB
+  { _frameARB :: Frame                -- ^ Unlemma form of verb when predicate. Frame when nominal predicate.
+  , _subjectA :: (FrameElement,Text)  -- ^ FrameElement of subject in a sentence and word itself.
+  , _predicateR :: (Frame,Text)      -- ^ FrameElement of predicate and word itself. Frame is a duplication to _frameARB.
+  , _isNegated :: Bool                -- ^ Negation information of verb.
+  , _objectB :: [(FrameElement,Either (PrepOr ARB) (PrepOr Text))] -- ^ A tuple of FrameElement and word, or a tuple of FrameElement and subsequent ARB.
+  , _hashTags :: [Text] -- ^ A list of words to be presented as hashtags.
+  } deriving (Eq,Show, Generic)
+
+makeLenses ''ARB
+
+instance ToJSON ARB
+instance FromJSON ARB
