37d991f75fdcdcccb8bd38cf704699a038b3ff98
Ian-Woo Kim <ianwookim@gmail.com>
2018-04-12 12:56:27 +0000

    modernize shell.nix

--- shell.nix
+++ shell.nix
@@ -1,25 +1,34 @@
-{ pkgs               ? import <nixpkgs> {}
-, graph-algorithms   ? <graph-algorithms>
-, nlp-types          ? <nlp-types>
-, uphere-nix-overlay ? <uphere-nix-overlay>
-}:
+{ revision }:
+
+with revision;
+
+let pkgs0 = import nixpkgs { config.allowUnfree = true; };
+
+    pkgs = import pkgs0.path {
+                overlays = [ (self: super: {
+                               libsvm = import (uphere-nix-overlay + "/nix/cpp-modules/libsvm/default.nix") { inherit (self) stdenv fetchurl; };
+                             })
+                           ];
+              };
+in
 
 with pkgs;
 
-let     
-    config1 = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix") { inherit pkgs; };
-    config2 =
-      self: super: {
-#        mkDerivation = args: super.mkDerivation (args // {
-#                         enableLibraryProfiling = true;
-#                       });
-        "nlp-types" = self.callPackage (import nlp-types) {};
-        "graph-algorithms" = self.callPackage (import graph-algorithms) {};
-      };
-    newHaskellPackages = haskellPackages.override {
-                           overrides = self: super: config1 self super // config2 self super;
-                         };
-    hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
+let
+  res_corenlp = import (uphere-nix-overlay + "/nix/linguistic-resources/corenlp.nix") {
+    inherit fetchurl fetchzip srcOnly;
+  };
+  corenlp = res_corenlp.corenlp;
+  corenlp_models = res_corenlp.corenlp_models;
+
+  hsconfig = lib.callPackageWith (pkgs//revision) (uphere-nix-overlay + "/nix/haskell-modules/configuration-semantic-parser-api.nix") {
+    inherit corenlp corenlp_models;
+    fasttext = null;
+    haskellLib = haskell.lib;
+  };
+
+  newHaskellPackages = haskellPackages.override { overrides = hsconfig; };
+  hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
               aeson
               aeson-pretty
               cassava
