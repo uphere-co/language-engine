6034b91e9b7f209aba6d524ffbd72caa39a08469
Ian-Woo Kim <ianwookim@gmail.com>
2018-05-06 21:59:02 +0000

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,50 +1,31 @@
-{ pkgs               ? import <nixpkgs> {}
-, uphere-nix-overlay ? <uphere-nix-overlay>
-, graph-algorithms   ? <graph-algorithms>
-, HCoreNLP           ? <HCoreNLP>
-, HFrameNet          ? <HFrameNet>
-, HWordNet           ? <HWordNet>
-, lexicon            ? <lexicon>
-, nlp-types          ? <nlp-types>
-, PropBank           ? <PropBank>
-, syntactic-analysis ? <syntactic-analysis>
-, VerbNet            ? <VerbNet>
-, wiki-ner           ? <wiki-ner>
-, textview           ? <textview>
-}:
+{ revision }:
+
+with revision;
+
+let pkgs0 = import nixpkgs { config.allowUnfree = true; };
+    pkgs = import pkgs0.path {
+             overlays = [ (self: super: {
+                            libsvm = import (uphere-nix-overlay + "/nix/cpp-modules/libsvm/default.nix") { inherit (self) stdenv fetchurl; };
+                          })
+                        ];
+           };
+
+in
 
 with pkgs;
 
 let
+  fasttext = import (uphere-nix-overlay + "/nix/cpp-modules/fasttext.nix") { inherit stdenv fetchgit; };
   res_corenlp = import (uphere-nix-overlay + "/nix/linguistic-resources/corenlp.nix") {
     inherit fetchurl fetchzip srcOnly;
   };
   corenlp = res_corenlp.corenlp;
   corenlp_models = res_corenlp.corenlp_models;
 
-  hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix")
-               { inherit pkgs; };
+  hsconfig = lib.callPackageWith (pkgs//revision) (uphere-nix-overlay + "/nix/haskell-modules/configuration-semantic-parser-api.nix")
+               { inherit corenlp corenlp_models fasttext; };
 
-  config2 =
-    self: super: {
-      "graph-algorithms" = self.callPackage (import graph-algorithms) {};
-      "HCoreNLP-Proto" = self.callPackage (import (HCoreNLP + "/HCoreNLP-Proto")) {};
-      "HCoreNLP"       = self.callPackage (import HCoreNLP) { inherit jdk corenlp corenlp_models; };
-      "HFrameNet"      = self.callPackage (import (builtins.filterSource (path: type: baseNameOf path != "run") HFrameNet)) {};
-      "HWordNet"       = self.callPackage (import (builtins.filterSource (path: type: baseNameOf path != "run") HWordNet)) {};
-      "lexicon"        = self.callPackage (import (builtins.filterSource (path: type: baseNameOf path != "dist") lexicon)) {};
-      "nlp-types" = self.callPackage (import nlp-types) {};
-      "PropBank" = self.callPackage (import PropBank) {};
-      "syntactic-analysis" = self.callPackage (import syntactic-analysis) {};
-      "textview" = self.callPackage (import textview) {};      
-      "VerbNet" = self.callPackage (import VerbNet) {};
-      "wiki-ner"  = self.callPackage (import wiki-ner) {};
-      
-    };
-  newHaskellPackages = haskellPackages.override {
-    overrides = self: super: hsconfig self super // config2 self super;
-  };
-
+  newHaskellPackages = haskellPackages.override { overrides = hsconfig; };
 
   hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
             cabal-install
