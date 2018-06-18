e127885964a5dba0b71239a060bb1bea27c4f84a
Ian-Woo Kim <ianwookim@gmail.com>
2018-06-18 17:42:16 +0000

    wip

--- lexicon/default.nix
+++ lexicon/default.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, base, stdenv, hashable, lens, text, nlp-types }:
+{ mkDerivation, base, stdenv, formatting, hashable, lens, text, nlp-types }:
 mkDerivation {
   pname = "lexicon";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base hashable lens text nlp-types ];
+  libraryHaskellDepends = [ base formatting hashable lens text nlp-types ];
   license = "unknown";
 }
--- syntactic-analysis/src/NLP/Syntax/Format.hs
+++ syntactic-analysis/src/NLP/Syntax/Format.hs
@@ -13,16 +13,14 @@ import           Data.Maybe
 import           Data.Monoid                   ((<>))
 import           Data.Text                     (Text)
 import qualified Data.Text               as T
-import           Text.Printf
+import           Formatting     ((%),(%.),sformat,stext,left,right,int)
 --
 import           NLP.Type.PennTreebankII
 import           NLP.Type.SyntaxProperty                (Tense(..),Voice(..),Aspect(..))
 --
--- import           NLP.Syntax.Clause
 import           NLP.Syntax.Format.Internal
 import           NLP.Syntax.Type.Verb
 import           NLP.Syntax.Type.XBar
--- import           NLP.Syntax.Util                        (rootTag)
 
 
 formatTense :: Tense -> Text
@@ -41,21 +39,24 @@ formatVoice :: Voice -> Text
 formatVoice Active  = "Actv"
 formatVoice Passive = "Pass"
 
-
-formatVerbProperty :: (a -> Text) -> VerbProperty a -> String
-formatVerbProperty f vp = printf "%3d %-15s : %-19s aux: %-7s neg: %-5s | %s"
-                            (vp^.vp_index) (vp^.vp_lemma.to unLemma)
-                            (formatTense  (vp^.vp_tense)  <> "." <>
-                             formatAspect (vp^.vp_aspect) <> "." <>
-                             formatVoice  (vp^.vp_voice))
-                            (T.intercalate " " (vp^..vp_auxiliary.traverse._2._2.to unLemma))
-                            (fromMaybe "" (vp^?vp_negation._Just._2._2.to unLemma))
-                            (T.intercalate " " (vp^..vp_words.traverse.to (f.fst)))
-
-
-
--- formatAdjunctCP :: AdjunctCP 'PH0 -> Text
--- formatAdjunctCP (AdjunctCP_CP         cp) = "CP" <> showRange (cp^.maximalProjection)
+--   "%3d %-15s : %-19s aux: %-7s neg: %-5s | %s"
+formatVerbProperty :: (a -> Text) -> VerbProperty a -> Text
+formatVerbProperty f vp =
+  sformat
+    (   (right 3 ' ' %. int)
+      %      " " % ls 15
+      %    " : " % ls 19
+      % " aux: " % ls 7
+      % " neg: " % ls 5
+      %    " | " % stext)
+    (vp^.vp_index)
+    (vp^.vp_lemma.to unLemma)
+    (    formatTense  (vp^.vp_tense) <> "."
+    <> formatAspect (vp^.vp_aspect)  <> "."
+    <> formatVoice  (vp^.vp_voice))
+    (T.intercalate " " (vp^..vp_auxiliary.traverse._2._2.to unLemma))
+    (fromMaybe "" (vp^?vp_negation._Just._2._2.to unLemma))
+    (T.intercalate " " (vp^..vp_words.traverse.to (f.fst)))
 
 
 adjunctVPText :: PreAnalysis t -> AdjunctVP 'PH0 -> Text
@@ -68,15 +69,16 @@ formatAdjunctVP (AdjunctVP_PP pp)          = formatPP pp
 
 formatCPDetail :: CP 'PH0 -> String
 formatCPDetail cp =
-  printf "Complementizer Phrase: %-6s\n\
-         \Complementizer       : %-6s  %s\n\
-         \Specifier            : %-6s  %s\n\
-         \Adjunct              :         %s\n\
-         \Tense Phrase         : %-6s\n\
-         \Determiner Phrase    :         %s\n\
-         \Verb Phrase          : %-6s\n\
-         \Verb Complements     :       %s\n\
-         \Verb Adjunts         :       %s\n"
+  stext
+    (   "Complementizer Phrase: "%-6s
+      % "\nComplementizer       : "%-6s"  " %s
+      % "\nSpecifier            : "%-6s"  " %s
+      % "\nAdjunct              :         "%s
+      % "\nTense Phrase         : "%-6s"
+      % "\nDeterminer Phrase    :         "%s
+      % "\nVerb Phrase          : "%-6s
+      % "\nVerb Complements     :       "%s
+      % "\nVerb Adjunts         :       "%s % "\n"
     (show (cp^.maximalProjection))
     head1 head2
     spec1 spec2
