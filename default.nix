9ef6342195e267aa7f46ff743f2ae2a038910ab4
Ian-Woo Kim <ianwookim@gmail.com>
2018-06-24 03:11:37 +0000

    use reflex-platform and callCabal2nix!

--- default.nix
+++ default.nix
@@ -1,72 +1,70 @@
 { revision }:
 
-with revision;
+
+
+(import ./reflex-platform {}).project ({ pkgs, ... }:
 
 let
 
-  pkgs = import nixpkgs { config.allowUnfree = true; };
-
-in 
-
-with pkgs;
-
-let
-
-  fasttext = import (uphere-nix-overlay + "/nix/cpp-modules/fasttext.nix") { inherit stdenv fetchgit; };
-
-  res_corenlp = import (uphere-nix-overlay + "/nix/linguistic-resources/corenlp.nix") {
-    inherit fetchurl fetchzip srcOnly;
+  fasttext = import (revision.uphere-nix-overlay + "/nix/cpp-modules/fasttext.nix") { inherit (pkgs) stdenv fetchgit; };
+  res_corenlp = import (revision.uphere-nix-overlay + "/nix/linguistic-resources/corenlp.nix") {
+    inherit (pkgs) fetchurl fetchzip srcOnly;
   };
   corenlp = res_corenlp.corenlp;
   corenlp_models = res_corenlp.corenlp_models;
 
-  hsconfig = lib.callPackageWith (pkgs//revision) (uphere-nix-overlay + "/nix/haskell-modules/configuration-language-engine.nix")
-               { inherit corenlp corenlp_models fasttext;
-               };
+  hsconfig = pkgs.lib.callPackageWith (pkgs//revision) (revision.uphere-nix-overlay + "/nix/haskell-modules/configuration-language-engine.nix")
+               { inherit corenlp corenlp_models fasttext; };
+
+in
 
 
-  newHaskellPackages = pkgs.haskell.packages.ghc821.override { overrides = hsconfig; };
+{
+  packages = {
+    graph-algorithms      = ./graph-algorithms;
+    HFrameNet             = ./HFrameNet;
+    lexicon               = ./lexicon;
+    lexicon-builder       = ./lexicon-builder;
+    multi-word-tagger     = ./multi-word-tagger;
+    OntoNotes             = ./OntoNotes;
+    PropBank              = ./PropBank;
+    semantic-role-labeler = ./semantic-role-labeler;
+    semantic-types        = ./semantic-types;
+    syntactic-analysis    = ./syntactic-analysis;
+    time-tagger           = ./time-tagger;
+    VerbNet               = ./VerbNet;
+    wiki-ner              = ./wiki-ner;
+  };
 
-  hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
-            aeson-pretty
-            async
-            base-prelude
-            binary-orphans
-            blaze-html
-            boxes
-            cassava
-            cmdargs
-            directory-tree
-            formatting
-            foreign-store
-            haskell-src-exts
-            html-entities
-            lens            
-            mwc-random
-            raw-strings-qq
-            taggy-lens
-            tasty
-            tasty-hunit
-            transformers-compat
-            unordered-containers
-            vector-algorithms
-            xxhash
-            #
-            fastText
-            HCoreNLP-Proto
-            HCoreNLP
-            HUKB-driver
-            textview
-          ]);
-in 
 
-stdenv.mkDerivation {
-  name = "language-engine-dev";
-  buildInputs = [ hsenv fasttext ];
-  shellHook = ''
-    export OMP_NUM_THREADS=12
-    export CLASSPATH="${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar:${hsenv}/share/x86_64-linux-ghc-8.0.2/HCoreNLP-0.1.0.0/HCoreNLPProto.jar";
-  '';
-  
+  overrides = hsconfig;
 
-}
+  tools = ghc:
+            # TODO: move this code to uphere-nix-overlay
+            let corenlpenv = pkgs.makeSetupHook { }
+                  (pkgs.writeText "setup-hook.sh" ''
+                     export CLASSPATH="${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar:${ghc.HCoreNLP}/share/x86_64-linux-ghc-8.2.1/HCoreNLP-0.1.0.0/HCoreNLPProto.jar"
+                  '');
+            in [ corenlpenv ] ;
+
+  shells = {
+    ghc8_2_1 = [ 
+                 "graph-algorithms"
+                 "HFrameNet"
+                 "lexicon"
+                 "lexicon-builder"
+                 "multi-word-tagger"
+                 "OntoNotes"
+                 "PropBank"
+                 "semantic-role-labeler"
+                 "semantic-types"
+                 "syntactic-analysis"
+                 "time-tagger"
+                 "VerbNet"
+                 "wiki-ner"
+
+               ];
+  };
+})
+
+
