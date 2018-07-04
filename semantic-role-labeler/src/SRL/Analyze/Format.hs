ade2d8d9938237a5b02d0d54be619d143e2f3887
Ian-Woo Kim <ianwookim@gmail.com>
2018-07-04 23:05:35 +0000

    remove all printf

--- semantic-role-labeler/src/SRL/Analyze/Format.hs
+++ semantic-role-labeler/src/SRL/Analyze/Format.hs
@@ -11,7 +11,7 @@ module SRL.Analyze.Format where
 import           Control.Lens                            ((^..),(^.),(^?),_1,_2,_3,_Just,_Right,to)
 import           Control.Lens.Extras                     (is)
 import           Data.Foldable
-import           Data.List                               (intercalate,intersperse)
+import           Data.List                               (intersperse)
 import           Data.Maybe                              (fromMaybe,mapMaybe)
 import           Data.Monoid                             ((<>))
 import           Data.Text                               (Text)
@@ -19,7 +19,8 @@ import qualified Data.Text                       as T
 import qualified Data.Text.Format                as T.F
 import qualified Data.Text.Lazy                  as T.L
 import qualified Data.Text.IO                    as T.IO
-import           Text.Printf                             (printf)
+import           Formatting                              (Format,(%),(%.),sformat,stext,int)
+import qualified Formatting                      as F    (left,right)
 import           Text.PrettyPrint.Boxes                  (Box,left,hsep,text,top,vcat,render)
 --
 import           CoreNLP.Simple.Convert                  (sentToTokens')
@@ -54,6 +55,14 @@ import           SRL.Analyze.Type.Match                  (ExceptionalFrame(..),O
                                                          ,tf_frameID,tf_feCore,tf_fePeri)
 import           SRL.Analyze.Util                        (addTag,convertTagPosFromTokenToChar,underlineText)
 
+-- note that left/right are reversed
+rs,ls :: Int -> Format r (Text -> r)
+rs n = F.left  n ' ' %. stext
+ls n = F.right n ' ' %. stext
+
+rd,ld :: (Integral a) => Int -> Format r (a -> r)
+rd n = F.left  n ' ' %. int
+ld n = F.right n ' ' %. int
 
 formatExFrame :: ExceptionalFrame -> Text
 formatExFrame FrameCopula = "** COPULA **"
@@ -62,8 +71,9 @@ formatExFrame FrameLightVerb = "** LIGHT VERB **"
 formatExFrame _              = "** UNIDENTIFIED **"
 
 
-formatLemmaPOS :: Token -> String
-formatLemmaPOS t = printf "%10s %5s" (t^.token_lemma) (show (t^.token_pos))
+formatLemmaPOS :: Token -> Text
+formatLemmaPOS t =
+  sformat (rs 10 % " " % rs 5) (t^.token_lemma) (T.pack (show (t^.token_pos)))
 
 
 formatTimex :: (SentItem CharIdx, [TagPos CharIdx (Maybe Text)]) -> [Text]
@@ -84,21 +94,21 @@ showFormatTimex' (s,a) = T.IO.putStrLn (T.intercalate "\n" (getFormatTimex' (s,a
 
 
 
-formatSense :: ((ONSenseFrameNetInstance,Int),a) -> String
+formatSense :: ((ONSenseFrameNetInstance,Int),a) -> Text
 formatSense ((onfninst,num),_) =
-  printf "%-8s (%4d cases) | %-40s | %-20s | %-40s      ------       %-30s "
-         (onfninst^.onfn_senseID._3)
-         num
-         (onfninst^.onfn_definition)
-         (onfninst^.onfn_frame.to (either formatExFrame (^.tf_frameID)))
-         (maybe "" (T.intercalate ", ") ((onfninst^?onfn_frame._Right.tf_feCore)))
-         (maybe "" (T.intercalate ", ") ((onfninst^?onfn_frame._Right.tf_fePeri)))
+  sformat (ls 8 % " (" % rd 4 % " cases) | " % ls 40 % " | " % ls 20 % " | " % ls 40 % "      ------       " % ls 30 % " ")
+    (onfninst^.onfn_senseID._3)
+    num
+    (onfninst^.onfn_definition)
+    (onfninst^.onfn_frame.to (either formatExFrame (^.tf_frameID)))
+    (maybe "" (T.intercalate ", ") ((onfninst^?onfn_frame._Right.tf_feCore)))
+    (maybe "" (T.intercalate ", ") ((onfninst^?onfn_frame._Right.tf_fePeri)))
 
 
 
-formatFrame :: (ONSenseFrameNetInstance, Int) -> String
+formatFrame :: (ONSenseFrameNetInstance, Int) -> Text
 formatFrame t =
-  printf " %-20s %-4d | %-40s      ------      %-30s\n"
+  sformat (" " % ls 20 % " " % rd 4 % " | " % ls 40 % "      ------      " % ls 30 % "\n")
     (t ^. _1 . onfn_frame . to (either formatExFrame (^.tf_frameID)))
     (t ^. _2)
     (maybe "" (T.intercalate ", ") (t^?_1.onfn_frame._Right.tf_feCore))
@@ -108,30 +118,30 @@ formatFrame t =
 formatSenses :: Bool  -- ^ doesShowOtherSense
              -> [((ONSenseFrameNetInstance,Int),[Text])]
              -> [(([Text],RoleInstance,Int), [(ArgPattern () GRel,Int)])]
-             -> String
+             -> Text
 formatSenses doesShowOtherSense onfnlst rmtoppatts
   =  "--------------------------------------------------------------------------------------------------\n"
-     ++ intercalate "\n--------------------------------------------------------------------------------------------------\n" (flip map rmtoppatts (\((_,rm,_),toppatts) ->
+     <> T.intercalate "\n--------------------------------------------------------------------------------------------------\n" (flip map rmtoppatts (\((_,rm,_),toppatts) ->
           let argpattstr = formatArgPattStat toppatts
               mfrm = find (\x -> x^._1._1.onfn_senseID == rm^._1 ) onfnlst
               framestr = case mfrm of
                            Nothing -> "Frames: "
-                           Just (frm,idiom) -> show idiom ++ " " ++ "Frames: " ++ formatFrame frm
-          in framestr ++
-             (T.unpack (formatRoleMap (rm^._2)) ++ ("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"<> T.unpack argpattstr))))
-     ++ "\n--------------------------------------------------------------------------------------------------\n"
-     ++ if doesShowOtherSense
-        then "\n\n\n*********************************************\n" ++ intercalate "\n" (map formatSense onfnlst)
+                           Just (frm,idiom) -> T.pack (show idiom) <> " " <> "Frames: " <> formatFrame frm
+          in framestr <>
+             (formatRoleMap (rm^._2) <> ("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"<> argpattstr))))
+     <> "\n--------------------------------------------------------------------------------------------------\n"
+     <> if doesShowOtherSense
+        then "\n\n\n*********************************************\n" <> T.unlines (map formatSense onfnlst)
         else ""
 
 
-formatLinkedMention :: EntityMention Text -> String
-formatLinkedMention Cite {..} = printf "%3d: (-> %3d) %s " (_emuid _uid) (_emuid _ref) (formatEMInfo _info)
-formatLinkedMention Self {..} = printf "%3d:          %s " (_emuid _uid)               (formatEMInfo _info)
+formatLinkedMention :: EntityMention Text -> Text
+formatLinkedMention Cite {..} = sformat (rd 3 % ": (-> " % rd 3 % ") " % stext % " ") (_emuid _uid) (_emuid _ref) (formatEMInfo _info)
+formatLinkedMention Self {..} = sformat (rd 3 % ":          "% stext % " ")           (_emuid _uid)               (formatEMInfo _info)
 
 
-formatIndexedTimex :: (Char,Maybe Text) -> String
-formatIndexedTimex (c,mtxt)   = printf "%3s:          %s " (T.singleton c) (fromMaybe "" mtxt)
+formatIndexedTimex :: (Char,Maybe Text) -> Text
+formatIndexedTimex (c,mtxt)   = sformat (rs 3 % ":          " % stext % " ") (T.singleton c) (fromMaybe "" mtxt)
 
 
 formatTaggedSentences :: (a -> Text)
@@ -142,14 +152,14 @@ formatTaggedSentences f sents_tagged =
   in vcat top $ map (text . T.unpack) txts
 
 
-formatPreNE :: PreNE -> String
+formatPreNE :: PreNE -> Text
 formatPreNE tag = case resolvedUID tag of
                     Left _ -> "unresolved"
-                    Right i -> show i
+                    Right i -> T.pack (show i)
 
 
-formatEMInfo :: EMInfo Text -> String
-formatEMInfo em@(_,_,tag) = printf "%-25s %-20s" (WEL.entityName em) (formatPreNE tag)
+formatEMInfo :: EMInfo Text -> Text
+formatEMInfo em@(_,_,tag) = sformat (ls 25 % " " % ls 20) (WEL.entityName em) (formatPreNE tag)
 
 
 formatTagged :: [[Maybe Token]]
@@ -161,7 +171,7 @@ formatTagged mtokenss sentitems tlst =
       clst = mapMaybe (convertTagPosFromTokenToChar toks) tlst
       sents_tagged = map (addTag clst) sentitems
       doc1 = formatTaggedSentences (either (T.pack . show . _emuid . _uid) (T.singleton . (^._1))) sents_tagged
-      doc2 = vcat top . intersperse (text "") . map (text. either formatLinkedMention formatIndexedTimex) $ map (\(TagPos (_,_,x)) -> x) clst
+      doc2 = vcat top . intersperse (text "") . map (text . T.unpack . either formatLinkedMention formatIndexedTimex) $ map (\(TagPos (_,_,x)) -> x) clst
   in hsep 10 left [doc1,doc2]
 
 
@@ -182,7 +192,7 @@ formatDocStructure showdetail (DocStructure mtokenss sentitems mergedtags sstrs)
 
 formatSentStructure :: Bool -> SentStructure -> [Text]
 formatSentStructure _showdetail ss@(SentStructure i ptr _ _ _ _ vstrs) =
-   let subline1 = [ T.pack (printf "-- Sentence %3d ----------------------------------------------------------------------------------" i)
+   let subline1 = [ sformat ("-- Sentence " % rd 3 % " ----------------------------------------------------------------------------------") i
                   , formatIndexTokensFromTree 0 ptr
                   , prettyPrint 0 ptr ]
 
@@ -196,31 +206,31 @@ formatSentStructure _showdetail ss@(SentStructure i ptr _ _ _ _ vstrs) =
 formatVerbStructure :: VerbStructure -> [Text]
 formatVerbStructure (VerbStructure vp senses mrmmtoppatts) =
   [--  formatVPwithPAWS tagged clausetr x'tr vp
-    T.pack (printf "Verb: %-20s" (vp^.vp_lemma.to unLemma))
-  , T.pack (formatSenses False senses mrmmtoppatts)
+    sformat ("Verb: " % ls 20) (vp^.vp_lemma.to unLemma)
+  , formatSenses False senses mrmmtoppatts
   ]
 
 
-showMatchedFE :: PreAnalysis '[Lemma] -> (FNFrameElement,Referent (CompVP 'PH1)) -> String
---                              FE   range prep text
+showMatchedFE :: PreAnalysis '[Lemma] -> (FNFrameElement,Referent (CompVP 'PH1)) -> Text
 showMatchedFE _a0 (fe,x) =
   case referent2CompVP x of
-    CompVP_DP rng_dp -> printf "%-15s: %-7s %3s %s" (unFNFrameElement fe) ("DP" ++ show rng_dp)  ("" :: Text) ("" :: Text)
-    CompVP_AP rng_ap -> printf "%-15s: %-7s %3s %s" (unFNFrameElement fe) ("AP" ++ show rng_ap)  ("" :: Text) ("" :: Text)
-    CompVP_CP rng_cp -> printf "%-15s: %-7s %3s %s" (unFNFrameElement fe) ("CP" ++ show rng_cp) ("" :: Text) ("" :: Text)
-    CompVP_PP rng_pp -> printf "%-15s: %-7s %3s %s" (unFNFrameElement fe) ("PP" ++ show rng_pp) ("" :: Text) ("" :: Text)
+    --                              FE         range         prep          text
+    CompVP_DP rng_dp -> sformat (ls 15 % ": " % ls 7 % " " % rs 3 % " " % stext) (unFNFrameElement fe) (T.pack ("DP" ++ show rng_dp)) "" ""
+    CompVP_AP rng_ap -> sformat (ls 15 % ": " % ls 7 % " " % rs 3 % " " % stext) (unFNFrameElement fe) (T.pack ("AP" ++ show rng_ap)) "" ""
+    CompVP_CP rng_cp -> sformat (ls 15 % ": " % ls 7 % " " % rs 3 % " " % stext) (unFNFrameElement fe) (T.pack ("CP" ++ show rng_cp)) "" ""
+    CompVP_PP rng_pp -> sformat (ls 15 % ": " % ls 7 % " " % rs 3 % " " % stext) (unFNFrameElement fe) (T.pack ("PP" ++ show rng_pp)) "" ""
 
 
 showMatchedFrame :: FrameDB
                  -> PreAnalysis '[Lemma]
                  -> (X'Tree 'PH1,VerbStructure, CP 'PH1)
-                 -> [String]
+                 -> [Text]
 showMatchedFrame framedb tagged (x'tr,vstr,cp) =
   let x = "---------------------------"
       xs = flip concatMap (matchFrame framedb x'tr (vstr,cp)) $ \(rng,_,_,FMR idiom frame mselected _,_) ->
-             ([ "predicate: "  <> show rng
-              , T.unpack ("Verb: "  <> T.intercalate " " idiom)
-              , T.unpack ("Frame: " <> unFNFrame frame) ]
+             ([ "predicate: "  <> T.pack (show rng)
+              , "Verb: "  <> T.intercalate " " idiom
+              , "Frame: " <> unFNFrame frame ]
               ++ (flip concatMap mselected (\(_,felst) -> map (showMatchedFE tagged) felst)))
   in x:xs
 
