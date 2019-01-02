028d1b380c21e299d692613f1ee6a18f68923a94
Ian-Woo Kim <ianwookim@gmail.com>
2019-01-02 10:48:03 +0000

    use ISO-8859-1.

--- wiki-ner/app/wiki-etl/Main.hs
+++ wiki-ner/app/wiki-etl/Main.hs
@@ -1,14 +1,18 @@
 module Main where
 
 import Control.Monad (void)
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource (runResourceT)
+import qualified Data.ByteString.Char8 as B
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Pipes ((>->),runEffect)
-import qualified Pipes.ByteString as PB (fromHandle)
+import qualified Pipes.ByteString as PB (fromHandle,take)
 import qualified Pipes.GZip as PZ (decompress)
 import qualified Pipes.Prelude as P (chain,drain,print)
 import qualified Pipes.Text as PT (take)
-import Pipes.Text.Encoding (decodeUtf8)
+import Pipes.Text.Encoding (decodeUtf8,decodeIso8859_1)
 import System.IO (withFile, IOMode(..))
 
 fileCategoryLinks :: FilePath
@@ -17,14 +21,16 @@ fileCategoryLinks = "/workspace/1/wavewave/wikipedia/enwiki-20181220-categorylin
 fileCategory :: FilePath
 fileCategory = "/workspace/1/wavewave/wikipedia/enwiki-20181220-category.sql.gz"
 
--- initiator = "INSERT INTO `categorylinks` VALUES"
+initiator :: Text
+initiator = "INSERT INTO `categorylinks` VALUES"
 
 main :: IO ()
 main = do
   putStrLn "wiki-etl"
 
-  withFile fileCategory ReadMode $ \h -> do
-    runEffect $     void (decodeUtf8 (PZ.decompress (PB.fromHandle h)))
-                >-> PT.take 20000
-                >-> P.chain TIO.putStr
-                >-> P.drain
+  withFile fileCategoryLinks ReadMode $ \h -> do
+    runEffect $
+          void (decodeIso8859_1 (PZ.decompress (PB.fromHandle h)))
+      >-> PT.take 2500
+      >-> P.chain TIO.putStr
+      >-> P.drain
