59ae5614dab573b0a7faa56c0e3e74fb1e3435b7
Ian-Woo Kim <ianwookim@gmail.com>
2019-03-11 05:42:49 +0000

    buildable CompanyEL.Load

--- wiki-ner/src/CompanyEL/Load.hs
+++ wiki-ner/src/CompanyEL/Load.hs
@@ -1,51 +1,52 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-
 module CompanyEL.Load where
 
-import           Control.Lens               ((^.),_2)
-import           Control.Monad              (forM)
-import qualified Data.Aeson                 as A
-import qualified Data.ByteString.Lazy       as BL
+import           Control.Lens        ( (^.), _2 )
+import           Control.Monad       ( forM )
+import qualified Data.Aeson as A
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Char8 as BL8
-import qualified Data.Csv             as C
-import           Data.Hashable              (Hashable)
-import qualified Data.HashMap.Strict  as HM
-import           Data.List                  (foldl')
-import           Data.Maybe                 (fromMaybe)
-import           Data.Text                  (Text)
-import qualified Data.Text            as T
-import qualified Data.Text.IO         as T.IO
-import qualified Data.Vector          as V
-import           System.FilePath            ((</>))
+import qualified Data.Csv as C
+import           Data.Hashable       ( Hashable )
+import           Data.HashMap.Strict ( HashMap )
+import qualified Data.HashMap.Strict as HM
+import           Data.List           ( foldl' )
+import           Data.Maybe          ( fromMaybe )
+import           Data.Text           ( Text )
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Vector as V
+import           System.FilePath     ( (</>) )
 --
 import           CompanyEL.Type
-import qualified WikiEL.ETL.LoadData     as LD
-import           WikiEL.Run                    (reprFileG)
-import qualified WikiEL.Type.FileFormat  as FF
-import qualified WikiEL.Type.Wikidata    as WD
+-- import qualified WikiEL.ETL.LoadData     as LD
+import           WikiEL.Tagger    ( loadEntityReprs, reprFileG )
+import           WikiEL.Type      ( ItemID(..), ItemRepr(..), EntityReprRow(..) )
+-- import qualified WikiEL.Type.FileFormat  as FF
+-- import qualified WikiEL.Type.Wikidata    as WD
 
 
-companyDataDir = "/data/groups/uphere/data/NER/company"
-wikiDataDir = "/data/groups/uphere/wikidata"
+-- companyDataDir = "/data/groups/uphere/data/NER/company"
+-- wikiDataDir = "/data/groups/uphere/wikidata"
 
 
-mkNameUIDHM :: [(WD.ItemID,Text)] -> HM.HashMap Text WD.ItemID
+mkNameUIDHM :: [(ItemID,Text)] -> HashMap Text ItemID
 mkNameUIDHM nameTable = foldl' (\acc (i,n) -> HM.insert n i acc) HM.empty nameTable
 
 
-mkUIDAliasHM :: [(WD.ItemID,Text)] -> HM.HashMap WD.ItemID [Text]
+mkUIDAliasHM :: [(ItemID,Text)] -> HashMap ItemID [Text]
 mkUIDAliasHM nameTable = foldl' (\acc (i,n) -> HM.insertWith (\xs1 -> (\xs2 -> xs1 ++ xs2)) i [n] acc) HM.empty nameTable
 
 
 -- | returns (Wiki UID, Text)
-loadNameTable :: FilePath -> IO [(WD.ItemID,Text)]
+loadNameTable :: FilePath -> IO [(ItemID,Text)]
 loadNameTable dataDir = do
-  reprs <- LD.loadEntityReprs (reprFileG dataDir)
-  pure $ map (\x -> (FF._uid x,(WD._repr . FF._repr) x)) reprs
+  reprs <- loadEntityReprs (reprFileG dataDir)
+  pure $ map (\x -> (_row_uid x, (_repr . _row_repr) x)) reprs
 
 
-aliasFinder :: (Hashable t, Hashable k, Eq t, Eq k) => HM.HashMap t k -> HM.HashMap k a -> t -> Maybe a
+aliasFinder :: (Hashable t, Hashable k, Eq t, Eq k) => HashMap t k -> HashMap k a -> t -> Maybe a
 aliasFinder nuid uida txt =
   let muid = HM.lookup txt nuid
   in case muid of
@@ -62,16 +63,17 @@ getCompanyListFromJSON fp = do
     Just result -> pure result
 
 
-
-
-constructCompanyListFromCSV :: [(WD.ItemID,Text)] -> IO [CompanyInfo]
-constructCompanyListFromCSV nameTable = do
+constructCompanyListFromCSV ::
+     (FilePath,FilePath) -- ^ company data dir, wiki data dir
+  -> [(ItemID,Text)]
+  -> IO [CompanyInfo]
+constructCompanyListFromCSV (companyDataDir,wikiDataDir) nameTable = do
   let nuid = mkNameUIDHM nameTable
       uida = mkUIDAliasHM nameTable
 
   let fps = map (companyDataDir </>) ["AMEX.csv","NASDAQ.csv","NYSE.csv"]
   clist <- fmap concat $ forM fps $ \fp -> do
-    txt' <- T.IO.readFile fp
+    txt' <- TIO.readFile fp
     let tlines = T.lines txt'
         txt = T.intercalate "\n" $ map (T.reverse . (T.drop 2) . T.reverse) tlines
         bstr = BL8.pack $ T.unpack txt
